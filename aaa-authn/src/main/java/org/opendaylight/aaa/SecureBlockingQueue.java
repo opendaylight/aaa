1390a8b8aa7bda892c5ef7db3d3aea83f61208ac
Liem Nguyen <liem_m_nguyen@hp.com>
2014-06-25 10:20:47 -0700

    BlockingQueue decorator to transfer auth context via data elements
    Change-Id: I7dc5e47a38f2c7407d3d8d9f580289f522c8da09
    Signed-off-by: Liem Nguyen <liem_m_nguyen@hp.com>

---
+++ aaa-authn/src/main/java/org/opendaylight/aaa/SecureBlockingQueue.java
@@ -0,0 +1,257 @@
+/*
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P. and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import org.opendaylight.aaa.api.Authentication;
+
+/**
+ * A {@link BlockingQueue} decorator with injected security context.
+ *
+ * @author liemmn
+ *
+ * @param <T>
+ *            queue element type
+ */
+public class SecureBlockingQueue<T> implements BlockingQueue<T> {
+    private final BlockingQueue<SecureData<T>> queue;
+
+    /**
+     * Constructor.
+     *
+     * @param queue
+     *            blocking queue implementation to use
+     */
+    public SecureBlockingQueue(BlockingQueue<SecureData<T>> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public T remove() {
+        return setAuth(queue.remove());
+    }
+
+    @Override
+    public T poll() {
+        return setAuth(queue.poll());
+    }
+
+    @Override
+    public T element() {
+        return setAuth(queue.element());
+    }
+
+    @Override
+    public T peek() {
+        return setAuth(queue.peek());
+    }
+
+    @Override
+    public int size() {
+        return queue.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Iterator<SecureData<T>> it = queue.iterator();
+
+            @Override
+            public boolean hasNext() {
+                return it.hasNext();
+            }
+
+            @Override
+            public T next() {
+                return it.next().data;
+            }
+
+            @Override
+            public void remove() {
+                it.remove();
+            }
+        };
+    }
+
+    @Override
+    public Object[] toArray() {
+        return toData().toArray();
+    }
+
+    @SuppressWarnings("hiding")
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return toData().toArray(a);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return toData().containsAll(c);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return queue.addAll(fromData(c));
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return queue.removeAll(fromData(c));
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return queue.retainAll(fromData(c));
+    }
+
+    @Override
+    public void clear() {
+        queue.clear();
+    }
+
+    @Override
+    public boolean add(T e) {
+        return queue.add(new SecureData<>(e));
+    }
+
+    @Override
+    public boolean offer(T e) {
+        return queue.offer(new SecureData<>(e));
+    }
+
+    @Override
+    public void put(T e) throws InterruptedException {
+        queue.put(new SecureData<T>(e));
+    }
+
+    @Override
+    public boolean offer(T e, long timeout, TimeUnit unit)
+            throws InterruptedException {
+        return queue.offer(new SecureData<>(e), timeout, unit);
+    }
+
+    @Override
+    public T take() throws InterruptedException {
+        return setAuth(queue.take());
+    }
+
+    @Override
+    public T poll(long timeout, TimeUnit unit) throws InterruptedException {
+        return setAuth(queue.poll(timeout, unit));
+    }
+
+    @Override
+    public int remainingCapacity() {
+        return queue.remainingCapacity();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        Iterator<SecureData<T>> it = queue.iterator();
+        while (it.hasNext()) {
+            SecureData<T> sd = it.next();
+            if (sd.data.equals(o))
+                return queue.remove(sd);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        Iterator<SecureData<T>> it = queue.iterator();
+        while (it.hasNext()) {
+            SecureData<T> sd = it.next();
+            if (sd.data.equals(o))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int drainTo(Collection<? super T> c) {
+        Collection<SecureData<T>> sd = new ArrayList<>();
+        int n = queue.drainTo(sd);
+        c.addAll(toData(sd));
+        return n;
+    }
+
+    @Override
+    public int drainTo(Collection<? super T> c, int maxElements) {
+        Collection<SecureData<T>> sd = new ArrayList<>();
+        int n = queue.drainTo(sd, maxElements);
+        c.addAll(toData(sd));
+        return n;
+    }
+
+    // Rehydrate security context
+    private T setAuth(SecureData<T> i) {
+        AuthenticationManager.instance().set(i.auth);
+        return i.data;
+    }
+
+    // Construct secure data collection from a plain old data collection
+    @SuppressWarnings("unchecked")
+    private Collection<SecureData<T>> fromData(Collection<?> c) {
+        Collection<SecureData<T>> sd = new ArrayList<>(c.size());
+        for (Object d : c)
+            sd.add((SecureData<T>) new SecureData<>(d));
+        return sd;
+    }
+
+    // Extract the data portion out from the secure data
+    @SuppressWarnings("unchecked")
+    private Collection<T> toData() {
+        return toData(Arrays.<SecureData<T>> asList(queue
+                .toArray(new SecureData[0])));
+    }
+
+    // Extract the data portion out from the secure data
+    private Collection<T> toData(Collection<SecureData<T>> secureData) {
+        Collection<T> data = new ArrayList<>(secureData.size());
+        Iterator<SecureData<T>> it = secureData.iterator();
+        while (it.hasNext())
+            data.add(it.next().data);
+        return data;
+    }
+
+    // Inject security context
+    public static final class SecureData<T> {
+        private final T data;
+        private final Authentication auth;
+
+        private SecureData(T data) {
+            this.data = data;
+            this.auth = AuthenticationManager.instance().get();
+        }
+
+        @SuppressWarnings("rawtypes")
+        @Override
+        public boolean equals(Object o) {
+            if (o == null) return false;
+            return (o instanceof SecureData)
+                    ? data.equals(((SecureData) o).data): false;
+        }
+
+        @Override
+        public int hashCode() {
+            return data.hashCode();
+        }
+    }
+}
--- aaa-authn/src/test/java/org/opendaylight/aaa/AuthenticationManagerTest.java
+++ aaa-authn/src/test/java/org/opendaylight/aaa/AuthenticationManagerTest.java
@@ -12,14 +12,10 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -57,96 +53,6 @@ public class AuthenticationManagerTest {
                 .invokeAll(Arrays.asList(new Worker(), new Worker()));
         for (Future<Authentication> f : fs)
             assertEquals(auth, f.get());
-    }
-
-    @Test
-    public void testContextPassing() throws InterruptedException,
-            ExecutionException {
-        Broker broker = new Broker();
-        ExecutorService threadPool = Executors.newFixedThreadPool(3);
-        threadPool.execute(new Consumer("1", broker));
-        threadPool.execute(new Consumer("2", broker));
-        threadPool.submit(new Producer(broker)).get();
-        threadPool.shutdown();
-    }
-
-    // Data queue
-    private class Broker {
-        BlockingQueue<SecureInteger> queue = new ArrayBlockingQueue<SecureInteger>(10);
-        volatile boolean continueProducing = true;
-
-        public void put(Integer data) throws InterruptedException {
-            // Set current security context into data
-            queue.put(new SecureInteger(data, as.get()));
-        }
-
-        public Integer get() throws InterruptedException {
-            SecureInteger i = queue.poll(1, TimeUnit.SECONDS);
-            // Set current security context from data
-            as.set(i.auth);
-            return i.data;
-        }
-
-        // Injected security data
-        final class SecureInteger {
-            Integer data;
-            Authentication auth;
-            SecureInteger(Integer data, Authentication auth) {
-                this.data = data;
-                this.auth = auth;
-            }
-        }
-    }
-
-    // Producer security context induced
-    private class Producer implements Runnable {
-        private final Broker broker;
-
-        public Producer(Broker broker) {
-            this.broker = broker;
-        }
-
-        @Override
-        public void run() {
-            try {
-                for (Integer i = 1; i < 5 + 1; ++i) {
-                    System.out.println("Producer produced: " + i);
-                    // Each data produced has a different security context!
-                    setAuth(i.toString());
-                    Thread.sleep(100);
-                    broker.put(i);
-                }
-                broker.continueProducing = false;
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
-
-    // Security context passed to consumer
-    private class Consumer implements Runnable {
-        private final String name;
-        private final Broker broker;
-
-        public Consumer(String name, Broker broker) {
-            this.name = name;
-            this.broker = broker;
-        }
-
-        @Override
-        public void run() {
-            try {
-                Integer data = broker.get();
-                while (broker.continueProducing || data != null) {
-                    System.out.println("Consumer " + this.name + " consumes: "
-                            + data + " " + as.get());
-                    data = broker.get();
-                    assertEquals(data.toString(), as.get().userName());
-                }
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
-        }
     }
 
     private class Worker implements Callable<Authentication> {
