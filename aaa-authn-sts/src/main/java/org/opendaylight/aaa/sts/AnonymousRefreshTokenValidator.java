a8d1403f652026633468345fc0c68861bccf67f6
Liem Nguyen <liem_m_nguyen@hp.com>
2014-06-30 15:51:18 -0700

    Added refresh token and federation endpoint.
    Change-Id: I9a086bdc2e4356ee071ca27371dcc6f4a7570cc6
    Signed-off-by: Liem Nguyen <liem_m_nguyen@hp.com>

---
+++ aaa-authn-sts/src/main/java/org/opendaylight/aaa/sts/AnonymousRefreshTokenValidator.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P. and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.sts;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.oltu.oauth2.common.OAuth;
+import org.apache.oltu.oauth2.common.validators.AbstractValidator;
+
+/**
+ * A refresh token validator that does not enforce client identification.
+ *
+ * @author liemmn
+ *
+ */
+public class AnonymousRefreshTokenValidator extends
+        AbstractValidator<HttpServletRequest> {
+
+    public AnonymousRefreshTokenValidator() {
+        requiredParams.add(OAuth.OAUTH_GRANT_TYPE);
+        requiredParams.add(OAuth.OAUTH_REFRESH_TOKEN);
+
+        enforceClientAuthentication = false;
+    }
+}
