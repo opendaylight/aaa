27e9440b7ac00777ce8cad93cc8675a8c05456d8
Liem Nguyen <liem_m_nguyen@hp.com>
2014-07-15 09:18:26 -0700

    Removed sal dependencies for authn
    Change-Id: I4db02ad554ffb3d4594a3a46d47f66b6e1c6f6fe
    Signed-off-by: Liem Nguyen <liem_m_nguyen@hp.com>

--- aaa-authn-sts/src/main/java/org/opendaylight/aaa/sts/Activator.java
+++ aaa-authn-sts/src/main/java/org/opendaylight/aaa/sts/Activator.java
@@ -8,14 +8,15 @@
  */
 package org.opendaylight.aaa.sts;
 
-import org.apache.felix.dm.Component;
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
 import org.opendaylight.aaa.api.AuthenticationService;
 import org.opendaylight.aaa.api.ClaimAuth;
 import org.opendaylight.aaa.api.CredentialAuth;
 import org.opendaylight.aaa.api.IdMService;
 import org.opendaylight.aaa.api.TokenAuth;
 import org.opendaylight.aaa.api.TokenStore;
-import org.opendaylight.controller.sal.core.ComponentActivatorAbstractBase;
+import org.osgi.framework.BundleContext;
 
 /**
  * An activator for the secure token server to inject in a
@@ -24,30 +25,31 @@ import org.opendaylight.controller.sal.core.ComponentActivatorAbstractBase;
  * @author liemmn
  *
  */
-public class Activator extends ComponentActivatorAbstractBase {
-    @Override
-    public Object[] getImplementations() {
-        Object[] res = { ServiceLocator.INSTANCE };
-        return res;
-    }
+public class Activator extends DependencyActivatorBase {
+	@Override
+	public void init(BundleContext context, DependencyManager manager)
+			throws Exception {
+		manager.add(createComponent()
+				.setImplementation(ServiceLocator.INSTANCE)
+				.add(createServiceDependency().setService(
+                    CredentialAuth.class).setRequired(true))
+                .add(createServiceDependency().setService(ClaimAuth.class)
+                	.setRequired(false)
+                	.setCallbacks("claimAuthAdded", "claimAuthRemoved"))
+                .add(createServiceDependency().setService(TokenAuth.class)
+                	.setRequired(false)
+                	.setCallbacks("tokenAuthAdded", "tokenAuthRemoved"))
+                .add(createServiceDependency().setService(
+                    TokenStore.class).setRequired(true))
+                .add(createServiceDependency().setService(
+                	AuthenticationService.class).setRequired(true))
+                .add(createServiceDependency().setService(
+                	IdMService.class).setRequired(true)));
+	}
 
-    @Override
-    public void configureInstance(Component c, Object imp, String container) {
-        if (imp.equals(ServiceLocator.INSTANCE)) {
-            c.add(createContainerServiceDependency(container).setService(
-                    CredentialAuth.class).setRequired(true));
-            c.add(createContainerServiceDependency(container)
-                    .setService(ClaimAuth.class).setRequired(false)
-                    .setCallbacks("claimAuthAdded", "claimAuthRemoved"));
-            c.add(createContainerServiceDependency(container)
-                    .setService(TokenAuth.class).setRequired(false)
-                    .setCallbacks("tokenAuthAdded", "tokenAuthRemoved"));
-            c.add(createContainerServiceDependency(container).setService(
-                    TokenStore.class).setRequired(true));
-            c.add(createContainerServiceDependency(container).setService(
-                    AuthenticationService.class).setRequired(true));
-            c.add(createContainerServiceDependency(container).setService(
-                    IdMService.class).setRequired(true));
-        }
-    }
+	@Override
+	public void destroy(BundleContext context, DependencyManager manager)
+			throws Exception {
+	}
+
 }
