c803cb08547045175aaf8c1a36f0818608d0975c
Liem Nguyen <liem_m_nguyen@hp.com>
2014-07-24 08:19:41 -0700

    Added unit tests
    Change-Id: I11d87244662bd21a164f48716be23f532fd1c718
    Signed-off-by: Liem Nguyen <liem_m_nguyen@hp.com>

--- aaa-authn-sts/src/test/java/org/opendaylight/aaa/sts/TokenAuthTest.java
+++ aaa-authn-sts/src/test/java/org/opendaylight/aaa/sts/TokenAuthTest.java
@@ -10,21 +10,47 @@ package org.opendaylight.aaa.sts;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.anyMap;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.opendaylight.aaa.AuthenticationBuilder;
+import org.opendaylight.aaa.api.Authentication;
+import org.opendaylight.aaa.api.AuthenticationService;
+import org.opendaylight.aaa.api.TokenAuth;
+import org.opendaylight.aaa.api.TokenStore;
 
 import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.test.framework.JerseyTest;
 import com.sun.jersey.test.framework.WebAppDescriptor;
 
-public class TokenAuthTest extends AbstractAuthTest {
+public class TokenAuthTest extends JerseyTest {
 
     private static final String RS_PACKAGES = "org.opendaylight.aaa.sts";
     private static final String JERSEY_FILTERS = "com.sun.jersey.spi.container.ContainerRequestFilters";
     private static final String AUTH_FILTERS = TokenAuthFilter.class.getName();
 
+    private static Authentication auth = new AuthenticationBuilder()
+            .setUserId("1234").setUser("Bob").addRole("admin").addRole("user")
+            .setDomain("tenantX")
+            .setExpiration(System.currentTimeMillis() + 1000).build();
+
+    private static final String GOOD_TOKEN = "9b01b7cf-8a49-346d-8c47-6a61193e2b60";
+    private static final String BAD_TOKEN = "9b01b7cf-8a49-346d-8c47-6a611badbeef";
+
     public TokenAuthTest() throws Exception {
         super(new WebAppDescriptor.Builder(RS_PACKAGES).initParam(
                 JERSEY_FILTERS, AUTH_FILTERS).build());
+    }
+
+    @BeforeClass
+    public static void init() {
+        ServiceLocator.INSTANCE.as = mock(AuthenticationService.class);
+        ServiceLocator.INSTANCE.ts = mock(TokenStore.class);
+        when(ServiceLocator.INSTANCE.ts.get(GOOD_TOKEN)).thenReturn(auth);
+        when(ServiceLocator.INSTANCE.ts.get(BAD_TOKEN)).thenReturn(null);
     }
 
     @Test()
@@ -43,6 +69,20 @@ public class TokenAuthTest extends AbstractAuthTest {
                 .header("Authorization", "Bearer " + GOOD_TOKEN)
                 .get(String.class);
         assertEquals("ok", resp);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testGetWithValidator() {
+        try {
+            // Mock a laxed tokenauth...
+            TokenAuth ta = mock(TokenAuth.class);
+            when(ta.validate(anyMap())).thenReturn(auth);
+            ServiceLocator.INSTANCE.ta.add(ta);
+            testGet();
+        } finally {
+            ServiceLocator.INSTANCE.ta.clear();
+        }
     }
 
 }
