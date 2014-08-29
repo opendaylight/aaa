e623c9d64676225b312e69516f284fd308249fd2
Wojciech Dec <wdec@cisco.com>
2014-08-29 20:32:33 +0200

    Working AuthZ Broker (DOM Data only) + config files  AuthZ service still needs to be invoked as noted in TODO
    Change-Id: I084926f9c8518e865527be4dafdcd0c3effc5340
    Signed-off-by: Wojciech Dec <wdec@cisco.com>

---
+++ aaa-authz/aaa-authz-service/src/main/java/org/opendaylight/aaa/authz/srv/AuthzReadOnlyTransaction.java
@@ -0,0 +1,42 @@
+package org.opendaylight.aaa.authz.srv;
+
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.CheckedFuture;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.controller.md.sal.dom.api.DOMDataReadOnlyTransaction;
+import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
+import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
+
+/**
+ * Created by wdec on 28/08/2014.
+ */
+
+public class AuthzReadOnlyTransaction implements DOMDataReadOnlyTransaction {
+
+  private final DOMDataReadOnlyTransaction ro;
+  public AuthzReadOnlyTransaction(DOMDataReadOnlyTransaction ro) {
+    this.ro = ro;
+  }
+
+  @Override
+  public void close() {
+    ro.close();
+  }
+
+  @Override
+  public CheckedFuture<Optional<NormalizedNode<?, ?>>, ReadFailedException> read(LogicalDatastoreType logicalDatastoreType, YangInstanceIdentifier yangInstanceIdentifier) {
+    //TODO: Do AuthZ check here.
+    return ro.read(logicalDatastoreType, yangInstanceIdentifier);
+  }
+
+  @Override
+  public CheckedFuture<Boolean, ReadFailedException> exists(LogicalDatastoreType logicalDatastoreType, YangInstanceIdentifier yangInstanceIdentifier) {
+    return ro.exists(logicalDatastoreType, yangInstanceIdentifier);
+  }
+
+  @Override
+  public Object getIdentifier() {
+    return ro.getIdentifier();
+  }
+}
