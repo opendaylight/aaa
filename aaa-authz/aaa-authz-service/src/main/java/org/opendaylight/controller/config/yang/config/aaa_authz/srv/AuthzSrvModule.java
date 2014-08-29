2c61c7ac5be8ebf4ea6c3ff8c09d9f7d49d063e3
Wojciech Dec <wdec@cisco.com>
2014-08-29 20:32:33 +0200

    Working AuthZ Broker (DOM Data only) + config files  AuthZ service still needs to be invoked as noted in TODO
    Change-Id: I084926f9c8518e865527be4dafdcd0c3effc5340
    Signed-off-by: Wojciech Dec <wdec@cisco.com>

---
+++ aaa-authz/aaa-authz-service/src/main/java/org/opendaylight/controller/config/yang/config/aaa_authz/srv/AuthzSrvModule.java
@@ -0,0 +1,58 @@
+package org.opendaylight.controller.config.yang.config.aaa_authz.srv;
+
+import org.opendaylight.aaa.api.AuthenticationService;
+import org.opendaylight.aaa.authz.srv.AuthzBrokerImpl;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AuthzSrvModule extends org.opendaylight.controller.config.yang.config.aaa_authz.srv.AbstractAuthzSrvModule {
+  private static final Logger log = LoggerFactory.getLogger(AuthzSrvModule.class);
+  private static boolean simple_config_switch;
+  private BundleContext bundleContext;
+
+  public AuthzSrvModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public AuthzSrvModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.controller.config.yang.config.aaa_authz.srv.AuthzSrvModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+      // add custom validation form module attributes here.
+      }
+
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+
+      //Get new AuthZ Broker
+      final AuthzBrokerImpl authzBrokerImpl = new AuthzBrokerImpl();
+
+      //Provide real broker to the new Authz broker
+      authzBrokerImpl.setBroker(getDomBrokerDependency());
+
+      //Get AuthN service reference and register it with the authzBroker
+      ServiceReference<AuthenticationService> authServiceReference = bundleContext.getServiceReference(AuthenticationService.class);
+      AuthenticationService as = bundleContext.getService(authServiceReference);
+      authzBrokerImpl.setAuthenticationService(as);
+
+      // Register AuthZ broker with the real Broker as a provider; triggers "onSessionInitiated" in AuthzBrokerImpl
+      getDomBrokerDependency().registerProvider(authzBrokerImpl);
+      getAction();
+
+
+      log.info("AuthZ Service Initialized from Config subsystem");
+      return authzBrokerImpl;
+
+
+    }
+
+  public void setBundleContext(BundleContext bundleContext) {
+    this.bundleContext = bundleContext;
+  }
+}
+
