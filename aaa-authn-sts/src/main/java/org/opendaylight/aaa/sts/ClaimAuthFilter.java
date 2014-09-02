f879c2bf4c835b642f272a4c77f8129aff67ac82
John Dennis <jdennis@redhat.com>
2014-09-02 16:48:57 -0400

    Remove some captured values, add comment, add logging
    CGI_CONTENT_TYPE and CGI_CONTENT_LENGTH are not relevant to an
    authentication claim, so remove them.
    
    Reorder the CGI variable name declarations so they are sorted. Makes
    it easier to cross check their usage when things are in the same
    order.
    
    Remove the wholesale collection of HTTP headers since in general
    they cannot be trusted, but retain the selected capture of specific
    HTTP headers configured by an admin.
    
    Add comment explaining why getAttributeNames() has problems and how we
    adopt.
    
    Add logger and then log the claims map.
    
    Change-Id: I19a274601eceb5e2f24a3c055e9c73d4bb52e9b9
    Signed-off-by: John Dennis <jdennis@redhat.com>

--- aaa-authn-sts/src/main/java/org/opendaylight/aaa/sts/ClaimAuthFilter.java
+++ aaa-authn-sts/src/main/java/org/opendaylight/aaa/sts/ClaimAuthFilter.java
@@ -23,6 +23,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.opendaylight.aaa.api.Claim;
 import org.opendaylight.aaa.api.ClaimAuth;
 
@@ -33,18 +35,20 @@ import org.opendaylight.aaa.api.ClaimAuth;
  *
  */
 public class ClaimAuthFilter implements Filter {
-    private static final String CGI_SERVER_PROTOCOL = "SERVER_PROTOCOL";
-    private static final String CGI_CONTENT_TYPE = "CONTENT_TYPE";
-    private static final String CGI_CONTENT_LENGTH = "CONTENT_LENGTH";
-    private static final String CGI_SCRIPT_NAME = "SCRIPT_NAME";
-    private static final String CGI_REMOTE_HOST = "REMOTE_HOST";
-    private static final String CGI_REMOTE_ADDR = "REMOTE_ADDR";
-    private static final String CGI_REMOTE_USER = "REMOTE_USER";
-    private static final String CGI_QUERY_STRING = "QUERY_STRING";
-    private static final String CGI_PATH_TRANSLATED = "PATH_TRANSLATED";
-    private static final String CGI_PATH_INFO = "PATH_INFO";
-    private static final String CGI_REQUEST_METHOD = "REQUEST_METHOD";
+    private static final Logger logger =
+        LoggerFactory.getLogger(ClaimAuthFilter.class);
+
     private static final String CGI_AUTH_TYPE = "AUTH_TYPE";
+    private static final String CGI_PATH_INFO = "PATH_INFO";
+    private static final String CGI_PATH_TRANSLATED = "PATH_TRANSLATED";
+    private static final String CGI_QUERY_STRING = "QUERY_STRING";
+    private static final String CGI_REMOTE_ADDR = "REMOTE_ADDR";
+    private static final String CGI_REMOTE_HOST = "REMOTE_HOST";
+    private static final String CGI_REMOTE_PORT = "REMOTE_PORT";
+    private static final String CGI_REMOTE_USER = "REMOTE_USER";
+    private static final String CGI_REQUEST_METHOD = "REQUEST_METHOD";
+    private static final String CGI_SCRIPT_NAME = "SCRIPT_NAME";
+    private static final String CGI_SERVER_PROTOCOL = "SERVER_PROTOCOL";
 
     @Override
     public void init(FilterConfig fc) throws ServletException {
@@ -72,6 +76,31 @@ public class ClaimAuthFilter implements Filter {
     private Map<String, Object> claims(HttpServletRequest req) {
         Map<String, Object> claims = new HashMap<>();
 
+        /*
+         * Tomcat has a bug/feature, not all attributes are enumerated
+         * by getAttributeNames() therefore getAttributeNames() cannot
+         * be used to obtain the full set of attributes. However if
+         * you know the name of the attribute a priori you can call
+         * getAttribute() and obtain the value. Therefore we maintain
+         * a list of attribute names (httpAttributes) which will be
+         * used to call getAttribute() with so we don't miss essential
+         * attributes.
+         *
+         * This is the Tomcat bug, note it is marked WONTFIX.
+         * Bug 25363 - request.getAttributeNames() not working properly
+         * Status: RESOLVED WONTFIX
+         * https://issues.apache.org/bugzilla/show_bug.cgi?id=25363
+         *
+         * The solution adopted by Tomcat is to document the behavior
+         * in the "The Apache Tomcat Connector - Reference Guide"
+         * under the JkEnvVar property where is says:
+         *
+         * You can retrieve the variables on Tomcat as request
+         * attributes via request.getAttribute(attributeName). Note
+         * that the variables send via JkEnvVar will not be listed in
+         * request.getAttributeNames().
+         */
+
         // Capture attributes...
         @SuppressWarnings("unchecked")
         Enumeration<String> attrs = req.getAttributeNames();
@@ -94,23 +123,27 @@ public class ClaimAuthFilter implements Filter {
         }
 
         // Capture custom headers...
+        // See above comment concerning getAttributeNames() enumeration
         for (String header : FederationConfiguration.instance().httpHeaders()) {
             claims.put(header, req.getHeader(header));
         }
 
         // Capture standard CGI variables...
         claims.put(CGI_AUTH_TYPE, req.getAuthType());
-        claims.put(CGI_REQUEST_METHOD, req.getMethod());
         claims.put(CGI_PATH_INFO, req.getPathInfo());
         claims.put(CGI_PATH_TRANSLATED, req.getPathTranslated());
         claims.put(CGI_QUERY_STRING, req.getQueryString());
-        claims.put(CGI_REMOTE_USER, req.getRemoteUser());
         claims.put(CGI_REMOTE_ADDR, req.getRemoteAddr());
         claims.put(CGI_REMOTE_HOST, req.getRemoteHost());
+        claims.put(CGI_REMOTE_PORT, req.getRemotePort());
+        claims.put(CGI_REMOTE_USER, req.getRemoteUser());
+        claims.put(CGI_REQUEST_METHOD, req.getMethod());
         claims.put(CGI_SCRIPT_NAME, req.getServletPath());
-        claims.put(CGI_CONTENT_LENGTH, req.getContentLength());
-        claims.put(CGI_CONTENT_TYPE, req.getContentType());
         claims.put(CGI_SERVER_PROTOCOL, req.getProtocol());
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("ClaimAuthFilter claims = " + claims.toString());
+        }
 
         return claims;
     }
