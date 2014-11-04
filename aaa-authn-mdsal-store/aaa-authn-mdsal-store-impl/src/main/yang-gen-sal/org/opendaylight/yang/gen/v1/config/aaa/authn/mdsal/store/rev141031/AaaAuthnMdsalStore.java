3a82fb86d0818022237d6178c1cc3dadb8e32c7d
Timotej Kubas <tkubas@cisco.com>
2014-11-04 17:54:11 +0100

    Initial commit for md-sal cache
    Change-Id: I7c3945de915daed2f146a3853f8534c2d6c061bb
    Signed-off-by: Timotej Kubas <tkubas@cisco.com>

---
+++ aaa-authn-mdsal-store/aaa-authn-mdsal-store-impl/src/main/yang-gen-sal/org/opendaylight/yang/gen/v1/config/aaa/authn/mdsal/store/rev141031/AaaAuthnMdsalStore.java
@@ -0,0 +1,33 @@
+package org.opendaylight.yang.gen.v1.config.aaa.authn.mdsal.store.rev141031;
+import org.opendaylight.yangtools.yang.common.QName;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.controller.config.rev130405.ModuleType;
+
+
+/**
+ * <p>This class represents the following YANG schema fragment defined in module <b>aaa-authn-mdsal-store-cfg</b>
+ * <br />(Source path: <i>META-INF/yang/aaa-authn-mdsal-store-cfg.yang</i>):
+ * <pre>
+ * identity aaa-authn-mdsal-store {
+ *     base "()IdentitySchemaNodeImpl[base=null, qname=(urn:opendaylight:params:xml:ns:yang:controller:config?revision=2013-04-05)module-type]";
+ *     status CURRENT;
+ * }
+ * </pre>
+ * The schema path to identify an instance is
+ * <i>aaa-authn-mdsal-store-cfg/aaa-authn-mdsal-store</i>
+ */
+public abstract class AaaAuthnMdsalStore extends ModuleType
+ {
+    public static final QName QNAME = org.opendaylight.yangtools.yang.common.QName.create("config:aaa:authn:mdsal:store","2014-10-31","aaa-authn-mdsal-store");;
+    
+    public AaaAuthnMdsalStore() {
+    }
+    
+
+
+
+
+
+
+
+
+}
