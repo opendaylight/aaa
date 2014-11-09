b76355c814f1bad9b00b34af6668163bcd5bd765
Mark Mozolewski <mark.mozolewski@hp.com>
2014-11-08 17:10:24 -0800

    Object-store API. (DO NOT MERGE - yet) The API provides a generic DAO layer to abstract the object model from the datastore layer, which can be either an RDBMS, NoSQL, or other stores for HA and scalability.
    Change-Id: I371440e21da573d7c9714a24b336766ca1b63dba
    Signed-off-by: Liem Nguyen <liem_m_nguyen@hp.com>
    Signed-off-by: mark.mozolewski <mark.mozolewski@hp.com>

---
+++ aaa-persistence-api/src/main/java/org/opendaylight/aaa/persistence/api/Queryable.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P. and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.persistence.api;
+
+/**
+ * An interface to denote that an object can be queried.
+ * 
+ * @author liemmn
+ * @author Mark Mozolewski
+ *
+ */
+public interface Queryable extends Orderable, Restrictable {
+
+}
---
+++ aaa-persistence-api/src/test/java/org/opendaylight/aaa/persistence/HammerStore.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P. and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.persistence;
+
+import org.opendaylight.aaa.persistence.api.Criteria;
+import org.opendaylight.aaa.persistence.api.ObjectStore;
+import org.opendaylight.aaa.persistence.api.Page;
+import org.opendaylight.aaa.persistence.api.Pageable;
+
+public class HammerStore implements ObjectStore<Hammer, Long> {
+
+    @Override
+    public <S extends Hammer> S save(S object) {
+        return object;
+    }
+
+    @Override
+    public <S extends Hammer> Iterable<S> save(Iterable<S> objects) {
+        return objects;
+    }
+
+    @Override
+    public Hammer findById(Long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Iterable<Hammer> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Page<Hammer, Long> findAll(Pageable<Long> p) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Iterable<Hammer> findAll(Criteria c) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Long count() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Long count(Criteria c) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void delete(Hammer object) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Long deleteAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Long deleteAll(Criteria c) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean exists(Long id) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Long deleteAll(Iterable<? extends Hammer> objects) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Page<Hammer, Long> findAll(Pageable<Long> p, Criteria c) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
