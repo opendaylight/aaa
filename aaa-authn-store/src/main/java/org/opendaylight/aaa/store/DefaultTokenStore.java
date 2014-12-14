28e6d7cd4eb8ffafcfb7b33b1218cb7464e18855
Thanh Ha <thanh.ha@linuxfoundation.org>
2014-12-14 15:18:35 -0500

    Fix checkstyle if-statements must use braces in aaa-authn-store
    - Fix checkstyle if-statement brances
    - Fix missing License header
    
    Change-Id: Iac67a50e459b363fd3b83f56011028d82b828c62
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- aaa-authn-store/src/main/java/org/opendaylight/aaa/store/DefaultTokenStore.java
+++ aaa-authn-store/src/main/java/org/opendaylight/aaa/store/DefaultTokenStore.java
@@ -1,3 +1,11 @@
+/*****************************************************************************
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P. and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *****************************************************************************/
 package org.opendaylight.aaa.store;
 
 import java.io.File;
@@ -118,9 +126,10 @@ public class DefaultTokenStore implements TokenStore, ManagedService {
     public void updated(@SuppressWarnings("rawtypes") Dictionary props)
             throws ConfigurationException {
         logger.info("Updating token store configuration...");
-        if (props == null)
+        if (props == null) {
             // Someone deleted the configuration, use defaults
             props = defaults;
+        }
         reconfig(props);
     }
 
