385e70283a8f9ed5367d4ea36a942a02babefe06
Kailash Khalasi <kailash.khalasi@hp.com>
2015-03-20 14:32:26 -0700

    Testing ODL Gerrit and Git environment
    Change-Id: I0830ce9be903851eb59a3304ed317483237d0d5c
    Signed-off-by: Kailash Khalasi <kailash.khalasi@hp.com>

--- aaa-authn-keystone/src/main/java/org/opendaylight/aaa/keystone/KeystoneTokenAuth.java
+++ aaa-authn-keystone/src/main/java/org/opendaylight/aaa/keystone/KeystoneTokenAuth.java
@@ -30,7 +30,7 @@ public class KeystoneTokenAuth implements TokenAuth {
     @Override
     public Authentication validate(Map<String, List<String>> headers) {
         if (!headers.containsKey(TOKEN)) {
-            return null;    // Not a Keystone token
+            return null;    // It's Not a Keystone token
         }
 
         // TODO: Call into Keystone to get security context...
