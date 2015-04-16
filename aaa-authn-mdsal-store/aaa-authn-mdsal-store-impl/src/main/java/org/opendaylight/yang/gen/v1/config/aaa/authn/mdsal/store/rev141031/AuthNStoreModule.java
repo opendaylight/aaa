f2394c8dc7cb5343e2ce05cdd9bdcd879a5cc5c7
Wojciech Dec <wdec@cisco.com>
2015-04-16 14:44:29 +0200

    Implementation of MD-SAL Token Cache
    Change-Id: I7d966b52edc1e6d62860285630ee3e215917b26c
    Signed-off-by: Wojciech Dec <wdec@cisco.com>

--- aaa-authn-mdsal-store/aaa-authn-mdsal-store-impl/src/main/java/org/opendaylight/yang/gen/v1/config/aaa/authn/mdsal/store/rev141031/AuthNStoreModule.java
+++ aaa-authn-mdsal-store/aaa-authn-mdsal-store-impl/src/main/java/org/opendaylight/yang/gen/v1/config/aaa/authn/mdsal/store/rev141031/AuthNStoreModule.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+
 package org.opendaylight.yang.gen.v1.config.aaa.authn.mdsal.store.rev141031;
+
+import org.opendaylight.aaa.api.TokenStore;
+import org.opendaylight.aaa.authn.mdsal.store.AuthNStore;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.osgi.framework.BundleContext;
+
 public class AuthNStoreModule extends org.opendaylight.yang.gen.v1.config.aaa.authn.mdsal.store.rev141031.AbstractAuthNStoreModule {
+    private BundleContext bundleContext;
+
     public AuthNStoreModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
         super(identifier, dependencyResolver);
     }
@@ -15,8 +32,43 @@ public class AuthNStoreModule extends org.opendaylight.yang.gen.v1.config.aaa.au
 
     @Override
     public java.lang.AutoCloseable createInstance() {
-        // TODO:implement
-        throw new java.lang.UnsupportedOperationException();
+
+        DataBroker dataBrokerService = getDataBrokerDependency();
+        final AuthNStore authNStore = new AuthNStore(dataBrokerService, getPassword());
+
+        authNStore.setTimeToLive(getTimeToLive());
+
+        //Register the MD-SAL Token store with OSGI
+        bundleContext.registerService(TokenStore.class.getName(), authNStore, null);
+
+        final class AutoCloseableStore implements AutoCloseable {
+
+            @Override
+            public void close() throws Exception {
+                authNStore.close();
+            }
+        }
+
+
+        return new AutoCloseableStore();
+
+//        return authNStore;
+
+//        throw new java.lang.UnsupportedOperationException();
+    }
+
+    /**
+     * @param bundleContext
+     */
+    public void setBundleContext(BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
+
+    /**
+     * @return the bundleContext
+     */
+    public BundleContext getBundleContext() {
+        return bundleContext;
     }
 
 }
