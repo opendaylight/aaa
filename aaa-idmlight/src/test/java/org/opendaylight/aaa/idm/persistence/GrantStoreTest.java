ec4e5f54c84a3b40c4c73a518a881b5fea1d52e9
Kailash Khalasi <kailash.khalasi@hp.com>
2015-04-16 15:01:12 -0700

    Adding unit tests to idmlite
    Change-Id: I5ebd86085cf3e1ac88843c656f10480203879da4
    Signed-off-by: Kailash Khalasi <kailash.khalasi@hp.com>

---
+++ aaa-idmlight/src/test/java/org/opendaylight/aaa/idm/persistence/GrantStoreTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2014-2015 Hewlett-Packard Development Company, L.P. and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.idm.persistence;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.*;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.opendaylight.aaa.idm.model.Grants;
+
+public class GrantStoreTest {
+
+    Connection connectionMock = mock(Connection.class);
+    private final GrantStore GrantStoreUnderTest = new GrantStore();
+    private Long did=(long) 5;
+    private Long uid=(long) 5;
+
+
+    @Before
+    public void setup() {
+        GrantStoreUnderTest.dbConnection = connectionMock;
+    }
+
+    @After
+    public void teardown() {
+        //dts.destroy();
+    }
+
+    @Test
+    public void getGrantsTest() throws SQLException, Exception {
+        //Setup Mock Behavior
+	Mockito.when(connectionMock.isClosed()).thenReturn(false);
+	DatabaseMetaData dbmMock = mock(DatabaseMetaData.class);
+	Mockito.when(connectionMock.getMetaData()).thenReturn(dbmMock);
+	ResultSet rsUserMock = mock(ResultSet.class);
+	Mockito.when(dbmMock.getTables(null,null,"grants",null)).thenReturn(rsUserMock);
+	Mockito.when(rsUserMock.next()).thenReturn(true);
+
+	Statement stmtMock = mock(Statement.class);
+	Mockito.when(connectionMock.createStatement()).thenReturn(stmtMock);
+
+	ResultSet rsMock = getMockedResultSet();
+	Mockito.when(stmtMock.executeQuery(anyString())).thenReturn(rsMock);
+
+	//Run Test
+	Grants grants = GrantStoreUnderTest.getGrants(did,uid);
+
+	//Verify
+	assertTrue(grants.getGrants().size() == 1);
+	verify(stmtMock).close();
+
+	}
+
+    public ResultSet getMockedResultSet(){
+        ResultSet rsMock = mock(ResultSet.class);
+	try {
+	    Mockito.when(rsMock.next()).thenReturn(true).thenReturn(false);
+	    Mockito.when(rsMock.getInt(GrantStore.SQL_ID)).thenReturn(1);
+	    Mockito.when(rsMock.getString(GrantStore.SQL_DESCR)).thenReturn("RoleofTenantUser_1");
+	    Mockito.when(rsMock.getLong(GrantStore.SQL_TENANTID)).thenReturn(did);
+	    Mockito.when(rsMock.getLong(GrantStore.SQL_USERID)).thenReturn(uid);
+	    Mockito.when(rsMock.getString(GrantStore.SQL_ROLEID)).thenReturn("Role_1");
+	} 
+        catch (SQLException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+
+	return rsMock;
+	}
+
+
+}
+
+
