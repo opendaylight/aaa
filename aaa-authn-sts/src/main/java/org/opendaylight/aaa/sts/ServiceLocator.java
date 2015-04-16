e8b02275b1820b6a5e80bee649af1e315e90a663
Wojciech Dec <wdec@cisco.com>
2015-04-16 14:44:29 +0200

    Implementation of MD-SAL Token Cache
    Change-Id: I7d966b52edc1e6d62860285630ee3e215917b26c
    Signed-off-by: Wojciech Dec <wdec@cisco.com>

--- aaa-authn-sts/src/main/java/org/opendaylight/aaa/sts/ServiceLocator.java
+++ aaa-authn-sts/src/main/java/org/opendaylight/aaa/sts/ServiceLocator.java
@@ -8,9 +8,6 @@
  */
 package org.opendaylight.aaa.sts;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import org.opendaylight.aaa.api.AuthenticationService;
 import org.opendaylight.aaa.api.ClientService;
 import org.opendaylight.aaa.api.CredentialAuth;
@@ -18,6 +15,9 @@ import org.opendaylight.aaa.api.IdMService;
 import org.opendaylight.aaa.api.PasswordCredentials;
 import org.opendaylight.aaa.api.TokenAuth;
 import org.opendaylight.aaa.api.TokenStore;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * A service locator to bridge between the web world and OSGi world.
@@ -47,5 +47,11 @@ public enum ServiceLocator {
     protected void tokenAuthRemoved(TokenAuth ta) {
         this.ta.remove(ta);
     }
+    protected void tokenStoreAdded(TokenStore ts) {
+        this.ts = ts;
+    }
 
+    protected void tokenStoreRemoved(TokenStore ts) {
+        this.ts = null;
+    }
 }
