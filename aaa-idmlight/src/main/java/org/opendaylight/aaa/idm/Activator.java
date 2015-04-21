62dc5b8ae9c089c85899d2669209007c75cc20ec
EduardoPerez <eduardo.perez2@hp.com>
2015-04-21 15:56:18 -0700

    Initial code to make H2 Database properties configurable
    DO NOT CHECKIN.  NEEDS REVIEW
    
    Change-Id: Ic7c0fb40c4889ab64a06354087d79714451c0922
    Signed-off-by: EduardoPerez <eduardo.perez2@hp.com>

--- aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/Activator.java
+++ aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/Activator.java
@@ -8,11 +8,17 @@
  */
 package org.opendaylight.aaa.idm;
 
+import java.util.Dictionary;
+
 import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
+import org.opendaylight.aaa.ClientManager;
 import org.opendaylight.aaa.api.CredentialAuth;
 import org.opendaylight.aaa.api.IdMService;
+import org.opendaylight.aaa.idm.config.IdmLightConfig;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.service.cm.ManagedService;
 
 /**
  * An activator to publish the {@link CredentialAuth} implementation provided by
@@ -23,6 +29,8 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends DependencyActivatorBase {
 
+	private static final String AAA_IDM_LIGHT_PID = "org.opendaylight.aaa.idm";
+	
     @Override
     public void init(BundleContext context, DependencyManager manager)
             throws Exception {
@@ -30,10 +38,18 @@ public class Activator extends DependencyActivatorBase {
                 new String[] { CredentialAuth.class.getName(),
                         IdMService.class.getName() }, null).setImplementation(
                 IdmLightProxy.class));
+        //Register properties
+        context.registerService(ManagedService.class.getName(), IdmLightConfig.getInstance(),
+                addPid(IdmLightConfig.defaults));
     }
 
     @Override
     public void destroy(BundleContext context, DependencyManager manager)
             throws Exception {
+    }
+    
+    private Dictionary<String, ?> addPid(Dictionary<String, String> dict) {
+        dict.put(Constants.SERVICE_PID, AAA_IDM_LIGHT_PID);
+        return dict;
     }
 }
--- aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/config/IdmLightConfig.java
+++ aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/config/IdmLightConfig.java
@@ -14,26 +14,47 @@ package org.opendaylight.aaa.idm.config;
  *
  */
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class IdmLightConfig {
+public class IdmLightConfig implements ManagedService{
    private static Logger logger = LoggerFactory.getLogger(IdmLightConfig.class);
 
+   private final static String DB_PATH_KEY = "db.file.path";
+   private final static String DB_USER_KEY = "db.user";
+   private final static String DB_PSWD_KEY = "db.pswd";
+   
+   private IdmLightConfig(){
+   } 
+   
+   private static IdmLightConfig IDMLIGHTINSTANCE = new IdmLightConfig();
+   
+   public static IdmLightConfig getInstance(){
+	   return IDMLIGHTINSTANCE;
+   }
+   
+   public static final Dictionary<String, String> defaults = new Hashtable<>();
+   static {
+       defaults.put(DB_PATH_KEY, "~/h2");
+       defaults.put(DB_USER_KEY, "foo");
+       defaults.put(DB_PSWD_KEY, "bar");
+   }
+   
    public static String dbName;
    public String dbPath;
    public String dbDriver;
    public String dbUser;
    public String dbPwd;
    public int dbValidTimeOut;
-
+   
    public boolean load() {
       dbName = "idmlight.db";
-      //TODO make configurable
-      dbPath = "jdbc:h2:./" + dbName;
       dbDriver = "org.h2.Driver";
-      dbUser = "foo";
-      dbPwd = "bar";
       dbValidTimeOut = 3;
 
       return true;
@@ -43,6 +64,30 @@ public class IdmLightConfig {
       logger.info("DB Path                 : " + dbPath);
       logger.info("DB Driver               : " + dbDriver);
       logger.info("DB Valid Time Out       : " + dbValidTimeOut);
+   }
+
+   @Override
+   public void updated(Dictionary<String, ?> properties)
+		   throws ConfigurationException {
+	  if (properties == null) {
+		   return;
+	  }
+			
+	  String pathProp = (String) properties.get(DB_PATH_KEY);
+	  String userProp = (String) properties.get(DB_USER_KEY);
+	  String pwdProp = (String) properties.get(DB_PSWD_KEY);
+
+	  if(null == dbPath || dbPath.isEmpty()){
+		  throw new ConfigurationException(DB_PATH_KEY, "DB Path Field empty/null");
+	  }else if(null == dbUser || dbUser.isEmpty()){
+		  throw new ConfigurationException(DB_USER_KEY, "DB User Field empty/null");
+	  }else if(null == dbPwd || dbPwd.isEmpty()){
+		  throw new ConfigurationException(DB_PSWD_KEY, "DB Pwd Field empty/null");
+	  }
+	  
+	  dbPath = pathProp;
+	  dbUser = userProp;
+	  dbPwd = pwdProp;
    }
 
 }
