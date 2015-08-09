2f39fb42adcbbc878871d26ce07facf645c604c3
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-09 01:55:38 -0400

    Fix license header violations in aaa-authn-store
    Change-Id: I60b24c366231dcd8f0b94e21fbe54c804a68b4fb
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- aaa-authn-mdsal-store/aaa-authn-mdsal-store-impl/src/main/java/org/opendaylight/aaa/authn/mdsal/store/AuthNStore.java
+++ aaa-authn-mdsal-store/aaa-authn-mdsal-store-impl/src/main/java/org/opendaylight/aaa/authn/mdsal/store/AuthNStore.java
@@ -4,11 +4,9 @@
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
- *
  */
 
 package org.opendaylight.aaa.authn.mdsal.store;
-
 
 import com.google.common.base.Optional;
 import com.google.common.util.concurrent.CheckedFuture;
