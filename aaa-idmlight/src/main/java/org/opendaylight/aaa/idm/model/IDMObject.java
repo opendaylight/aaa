889a834925c4d13497e60b0d48f9db931dd00e7c
Sharon Aicler <saichler@cisco.com>
2015-09-02 14:08:53 -0700

    AAA IDM MD-SAL Store
    Change-Id: I49e76b3d85708a1926a0247aed6156d25708cc93
    Signed-off-by: Sharon Aicler <saichler@cisco.com>

---
+++ aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/model/IDMObject.java
@@ -0,0 +1,76 @@
+package org.opendaylight.aaa.idm.model;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.opendaylight.yangtools.yang.binding.DataObject;
+
+public abstract class IDMObject {
+    private static Map<Class<?>,ConvertionMethods> typesMethods = new HashMap<Class<?>,ConvertionMethods>();
+    public static Object fromMDSALObject(Object mdsalObject,Class<?> type) throws Exception{
+        Object result = type.newInstance();
+        ConvertionMethods cm = typesMethods.get(type);
+        if(cm==null){
+            cm = new ConvertionMethods();
+            typesMethods.put(type, cm);
+            Method methods[] = type.getMethods();
+            for(Method m:methods){
+                if(m.getName().startsWith("set")){
+                    cm.setMethods.add(m);
+                    Method gm = null;
+                    if(m.getParameterTypes()[0].equals(Boolean.class) || m.getParameterTypes()[0].equals(boolean.class))
+                        gm = ((DataObject)mdsalObject).getImplementedInterface().getMethod("is"+m.getName().substring(3), (Class[])null);
+                    else
+                        gm = ((DataObject)mdsalObject).getImplementedInterface().getMethod("get"+m.getName().substring(3), (Class[])null);
+                    cm.getMethods.put(m.getName(), gm);
+                }
+            }
+        }
+        for(Method m:cm.setMethods){
+            try{
+                m.invoke(result, new Object[]{cm.getMethods.get(m.getName()).invoke(mdsalObject, (Object[])null)});
+            }catch(Exception err){
+                err.printStackTrace();
+            }
+        }
+        return result;
+    }
+    
+    public static Object toMDSALObject(Object object,Class<?> mdSalBuilderType) throws Exception{
+        Object result = mdSalBuilderType.newInstance();
+        ConvertionMethods cm = typesMethods.get(mdSalBuilderType);
+        if(cm==null){
+            cm = new ConvertionMethods();
+            typesMethods.put(mdSalBuilderType, cm);
+            Method methods[] = mdSalBuilderType.getMethods();
+            for(Method m:methods){
+                if(m.getName().startsWith("set")){
+                    try{
+                        Method gm = null;
+                        if(m.getParameterTypes()[0].equals(Boolean.class) || m.getParameterTypes()[0].equals(boolean.class))
+                            gm = object.getClass().getMethod("is"+m.getName().substring(3), (Class[])null);
+                        else
+                            gm = object.getClass().getMethod("get"+m.getName().substring(3), (Class[])null);
+                        cm.getMethods.put(m.getName(), gm);
+                        cm.setMethods.add(m);
+                    }catch(NoSuchMethodException err){}
+                }
+            }
+            cm.builderMethod = mdSalBuilderType.getMethod("build",(Class[])null);
+        }
+        for(Method m:cm.setMethods){
+            m.invoke(result, new Object[]{cm.getMethods.get(m.getName()).invoke(object, (Object[])null)});
+        }
+
+        return cm.builderMethod.invoke(result, (Object[])null);
+    }
+    
+    private static class ConvertionMethods {
+        private List<Method> setMethods = new ArrayList<Method>();
+        private Map<String,Method> getMethods = new HashMap<String,Method>();
+        private Method builderMethod = null;
+    }
+}
