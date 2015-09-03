60d3235f9017e542c2c8c667c486dbe7e6766612
Ryan Goulding <ryandgoulding@gmail.com>
2015-09-03 11:54:24 -0400

    DO NOT MERGE- Initial cut at an odl-aaa-shiro feature
    Change-Id: Ifbdec290d87434279882295c24e35412b641154f
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

---
+++ aaa-shiro/src/test/java/org/opendaylight/aaa/shiro/web/env/KarafIniWebEnvironmentTest.java
@@ -0,0 +1,71 @@
+package org.opendaylight.aaa.shiro.web.env;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.shiro.config.Ini;
+import org.apache.shiro.config.Ini.Section;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class KarafIniWebEnvironmentTest {
+  private static File iniFile;
+  
+  @BeforeClass
+  public static void setup() throws IOException {
+    iniFile = createShiroIniFile();
+    assertTrue(iniFile.exists());
+  }
+  
+  @AfterClass
+  public static void teardown() {
+    iniFile.delete();
+  }
+
+  private static String createFakeShiroIniContents() {
+    return "[users]\n"
+        + "admin=admin, ROLE_ADMIN \n"
+        + "[roles]\n"
+        + "ROLE_ADMIN = *\n"
+        + "[urls]\n"
+        + "/** = authcBasic";
+        
+  }
+
+  private static File createShiroIniFile() throws IOException {
+    File shiroIni = File.createTempFile("shiro", "ini");
+    FileWriter writer = new FileWriter(shiroIni);
+    writer.write(createFakeShiroIniContents());
+    writer.flush();
+    writer.close();
+    return shiroIni;
+  }
+
+  @Test
+  public void testCreateShiroIni() throws IOException {
+    Ini ini = KarafIniWebEnvironment.createShiroIni(iniFile.getAbsolutePath());
+    assertNotNull(ini);
+    assertNotNull(ini.getSection("users"));
+    assertNotNull(ini.getSection("roles"));
+    assertNotNull(ini.getSection("urls"));
+    Section usersSection = ini.getSection("users");
+    assertTrue(usersSection.containsKey("admin"));
+    assertTrue(usersSection.get("admin").contains("admin"));
+    assertTrue(usersSection.get("admin").contains("ROLE_ADMIN"));
+  }
+
+  @Test
+  public void testCreateFileBasedIniPath() {
+    String testPath = "/shiro.ini";
+    String expectedFileBasedIniPath =
+        KarafIniWebEnvironment.SHIRO_FILE_PREFIX + testPath;
+    String actualFileBasedIniPath =
+        KarafIniWebEnvironment.createFileBasedIniPath(testPath);
+    assertEquals(expectedFileBasedIniPath, actualFileBasedIniPath);
+  }
+
+}
