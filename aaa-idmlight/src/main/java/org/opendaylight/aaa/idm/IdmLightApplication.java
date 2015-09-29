b12f374d1edcb04e1120b6494c9ec2903a8af9b7
Ryan Goulding <ryandgoulding@gmail.com>
2015-09-29 09:44:20 -0400

    DO NOT MERGE: WIP: Bug5760 AAA Jersey and JAX-RS upgrades
    Upgrade versions of Jersey and JAX-RS.  This is a first pass, and since the testing
    framework code has not yet been updated, the tests have been temporarily removed.
    These tests will be brought back in again before this patch is merged.
    
    This patch is here to save work off at a checkpoint.
    
    Change-Id: Ic12bfe81feb18ad00221f39e3d973978b57bb7ec
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/IdmLightApplication.java
+++ aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/IdmLightApplication.java
@@ -14,6 +14,7 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import org.eclipse.persistence.jaxb.rs.MOXyJsonProvider;
 import org.opendaylight.aaa.idm.rest.DomainHandler;
 import org.opendaylight.aaa.idm.rest.RoleHandler;
 import org.opendaylight.aaa.idm.rest.UserHandler;
@@ -48,7 +49,8 @@ public class IdmLightApplication extends Application {
 
     @Override
     public Set<Class<?>> getClasses() {
-        return new HashSet<Class<?>>(Arrays.asList(DomainHandler.class,
+        return new HashSet<Class<?>>(Arrays.asList(MOXyJsonProvider.class,
+                                                   DomainHandler.class,
                                                    RoleHandler.class,
                                                    UserHandler.class));
     }
