bf24142a10a0b38fac2093351d7cae2e25cdfa5c
melserngawy <melserngawy@inocybe.com>
2015-10-02 16:35:47 -0400

    Fix DataEncrypter and add Unit Test
    Change-Id: Ib8ef8d5e2532f96614cea2bd0efb663c69347953
    Signed-off-by: melserngawy <melserngawy@inocybe.com>

---
+++ aaa-authn-mdsal-store/aaa-authn-mdsal-store-impl/src/test/java/org/opendaylight/aaa/authn/mdsal/store/DataEncrypterTest.java
@@ -0,0 +1,27 @@
+package org.opendaylight.aaa.authn.mdsal.store;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DataEncrypterTest {
+
+    @Test
+    public void testEncrypt() {
+        DataEncrypter dataEncry = new DataEncrypter("foo_key_test");
+        String token = "foo_token_test";
+        String eToken = dataEncry.encrypt(token);
+        //check for decryption result
+        assertEquals(token, dataEncry.decrypt(eToken));
+    }
+
+    @Test
+    public void testDecrypt() {
+        DataEncrypter dataEncry = new DataEncrypter("foo_key_test");
+        String eToken = "foo_etoken_test";
+        assertEquals(dataEncry.decrypt(""), null);
+        //check for encryption Tag
+        assertEquals(eToken, dataEncry.decrypt(eToken));
+    }
+
+}
