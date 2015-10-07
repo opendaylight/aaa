c8e1eebddf5e6d70be560c1c7a8107b3d6d74c35
Ryan Goulding <ryandgoulding@gmail.com>
2015-10-07 15:23:35 -0400

    Bug4430 Unclear error message when Basic Authentication has a bad header format
    This change ensures an AuthenticationException is thrown when and improperly
    formatted Authentication Header is provided.  Tests are provided to ensure this
    functionality.  The HttpBasicAuth.validate() function is refactored to utilize a
    series of helper methods.
    
    Change-Id: I62ae6049ca44aa61cd9446ce5eb961d469b8798f
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- aaa-authn-basic/src/main/java/org/opendaylight/aaa/basic/HttpBasicAuth.java
+++ aaa-authn-basic/src/main/java/org/opendaylight/aaa/basic/HttpBasicAuth.java
@@ -19,6 +19,8 @@ import org.opendaylight.aaa.api.Claim;
 import org.opendaylight.aaa.api.CredentialAuth;
 import org.opendaylight.aaa.api.PasswordCredentials;
 import org.opendaylight.aaa.api.TokenAuth;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.sun.jersey.core.util.Base64;
 
@@ -27,29 +29,72 @@ import com.sun.jersey.core.util.Base64;
  * backward compatible authenticator, but usage of this authenticator or
  * HTTP Basic Authentication is highly discouraged due to its vulnerability.
  *
+ * To obtain a token using the HttpBasicAuth Strategy, add a header to your
+ * HTTP request in the form:
+ * <code>Authorization: Basic BASE_64_ENCODED_CREDENTIALS</code>
+ *
+ * Where <code>BASE_64_ENCODED_CREDENTIALS</code> is the base 64 encoded value
+ * of the user's credentials in the following form:
+ * <code>user:password</code>
+ *
+ * For example, assuming the user is "admin" and the password is "admin":
+ * <code>Authorization: Basic YWRtaW46YWRtaW4=</code>
+ *
  * @author liemmn
  *
  */
 public class HttpBasicAuth implements TokenAuth {
+
     private static final String AUTH_HEADER = "Authorization";
+
     private static final String AUTH_SEP = ":";
+
     private static final String BASIC_PREFIX = "Basic ";
 
-    volatile CredentialAuth<PasswordCredentials> ca;
+    private static final Logger LOG = LoggerFactory.getLogger(HttpBasicAuth.class);
+
+    volatile CredentialAuth<PasswordCredentials> credentialAuth;
+
+    private static boolean checkAuthHeaderFormat(final String authHeader) {
+       return (authHeader != null && authHeader.startsWith(BASIC_PREFIX));
+    }
+
+    private static String extractAuthHeader(final Map<String, List<String>> headers) {
+        return headers.get(AUTH_HEADER).get(0);
+    }
+
+    private static String [] extractCredentialArray(final String authHeader) {
+        return new String(Base64.base64Decode(authHeader
+                .substring(BASIC_PREFIX.length()))).split(AUTH_SEP);
+    }
+
+    private static Authentication generateAuthentication(CredentialAuth<PasswordCredentials> credentialAuth, final String [] creds)
+            throws ArrayIndexOutOfBoundsException, IllegalStateException {
+        final PasswordCredentials pc = new PasswordCredentialBuilder()
+                .setUserName(creds[0]).setPassword(creds[1]).build();
+        // domain set to null;  assumes the default "sdn" domain
+        final Claim claim = credentialAuth.authenticate(pc, null);
+        return new AuthenticationBuilder(claim).build();
+    }
 
     @Override
-    public Authentication validate(Map<String, List<String>> headers)
+    public Authentication validate(final Map<String, List<String>> headers)
             throws AuthenticationException {
         if (headers.containsKey(AUTH_HEADER)) {
-            final String authHeader = headers.get(AUTH_HEADER).get(0);
-            if (authHeader != null && authHeader.startsWith(BASIC_PREFIX)) {
+            final String authHeader = extractAuthHeader(headers);
+            if (checkAuthHeaderFormat(authHeader)) {
                 // HTTP Basic Auth
-                String[] creds = new String(Base64.base64Decode(authHeader
-                        .substring(BASIC_PREFIX.length()))).split(AUTH_SEP);
-                PasswordCredentials pc = new PasswordCredentialBuilder()
-                        .setUserName(creds[0]).setPassword(creds[1]).build();
-                Claim claim = ca.authenticate(pc, null);
-                return new AuthenticationBuilder(claim).build();
+                String[] creds = extractCredentialArray(authHeader);
+                // Assumes correct formatting in form Base64("user:password").
+                // Throws an exception if an unknown format is used.
+                try {
+                    return generateAuthentication(this.credentialAuth, creds);
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    final String message = "Login Attempt in Bad Format."
+                            + " Please provide user:password in Base64 format.";
+                    LOG.info(message);
+                    throw new AuthenticationException(message);
+                }
             }
         }
         return null;
