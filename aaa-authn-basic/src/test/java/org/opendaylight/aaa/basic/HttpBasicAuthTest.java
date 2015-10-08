a095782e4e43248cbbcf961bbb8bfdaeba00ed46
Ryan Goulding <ryandgoulding@gmail.com>
2015-10-08 14:11:17 -0400

    Revert "Bug4430 Unclear error message when Basic Authentication has a bad header format"
    This reverts commit c8e1eebddf5e6d70be560c1c7a8107b3d6d74c35.
    
    Change-Id: Ic3a4bd0def317056de81616c0d5e938810fa0e2b
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- aaa-authn-basic/src/test/java/org/opendaylight/aaa/basic/HttpBasicAuthTest.java
+++ aaa-authn-basic/src/test/java/org/opendaylight/aaa/basic/HttpBasicAuthTest.java
@@ -38,14 +38,14 @@ public class HttpBasicAuthTest {
     @Before
     public void setup() {
         auth = new HttpBasicAuth();
-        auth.credentialAuth = mock(CredentialAuth.class);
+        auth.ca = mock(CredentialAuth.class);
         when(
-                auth.credentialAuth.authenticate(new PasswordCredentialBuilder()
+                auth.ca.authenticate(new PasswordCredentialBuilder()
                         .setUserName(USERNAME).setPassword(PASSWORD).build(),
                         null)).thenReturn(
                 new ClaimBuilder().setUser("admin").addRole("admin").setUserId("123").build());
         when(
-                auth.credentialAuth.authenticate(new PasswordCredentialBuilder()
+                auth.ca.authenticate(new PasswordCredentialBuilder()
                         .setUserName(USERNAME).setPassword("bozo").build(),
                         null)).thenThrow(new AuthenticationException("barf"));
     }
@@ -67,30 +67,6 @@ public class HttpBasicAuthTest {
     @Test(expected = AuthenticationException.class)
     public void testValidateBadPassword() throws UnsupportedEncodingException {
         String data = USERNAME + ":bozo";
-        Map<String, List<String>> headers = new HashMap<>();
-        headers.put(
-                "Authorization",
-                Arrays.asList("Basic "
-                        + new String(Base64.encode(data.getBytes("utf-8")))));
-        auth.validate(headers);
-    }
-
-    @Test(expected = AuthenticationException.class)
-    public void testBadHeaderFormatNoPassword() throws UnsupportedEncodingException {
-        // just provide the username
-        String data = USERNAME;
-        Map<String, List<String>> headers = new HashMap<>();
-        headers.put(
-                "Authorization",
-                Arrays.asList("Basic "
-                        + new String(Base64.encode(data.getBytes("utf-8")))));
-        auth.validate(headers);
-    }
-
-    @Test(expected = AuthenticationException.class)
-    public void testBadHeaderFormat() throws UnsupportedEncodingException {
-        // provide username:
-        String data = USERNAME + "$" + PASSWORD;
         Map<String, List<String>> headers = new HashMap<>();
         headers.put(
                 "Authorization",
