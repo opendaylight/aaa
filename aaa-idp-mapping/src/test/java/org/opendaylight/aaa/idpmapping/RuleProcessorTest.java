445208d6eb4ee6ef4005752b3cfa48727d5c0d17
melserngawy <melserngawy@inocybe.com>
2015-10-08 17:50:11 -0400

    unit test for authn-idp-mapping
    Change-Id: Ic720195734e18f8ddbea6e54713f70be2920b30f
    Signed-off-by: melserngawy <melserngawy@inocybe.com>

---
+++ aaa-idp-mapping/src/test/java/org/opendaylight/aaa/idpmapping/RuleProcessorTest.java
@@ -0,0 +1,50 @@
+package org.opendaylight.aaa.idpmapping;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+public class RuleProcessorTest {
+
+    @Mock private RuleProcessor ruleProcess;
+
+    @Test
+    public void testJoin() {
+        List<Object> list = new ArrayList<Object>();
+        list.add("str1");
+        list.add("str2");
+        list.add("str3");
+        assertEquals("str1/str2/str3", RuleProcessor.join(list, "/"));
+    }
+
+    @Test
+    public void testGetMapping() {
+        Map<String, Object> namespace = new HashMap<String, Object>() {{
+            put("foo1", new HashMap<String, String>(){{put("0", "1"); }});
+        }};
+        final Map<String, Object> item = new HashMap<String, Object>(){{put("str", "val"); }};
+        Map<String, Object> rules = new HashMap<String, Object>() {{
+            put("mapping", item);
+            put("mapping_name", "mapping");
+        }};
+        assertNotNull(ruleProcess.getMapping(namespace, rules));
+    }
+
+    @Test
+    public void testProcess() {
+        String json =  " {\"rules\":[" +
+                "{\"Name\":\"user\", \"Id\":1},"+
+                "{\"Name\":\"Admin\", \"Id\":2}]} ";
+        assertNotNull(ruleProcess.process(json));
+    }
+
+}
