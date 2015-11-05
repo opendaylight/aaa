f429ddf8d4db71c40ca9957c52e4052b95cb10de
Sharon Aicler <saichler@cisco.com>
2015-11-05 10:18:09 -0800

    MD-SAL Store applied Comments
    Change-Id: Idc25ccc288767ee7d97681734a5a16b78689729f
    Signed-off-by: Sharon Aicler <saichler@cisco.com>

--- aaa-authn-api/src/main/java/org/opendaylight/aaa/api/IDMStoreException.java
+++ aaa-authn-api/src/main/java/org/opendaylight/aaa/api/IDMStoreException.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.aaa.api;
 
-@SuppressWarnings("serial")
+/*
+ * @author - Sharon Aicler (saichler@cisco.com)
+ */
 public class IDMStoreException extends Exception{
+
+    private static final long serialVersionUID = -7534127680943957878L;
+
+    public IDMStoreException(Exception e){
+        super(e);
+    }
     public IDMStoreException(String msg){
         super(msg);
     }
