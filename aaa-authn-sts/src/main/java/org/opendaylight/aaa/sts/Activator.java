1522733d1b36ddc4bd335c5d0ed00763f285eadd
Sharon Aicler <saichler@cisco.com>
2015-11-12 11:21:42 -0800

    Hardwire Services in sts ServiceLocator
    Change-Id: Ia408c78484ee0183626437318c147d60979d204f
    Signed-off-by: Sharon Aicler <saichler@cisco.com>

--- aaa-authn-sts/src/main/java/org/opendaylight/aaa/sts/Activator.java
+++ aaa-authn-sts/src/main/java/org/opendaylight/aaa/sts/Activator.java
@@ -18,6 +18,9 @@ import org.opendaylight.aaa.api.IdMService;
 import org.opendaylight.aaa.api.TokenAuth;
 import org.opendaylight.aaa.api.TokenStore;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * An activator for the secure token server to inject in a
@@ -27,7 +30,8 @@ import org.osgi.framework.BundleContext;
  *
  */
 public class Activator extends DependencyActivatorBase {
-
+    public static final Logger logger = LoggerFactory.getLogger(Activator.class);
+    private static boolean HARDWIRE_SERVICES=true;
     @Override
     public void init(BundleContext context, DependencyManager manager)
             throws Exception {
@@ -54,10 +58,59 @@ public class Activator extends DependencyActivatorBase {
                 .setRequired(true))
             .add(createServiceDependency().setService(ClientService.class)
                         .setRequired(true)));
+
+        new ServiceWireTask(AuthenticationService.class, context);
+        new ServiceWireTask(IdMService.class, context);
+        new ServiceWireTask(TokenAuth.class, context);
+        new ServiceWireTask(TokenStore.class, context);
+        new ServiceWireTask(ClientService.class, context);
     }
 
     @Override
     public void destroy(BundleContext context, DependencyManager manager)
             throws Exception {
     }
+
+    private class ServiceWireTask extends Thread {
+        private Class<?> serviceClass = null;
+        private BundleContext context = null;
+
+        public ServiceWireTask(Class<?> _serviceClass, BundleContext _context){
+            this.serviceClass = _serviceClass;
+            this.context = _context;
+            this.start();
+        }
+
+        public void run(){
+            ServiceReference<?> ref = null;
+            while(ref==null){
+                try{Thread.sleep(1000);}catch(Exception err){}
+                ref = context.getServiceReference(this.serviceClass);
+                if(ref==null){
+                    logger.error("Service "+this.serviceClass.getSimpleName()+" is still missing");
+                }
+                if(ref!=null && HARDWIRE_SERVICES){
+                    logger.info("Hardwirering Service "+this.serviceClass.getSimpleName());
+                    Object service = context.getService(ref);
+                    if(service instanceof IdMService){
+                        ServiceLocator.getInstance().idmService = (IdMService)service;
+                        ServiceLocator.getInstance().credentialAuth = (CredentialAuth)service;
+                    }else
+                    if(service instanceof AuthenticationService){
+                        ServiceLocator.getInstance().authenticationService = (AuthenticationService)service;
+                    }else
+                    if(service instanceof TokenAuth){
+                        ServiceLocator.getInstance().tokenAuthAdded((TokenAuth)service);
+                    }else
+                    if(service instanceof TokenStore){
+                        ServiceLocator.getInstance().tokenStoreAdded((TokenStore)service);
+                    }else
+                    if(service instanceof ClientService){
+                        ServiceLocator.getInstance().clientService = (ClientService)service;
+                    }
+                }
+            }
+        }
+    }
+
 }
