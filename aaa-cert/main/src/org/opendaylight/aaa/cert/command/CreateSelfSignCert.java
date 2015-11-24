baf222f5a4d5d48505b8f6bff01d92d9ecc3e0a7
Mohamed El-Serngawy <melserngawy@inocybe.com>
2015-11-24 18:16:22 -0500

    WIP adding command-line and certificate functionalities
    Change-Id: I0d5ffe7d004146fdcc92b3cf06cf45762b99cbd2
    Signed-off-by: Mohamed El-Serngawy <melserngawy@inocybe.com>

---
+++ aaa-cert/main/src/org/opendaylight/aaa/cert/command/CreateSelfSignCert.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015 Inocybe Technologies. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.aaa.cert.command;
+
+import org.apache.maven.shared.utils.cli.StreamConsumer;
+import org.opendaylight.aaa.cert.KeyStoreUtilis;
+import org.opendaylight.aaa.cert.abstractKeyToolCommand;
+
+public class CreateSelfSignCert extends abstractKeyToolCommand {
+
+    public CreateSelfSignCert() {
+        arguments.put(KeyStoreUtilis.KeyToolArguments.genKey, "");
+        arguments.put(KeyStoreUtilis.KeyToolArguments.keyAlg, "RSA");
+        arguments.put(KeyStoreUtilis.KeyToolArguments.alias, "");
+        arguments.put(KeyStoreUtilis.KeyToolArguments.keyStore, "");
+        arguments.put(KeyStoreUtilis.KeyToolArguments.dName, "");
+        arguments.put(KeyStoreUtilis.KeyToolArguments.storePass, "");
+        arguments.put(KeyStoreUtilis.KeyToolArguments.keyPass, "");
+        arguments.put(KeyStoreUtilis.KeyToolArguments.keySize, "2048");
+        arguments.put(KeyStoreUtilis.KeyToolArguments.validity, "360");
+    }
+
+    @Override
+    public StreamConsumer getSystemOutStreamConsumer() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public StreamConsumer getSystemErrorStreamConsumer() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setSystemOutStreamConsumer(StreamConsumer systemOutStreamConsumer) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void setSystemErrorStreamConsumer(StreamConsumer systemErrorStreamConsumer) {
+        // TODO Auto-generated method stub
+    }
+}
