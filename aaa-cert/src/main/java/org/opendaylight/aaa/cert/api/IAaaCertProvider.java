26c62233e2ff2cd1d6ea36288f086bb0df795dd2
Mohamed El-Serngawy <melserngawy@inocybe.com>
2015-11-24 18:16:22 -0500

    adding command-line and certificate functionalities
    Change-Id: I0d5ffe7d004146fdcc92b3cf06cf45762b99cbd2
    Signed-off-by: Mohamed El-Serngawy <melserngawy@inocybe.com>

---
+++ aaa-cert/src/main/java/org/opendaylight/aaa/cert/api/IAaaCertProvider.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2015 Inocybe Technologies. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.aaa.cert.api;
+
+import java.security.KeyStore;
+
+/**
+ *
+ * @author mserngawy
+ *
+ * IAaaCertProvider define the basic operation for certificates management
+ */
+public interface IAaaCertProvider extends java.lang.AutoCloseable {
+
+    boolean addCertificateODLKeyStore(String storePasswd, String alias, String certificate);
+
+    boolean addCertificateTrustStore(String storePasswd, String alias, String certificate);
+
+    String createODLKeyStore(String keyStore, String storePasswd, String alias, String dName, int validity);
+
+    String createTrustKeyStore(String keyStore, String storePasswd, String alias);
+
+    String genODLKeyStorCertificateReq(String storePasswd, String alias);
+
+    String getCertificateTrustStore(String storePasswd, String aliase);
+
+    String getODLKeyStorCertificate(String storePasswd, String alias);
+
+    KeyStore getODLKeyStore();
+
+    KeyStore getTrustKeyStore();
+}
