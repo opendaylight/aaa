8b869000aa173deaae7c198390cb3dca3a6c9589
Sharon Aicler <saichler@cisco.com>
2015-12-04 13:32:32 -0800

    Convert IDM Light to use config subsystem - DO NOT MERGE
    Change-Id: I3800b15c4371c5acb7c9ae9b2af0c85412224d72
    Signed-off-by: Sharon Aicler <saichler@cisco.com>

--- aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/StoreBuilder.java
+++ aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/StoreBuilder.java
@@ -20,6 +20,7 @@ import org.opendaylight.aaa.api.model.User;
  * @author saichler@cisco.com
  *
  */
+import org.opendaylight.yang.gen.v1.config.aaa.authn.idmlight.rev151204.AAAIDMLightModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -43,14 +44,16 @@ public class StoreBuilder {
     public static void init() throws IDMStoreException {
         logger.info("creating idmlight schema in store");
         int waitingTime = 5;
-        while(ServiceLocator.INSTANCE.getStore()==null){
+        IIDMStore store = AAAIDMLightModule.getStore();
+
+        while(store==null){
             try{Thread.sleep(STOREBUILDER_POLL_INTERVAL);}catch(Exception err){logger.error("Interrupted",err);}
             logger.info("No store service is available yet, waiting up until 30 seconds, waited for "+waitingTime+" seconds..");
             waitingTime+=5;
             if(waitingTime>=STOREBUILDER_INIT_TIMEOUT)
                 break;
         }
-        if(ServiceLocator.INSTANCE.getStore()==null){
+        if(store==null){
             logger.info("Store is not available, aborting initialization");
             return;
         }else{
@@ -58,7 +61,7 @@ public class StoreBuilder {
         }
 
         //Check if default domain exist, if it exist then do not create default data in the store
-        Domain defaultDomain = ServiceLocator.INSTANCE.getStore().readDomain(IIDMStore.DEFAULT_DOMAIN);
+        Domain defaultDomain = store.readDomain(IIDMStore.DEFAULT_DOMAIN);
         if(defaultDomain!=null){
             logger.info("Found default domain in Store, skipping insertion of default data");
             return;
@@ -72,7 +75,7 @@ public class StoreBuilder {
         domain.setEnabled(true);
         domain.setName(IIDMStore.DEFAULT_DOMAIN);
         domain.setDescription("default odl sdn domain");
-        domain = ServiceLocator.INSTANCE.getStore().writeDomain(domain);
+        domain = store.writeDomain(domain);
 
         // create users
         //admin user
@@ -82,7 +85,7 @@ public class StoreBuilder {
         adminUser.setDescription("admin user");
         adminUser.setEmail("");
         adminUser.setPassword("admin");
-        adminUser = ServiceLocator.INSTANCE.getStore().writeUser(adminUser);
+        adminUser = store.writeUser(adminUser);
         // user user
         userUser.setEnabled(true);
         userUser.setName("user");
@@ -90,33 +93,33 @@ public class StoreBuilder {
         userUser.setDescription("user user");
         userUser.setEmail("");
         userUser.setPassword("user");
-        userUser = ServiceLocator.INSTANCE.getStore().writeUser(userUser);
+        userUser = store.writeUser(userUser);
 
         // create Roles
         adminRole.setName("admin");
         adminRole.setDomainid(domain.getDomainid());
         adminRole.setDescription("a role for admins");
-        adminRole = ServiceLocator.INSTANCE.getStore().writeRole(adminRole);
+        adminRole = store.writeRole(adminRole);
         userRole.setName("user");
         userRole.setDomainid(domain.getDomainid());
         userRole.setDescription("a role for users");
-        userRole = ServiceLocator.INSTANCE.getStore().writeRole(userRole);
+        userRole = store.writeRole(userRole);
 
         // create grants
         Grant grant = new Grant();
         grant.setDomainid(domain.getDomainid());
         grant.setUserid(userUser.getUserid());
         grant.setRoleid(userRole.getRoleid());
-        grant = ServiceLocator.INSTANCE.getStore().writeGrant(grant);
+        grant = store.writeGrant(grant);
 
         grant.setDomainid(domain.getDomainid());
         grant.setUserid(adminUser.getUserid());
         grant.setRoleid(userRole.getRoleid());
-        grant = ServiceLocator.INSTANCE.getStore().writeGrant(grant);
+        grant = store.writeGrant(grant);
 
         grant.setDomainid(domain.getDomainid());
         grant.setUserid(adminUser.getUserid());
         grant.setRoleid(adminRole.getRoleid());
-        grant = ServiceLocator.INSTANCE.getStore().writeGrant(grant);
+        grant = store.writeGrant(grant);
    }
 }
