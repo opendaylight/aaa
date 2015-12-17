26f4b4f2ff91fd3cac47dc23a5bf5b3768a2b425
Ryan Goulding <ryandgoulding@gmail.com>
2015-12-17 12:56:15 -0500

    Bug 4804 IDMLight REST endpoints do not redact the salt user field
    Enforces redacted fields salt fields for IDMLight rest endpoints, since
    exposing such information is a plausible security concern.   Abstracts several
    helper methods to avoid code duplication.  Cleans up error reporting so error
    messages are consistent among operations on the /auth/v1/users REST
    endpoint.  Reformats several areas of code that do not align to AAA
    checkstyle.  Adds much needed documentation on how to utilize the REST
    endpoint.
    
    Change-Id: I65319a34d1c0f0363fc2c73a6e872cdb296f9604
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/rest/UserHandler.java
+++ aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/rest/UserHandler.java
@@ -8,11 +8,7 @@
 
 package org.opendaylight.aaa.idm.rest;
 
-/**
- *
- * @author peter.mellquist@hp.com
- *
- */
+import java.util.Collection;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -25,6 +21,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+
 import org.opendaylight.aaa.api.IDMStoreException;
 import org.opendaylight.aaa.api.model.IDMError;
 import org.opendaylight.aaa.api.model.User;
@@ -35,155 +32,358 @@ import org.opendaylight.yang.gen.v1.config.aaa.authn.idmlight.rev151204.AAAIDMLi
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * REST application used to manipulate the H2 database users table. The REST
+ * endpoint is <code>/auth/v1/users</code>.
+ *
+ * The following provides examples of how curl commands and payloads to utilize
+ * the users REST endpoint:
+ *
+ * <b>Get All Users</b>
+ * <code>curl -u admin:admin http://{HOST}:{PORT}/auth/v1/users</code>
+ *
+ * <b>Get A Specific User</b>
+ * <code>curl -u admin:admin http://{HOST}:{PORT}/auth/v1/users/{id}</code>
+ *
+ * <b>Create A User</b>
+ * <code>curl -u admin:admin -X POST -H "Content-type: application/json" --data-binary {@literal @}user.json http://{HOST}:{PORT}/auth/v1/users
+ * An example of user.json file is:
+ * <code>{
+ *   "name": "admin2",
+ *   "password", "admin2",
+ *   "domain": "sdn"
+ * }</code>
+ *
+ * <b>Update A User</b>
+ * <code>curl -u admin:admin -X PUT -H "Content-type: application/json" --data-binary {@literal @}user.json http://{HOST}:{PORT}/auth/v1/users/{id}
+ * An example of user.json file is:
+ * <code>{
+ *   "name": "admin2",
+ *   "password", "admin2",
+ *   "domain": "sdn",
+ *   "description", "Simple description."
+ * }</code>
+ *
+ * <b>Delete A User</b>
+ * <code>curl -u admin:admin -X DELETE http://{HOST}:{PORT}/auth/v1/users/{id}</code>
+ *
+ * @author peter.mellquist@hp.com
+ * @author Ryan Goulding (ryandgoulding@gmail.com)
+ */
 @Path("/v1/users")
 public class UserHandler {
-   private static Logger logger = LoggerFactory.getLogger(UserHandler.class);
-   protected final static String DEFAULT_PWD = "changeme";
-   public static final String REDACTED_PASSWORD = "**********";
 
-   @GET
-   @Produces("application/json")
-   public Response getUsers() {
-      logger.info("get all users");
-      Users users=null;
-      try {
-         users = AAAIDMLightModule.getStore().getUsers();
-      }
-      catch (IDMStoreException se) {
-         return new IDMError(500,"internal error getting users",se.getMessage()).response();
-      }
+    private static final Logger LOG = LoggerFactory.getLogger(UserHandler.class);
 
-      // obsfucate pwd
-      for (int z=0;z<users.getUsers().size();z++) {
-          users.getUsers().get(z).setPassword(REDACTED_PASSWORD);
-      }
+    /**
+     * If a user is created through the <code>/auth/v1/users</code> rest
+     * endpoint without a password, the default password is assigned to the
+     * user.
+     */
+    protected final static String DEFAULT_PWD = "changeme";
 
-      return Response.ok(users).build();
-   }
+    /**
+     * When an HTTP GET is performed on <code>/auth/v1/users</code>, the
+     * password field is replaced with <code>REDACTED_PASSWORD</code> for
+     * security reasons.
+     */
+    protected static final String REDACTED_PASSWORD = "**********";
 
+    /**
+     * When an HTTP GET is performed on <code>/auth/v1/users</code>, the salt
+     * field is replaced with <code>REDACTED_SALT</code> for security reasons.
+     */
+    protected static final String REDACTED_SALT = "**********";
 
-   @GET
-   @Path("/{id}")
-   @Produces("application/json")
-   public Response getUser(@PathParam("id") String id)  {
-      logger.info("get /users/" + id);
-      User user=null;
-      try {
-         user = AAAIDMLightModule.getStore().readUser(id);
-      }
-      catch(IDMStoreException se) {
-         return new IDMError(500,"internal error getting user",se.getMessage()).response();
-      }
-      if (user==null) {
-         return new IDMError(404,"user not found! id:" + id,"").response();
-      }
-      // obsfucate pwd
-      user.setPassword(REDACTED_PASSWORD);
-      user.setSalt(REDACTED_PASSWORD);
-      return Response.ok(user).build();
-   }
+    /**
+     * When creating a user, the description is optional and defaults to an
+     * empty string.
+     */
+    protected static final String DEFAULT_DESCRIPTION = "";
 
-   @POST
-   @Consumes("application/json")
-   @Produces("application/json")
-   public Response createUser(@Context UriInfo info,User user) {
-      logger.info("post /users");
-      try {
-         // enabled by default
-         if (user.isEnabled()==null) {
+    /**
+     * When creating a user, the email is optional and defaults to an empty
+     * string.
+     */
+    protected static final String DEFAULT_EMAIL = "";
+
+    /**
+     * Extracts all users. The password and salt fields are redacted for
+     * security reasons.
+     *
+     * @return A response containing the users, or internal error if one occurs
+     */
+    @GET
+    @Produces("application/json")
+    public Response getUsers() {
+        LOG.info("GET /auth/v1/users  (extracts all users)");
+
+        try {
+            final Users users = AAAIDMLightModule.getStore().getUsers();
+
+            // Redact the password and salt for security purposes.
+            final Collection<User> usersList = users.getUsers();
+            for (User user : usersList) {
+                prepareUserForOutput(user);
+            }
+
+            return Response.ok(users).build();
+        } catch (IDMStoreException se) {
+            return internalError("getting", se);
+        }
+    }
+
+    /**
+     * Extracts the user represented by <code>id</code>. The password and salt
+     * fields are redacted for security reasons.
+     *
+     * @param id the unique id of representing the user account
+     * @return A response with the user information, or internal error if one occurs
+     */
+    @GET
+    @Path("/{id}")
+    @Produces("application/json")
+    public Response getUser(@PathParam("id") String id) {
+        LOG.info("GET auth/v1/users/ {}  (extract user with specified id)", id);
+
+        try {
+            final User user = AAAIDMLightModule.getStore().readUser(id);
+
+            if (user == null) {
+                return new IDMError(404, String.format("user not found! id: %d", id), "").response();
+            }
+
+            // Redact the password and salt for security purposes.
+            prepareUserForOutput(user);
+
+            return Response.ok(user).build();
+        } catch (IDMStoreException se) {
+            return internalError("getting", se);
+        }
+    }
+
+    /**
+     * REST endpoint to create a user. Name and domain are required attributes,
+     * and all other fields (description, email, password, enabled) are
+     * optional. Optional fields default in the following manner:
+     * <code>description</code>: An empty string (<code>""</code>).
+     * <code>email</code>: An empty string (<code>""</code>).
+     * <code>password</code>: <code>changeme</code> <code>enabled</code>:
+     * <code>true</code>
+     *
+     * If a password is not provided, please ensure you change the default
+     * password ASAP for security reasons!
+     *
+     * @param info passed from Jersey
+     * @param user the user defined in the JSON payload
+     * @return A response stating success or failure of user creation
+     */
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response createUser(@Context UriInfo info, User user) {
+        LOG.info("POST /auth/v1/users  (create a user with the specified payload");
+
+        // The "enabled" field is optional, and defaults to true.
+        if (user.isEnabled() == null) {
             user.setEnabled(true);
-         }
+        }
 
-         // user name is required
-         if (user.getName()==null) {
-            return new IDMError(400,"user name is required","").response();
-         }
-         else if (user.getName().length()> IdmLightApplication.MAX_FIELD_LEN) {
-            return new IDMError(400,"user name max length is :" + IdmLightApplication.MAX_FIELD_LEN,"").response();
-         }
+        // The "name" field is required.
+        final String userName = user.getName();
+        if (userName == null) {
+            return missingRequiredField("name");
+        }
+        // The "name" field has a maximum length.
+        if (userName.length() > IdmLightApplication.MAX_FIELD_LEN) {
+            return providedFieldTooLong("name", IdmLightApplication.MAX_FIELD_LEN);
+        }
 
-         // domain id/name is required
-         if (user.getDomainid()==null) {
-            return new IDMError(400,"user domain is required","").response();
-         }
-         else if (user.getDomainid().length()>IdmLightApplication.MAX_FIELD_LEN) {
-            return new IDMError(400,"user domain max length is :" + IdmLightApplication.MAX_FIELD_LEN,"").response();
-         }
+        // The "domain field is required.
+        final String domainId = user.getDomainid();
+        if (domainId == null) {
+            return missingRequiredField("domain");
+        }
+        // The "domain" field has a maximum length.
+        if (domainId.length() > IdmLightApplication.MAX_FIELD_LEN) {
+            return providedFieldTooLong("domain", IdmLightApplication.MAX_FIELD_LEN);
+        }
 
-         // user description is optional
-         if (user.getDescription()==null) {
-            user.setDescription("");
-         }
-         else if (user.getDescription().length()>IdmLightApplication.MAX_FIELD_LEN) {
-            return new IDMError(400,"user description max length is :" + IdmLightApplication.MAX_FIELD_LEN,"").response();
-         }
+        // The "description" field is optional and defaults to "".
+        final String userDescription = user.getDescription();
+        if (userDescription == null) {
+            user.setDescription(DEFAULT_DESCRIPTION);
+        }
+        // The "description" field has a maximum length.
+        if (userDescription.length() > IdmLightApplication.MAX_FIELD_LEN) {
+            return providedFieldTooLong("description", IdmLightApplication.MAX_FIELD_LEN);
+        }
 
-         // user email is optional
-         if (user.getEmail()==null) {
-            user.setEmail("");
-         }
-         else if (user.getEmail().length()>IdmLightApplication.MAX_FIELD_LEN) {
-            return new IDMError(400,"user email max length is :" + IdmLightApplication.MAX_FIELD_LEN,"").response();
-         }
+        // The "email" field is optional and defaults to "".
+        final String userEmail = user.getEmail();
+        if (userEmail == null) {
+            user.setEmail(DEFAULT_EMAIL);
+        }
+        if (userEmail.length() > IdmLightApplication.MAX_FIELD_LEN) {
+            return providedFieldTooLong("email", IdmLightApplication.MAX_FIELD_LEN);
+        }
+        // TODO add a check on email format here.
 
-         // user password optional and will default if not provided
-         if (user.getPassword()==null) {
+        // The "password" field is optional and defautls to "changeme".
+        final String userPassword = user.getPassword();
+        if (userPassword == null) {
             user.setPassword(DEFAULT_PWD);
-         }
-         else if (user.getPassword().length()>IdmLightApplication.MAX_FIELD_LEN) {
-            return new IDMError(400,"user password max length is :" + IdmLightApplication.MAX_FIELD_LEN,"").response();
-         }
+        }
+        if (userPassword.length() > IdmLightApplication.MAX_FIELD_LEN) {
+            return providedFieldTooLong("password", IdmLightApplication.MAX_FIELD_LEN);
+        }
 
-         // create user
-         User createdUser = AAAIDMLightModule.getStore().writeUser(user);
-         user.setUserid(createdUser.getUserid());
-      }
-      catch (IDMStoreException se) {
-         return new IDMError(500,"internal error creating user",se.getMessage()).response();
-      }
+        try {
+            // At this point, fields have been properly verified. Create the
+            // user account
+            final User createdUser = AAAIDMLightModule.getStore().writeUser(user);
+            user.setUserid(createdUser.getUserid());
+        } catch (IDMStoreException se) {
+            LOG.error("There was an internal error creating the user", se);
+            return internalError("creating", se);
+        }
 
-      // created!
-      return Response.status(201).entity(user).build();
-   }
+        // Redact the password and salt for security reasons.
+        prepareUserForOutput(user);
+        // TODO report back to the client a warning message to change the
+        // default password if none was specified.
+        return Response.status(201).entity(user).build();
+    }
 
+    /**
+     * REST endpoint to update a user account.
+     *
+     * @param info passed from Jersey
+     * @param user the user defined in the JSON payload
+     * @param id the unique id for the user that will be updated
+     * @return A response stating success or failure of the user update
+     */
+    @PUT
+    @Path("/{id}")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response putUser(@Context UriInfo info, User user, @PathParam("id") String id) {
 
-   @PUT
-   @Path("/{id}")
-   @Consumes("application/json")
-   @Produces("application/json")
-   public Response putUser(@Context UriInfo info,User user,@PathParam("id") String id) {
-      logger.info("put /users/" + id);
+        LOG.info("PUT /auth/v1/users/{}  (Updates a user account)", id);
 
-      try {
-         user.setUserid(id);
-         user = AAAIDMLightModule.getStore().updateUser(user);
-         if (user==null) {
-            return new IDMError(404,"user id not found id :"+id,"").response();
-         }
-         IdmLightProxy.clearClaimCache();
-         user.setPassword(REDACTED_PASSWORD);
-         return Response.status(200).entity(user).build();
-      }
-      catch (IDMStoreException se) {
-         return new IDMError(500,"internal error putting user",se.getMessage()).response();
-      }
-   }
+        try {
+            user.setUserid(id);
+            user = AAAIDMLightModule.getStore().updateUser(user);
+            if (user == null) {
+                return new IDMError(404, String.format("User not found for id %s", id), "").response();
+            }
+            IdmLightProxy.clearClaimCache();
 
-   @DELETE
-   @Path("/{id}")
-   public Response deleteUser(@Context UriInfo info,@PathParam("id") String id) {
-      logger.info("delete /users/" + id);
+            // Redact the password and salt for security reasons.
+            prepareUserForOutput(user);
+            return Response.status(200).entity(user).build();
+        } catch (IDMStoreException se) {
+            LOG.error("There was an internal error updating the user", se);
+            return internalError("updating", se);
+        }
+    }
 
-      try {
-         User user = AAAIDMLightModule.getStore().deleteUser(id);
-         if (user==null) {
-            return new IDMError(404,"user id not found id :"+id,"").response();
-         }
-      }
-      catch (IDMStoreException se) {
-         return new IDMError(500,"internal error deleting user",se.getMessage()).response();
-      }
-      IdmLightProxy.clearClaimCache();
-      return Response.status(204).build();
-   }
+    /**
+     * REST endpoint to delete a user account.
+     *
+     * @param info passed from Jersey
+     * @param id the unique id of the user which is being deleted
+     * @return A response stating success or failure of user deletion
+     */
+    @DELETE
+    @Path("/{id}")
+    public Response deleteUser(@Context UriInfo info, @PathParam("id") String id) {
+        LOG.info("DELETE /auth/v1/users/{}  (Delete a user account)", id);
+
+        try {
+            final User user = AAAIDMLightModule.getStore().deleteUser(id);
+
+            if (user == null) {
+                return new IDMError(404,
+                        String.format("Error deleting user.  " +
+                                      "Couldn't find user with id %s", id),
+                                      "").response();
+            }
+        } catch (IDMStoreException se) {
+            LOG.error("There was an internal error deleting the user", se);
+            return internalError("deleting", se);
+        }
+
+        // Successfully deleted the user; report success to the client.
+        IdmLightProxy.clearClaimCache();
+        return Response.status(204).build();
+    }
+
+    /**
+     * Creates a <code>Response</code> related to an internal server error.
+     *
+     * @param verbal such as "creating", "deleting", "updating"
+     * @param e The exception, which is propagated in the response
+     * @return A response containing internal error with specific reasoning
+     */
+    private Response internalError(final String verbal, final Exception e) {
+        return new IDMError(500,
+                "There was an internal error deleting the user",
+                e.getMessage()).response();
+    }
+
+    /**
+     * Creates a <code>Response</code> related to the user not providing a
+     * required field.
+     *
+     * @param fieldName the name of the field which is missing
+     * @return A response explaining that the request is missing a field
+     */
+    private Response missingRequiredField(final String fieldName) {
+        return new IDMError(400,
+                String.format("{} is required to create the user account.  " +
+                              "Please provide a name in your payload.", fieldName),
+                              "").response();
+    }
+
+    /**
+     * Creates a <code>Response</code> related to the user providing a field
+     * that is too long.
+     *
+     * @param fieldName the name of the field that is too long
+     * @param maxFieldLength the maximum length of <code>fieldName</code>
+     * @return A response containing the bad field and the maximum field length
+     */
+    private Response providedFieldTooLong(final String fieldName, final int maxFieldLength) {
+
+        return new IDMError(400,
+                getProvidedFieldTooLongMessage(fieldName, maxFieldLength),
+                "").response();
+    }
+
+    /**
+     * Creates the client-facing message related to the user providing a field
+     * that is too long.
+     *
+     * @param fieldName the name of the field that is too long
+     * @param maxFieldLength the maximum length of <code>fieldName</code>
+     * @return
+     */
+    private static String getProvidedFieldTooLongMessage(final String fieldName,
+            final int maxFieldLength) {
+
+        return String.format("The provided {} field is too long.  " +
+                             "The max length is {}.", fieldName, maxFieldLength);
+    }
+
+    /**
+     * Prepares a user account for output by redacting the appropriate fields.
+     * This method side-effects the <code>user</code> parameter.
+     *
+     * @param user the user account which will have fields redacted
+     */
+    private static void prepareUserForOutput(final User user) {
+        user.setPassword(REDACTED_PASSWORD);
+        user.setSalt(REDACTED_SALT);
+    }
 }
