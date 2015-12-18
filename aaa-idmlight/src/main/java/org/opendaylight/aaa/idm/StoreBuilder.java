78e94e0ad0601bbcfafa25e5d407b59a8a565c58
Ryan Goulding <ryandgoulding@gmail.com>
2015-12-17 19:03:44 -0500

    Formatting applied to StoreBuilder
    This change tackles some vital formatting issues with StoreBuilder.  The
    following is done to the IdmLightApplication class to promote clarity and conformance
    to project style:
    
    1) Add some detailed information to the class comment concerning the default installed
    accounts.
    
    2) Add TODO concerning what to do if "sdn" is deleted.  Implies that "sdn" could
    be made a mandatory domain.
    
    3) Format in eclipse by clicking "format".
    
    4) Fix any errors reported by "mvn checkstyle:checkstyle".
    
    No actual code or functionality changes were made.  This is strictly a
    formatting related patch.
    
    Change-Id: I09f45e529201d3eeaca94286f86e3dc06bf727c8
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/StoreBuilder.java
+++ aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/StoreBuilder.java
@@ -18,23 +18,47 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * StoreBuilder is triggered during feature installation by
+ * <code>AAAIDMLightModule.createInstance()</code>. StoreBuilder is responsible
+ * for initializing the H2 database with initial default user account
+ * information. By default, the following users are created:
+ * <ol>
+ * <li>admin</li>
+ * <li>user</li>
+ * </ol>
+ *
+ * By default, the following domain is created:
+ * <ol>
+ * <li>sdn</li>
+ * </ol>
+ *
+ * By default, the following grants are created:
+ * <ol>
+ * <li>admin with admin role on sdn</li>
+ * <li>admin with user role on sdn</li>
+ * <li>user with user role on sdn</li>
+ * </ol>
+ *
  * @author peter.mellquist@hp.com
  * @author saichler@cisco.com
  */
-
 public class StoreBuilder {
 
-    private static Logger logger = LoggerFactory.getLogger(StoreBuilder.class);
+    private static final Logger LOG = LoggerFactory.getLogger(StoreBuilder.class);
 
     public static void init(IIDMStore store) throws IDMStoreException {
-        logger.info("creating idmlight schema in store");
+        LOG.info("creating idmlight schema in store");
 
-        //Check if default domain exist, if it exist then do not create default data in the store
+        // Check whether the default domain exists. If it exists, then do not
+        // create default data in the store.
+        // TODO Address the fact that someone may delete the sdn domain, or make
+        // sdn mandatory.
         Domain defaultDomain = store.readDomain(IIDMStore.DEFAULT_DOMAIN);
-        if(defaultDomain!=null){
-            logger.info("Found default domain in Store, skipping insertion of default data");
+        if (defaultDomain != null) {
+            LOG.info("Found default domain in Store, skipping insertion of default data");
             return;
         }
+
         // make domain
         Domain domain = new Domain();
         User adminUser = new User();
@@ -46,8 +70,8 @@ public class StoreBuilder {
         domain.setDescription("default odl sdn domain");
         domain = store.writeDomain(domain);
 
-        // create users
-        //admin user
+        // Create default users
+        // "admin" user
         adminUser.setEnabled(true);
         adminUser.setName("admin");
         adminUser.setDomainid(domain.getDomainid());
@@ -55,7 +79,7 @@ public class StoreBuilder {
         adminUser.setEmail("");
         adminUser.setPassword("admin");
         adminUser = store.writeUser(adminUser);
-        // user user
+        // "user" user
         userUser.setEnabled(true);
         userUser.setName("user");
         userUser.setDomainid(domain.getDomainid());
@@ -64,7 +88,7 @@ public class StoreBuilder {
         userUser.setPassword("user");
         userUser = store.writeUser(userUser);
 
-        // create Roles
+        // Create default Roles ("admin" and "user")
         adminRole.setName("admin");
         adminRole.setDomainid(domain.getDomainid());
         adminRole.setDescription("a role for admins");
@@ -74,7 +98,7 @@ public class StoreBuilder {
         userRole.setDescription("a role for users");
         userRole = store.writeRole(userRole);
 
-        // create grants
+        // Create default grants
         Grant grant = new Grant();
         grant.setDomainid(domain.getDomainid());
         grant.setUserid(userUser.getUserid());
@@ -90,5 +114,5 @@ public class StoreBuilder {
         grant.setUserid(adminUser.getUserid());
         grant.setRoleid(adminRole.getRoleid());
         grant = store.writeGrant(grant);
-   }
+    }
 }
