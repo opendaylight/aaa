db62e4bf0c8e9f72571c2bf8e9aa31410cb7b7b4
Ryan Goulding <ryandgoulding@gmail.com>
2016-01-22 20:10:09 -0500

    Bug 5060 Cannot Delete Users
    Fix SQL typo.
    
    Change-Id: I919938db0818a9f1ecf8eb9b61a3ed363c1b65fb
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/persistence/UserStore.java
+++ aaa-idmlight/src/main/java/org/opendaylight/aaa/idm/persistence/UserStore.java
@@ -288,7 +288,7 @@ protected void finalize () throws Throwable {
 
       Connection conn = dbConnect();
       try {
-         String query = "DELETE FROM DOMAINS WHERE domainid = ?";
+         String query = "DELETE FROM USERS WHERE userid = ?";
          PreparedStatement statement = conn.prepareStatement(query);
          statement.setLong(1, savedUser.getUserid());
          int deleteCount = statement.executeUpdate(query);
