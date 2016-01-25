4edadcd7b7dbe202c16e0bea3f3e5de05673ddd7
Sai MarapaReddy <smarapar@brocade.com>
2016-01-25 11:54:00 -0800

    Fix license header violations
    Change-Id: I7f41259477a30e58a88e635f74234366f308c94c
    Signed-off-by: Sai MarapaReddy <sai.marapareddy@gmail.com>

--- aaa-authn-federation/src/test/java/org/opendaylight/aaa/federation/FederationEndpointTest.java
+++ aaa-authn-federation/src/test/java/org/opendaylight/aaa/federation/FederationEndpointTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2014 Hewlett-Packard Development Company, L.P. and others.
- * All rights reserved.
+ * Copyright (c) 2014, 2015 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
- * terms of the Eclipse License v1.0 which accompanies this distribution,
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.opendaylight.aaa.federation;
 
 import static org.junit.Assert.assertEquals;
