8661114754daa5b2e1b9ab505d72d8d1a691169d
Stephen Kitt <skitt@redhat.com>
2016-02-02 18:14:04 +0100

    Ensure H2 resources are closed
    This patch uses try-with-resources with all database resources
    (connections, statements and result sets) to ensure they're closed
    correctly in all cases. It drops the re-used dbConnection since that
    seems fragile (two threads accessing the store simultaneously might
    get the same connection, and one of the threads will close it before
    the other has finished), except for tests.
    
    The initial table check/creation is synchronized to avoid
    time-of-check to time-of-use races.
    
    Common code is extracted to an AbstractStore.
    
    Exceptions are logged and re-thrown as StoreExceptions with exception
    chaining.
    
    Change-Id: Ia63493fcb1361e53a5f3400ee5e2fdf09bccb574
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- aaa-h2-store/src/main/java/org/opendaylight/aaa/h2/persistence/DomainStore.java
+++ aaa-h2-store/src/main/java/org/opendaylight/aaa/h2/persistence/DomainStore.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2015 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+ * Copyright (c) 2014, 2016 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -11,13 +11,9 @@ package org.opendaylight.aaa.h2.persistence;
 import com.google.common.base.Preconditions;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.opendaylight.aaa.api.model.Domain;
 import org.opendaylight.aaa.api.model.Domains;
@@ -29,165 +25,68 @@ import org.slf4j.LoggerFactory;
  * @author peter.mellquist@hp.com
  *
  */
-public class DomainStore {
+public class DomainStore extends AbstractStore<Domain> {
     private static final Logger LOG = LoggerFactory.getLogger(DomainStore.class);
 
-    protected Connection dbConnection = null;
     protected final static String SQL_ID = "domainid";
     protected final static String SQL_NAME = "name";
     protected final static String SQL_DESCR = "description";
     protected final static String SQL_ENABLED = "enabled";
+    private static final String TABLE_NAME = "DOMAINS";
 
     protected DomainStore() {
-    }
-
-    protected Connection getDBConnect() throws StoreException {
-        dbConnection = H2Store.getConnection(dbConnection);
-        return dbConnection;
-    }
-
-    protected void dbClean() throws StoreException, SQLException {
-        Connection c = dbConnect();
-        String sql = "delete from DOMAINS where true";
-        c.createStatement().execute(sql);
-        c.close();
-    }
-
-    protected Connection dbConnect() throws StoreException {
-        Connection conn;
-        try {
-            conn = getDBConnect();
-        } catch (StoreException se) {
-            throw se;
-        }
-        try {
-            DatabaseMetaData dbm = conn.getMetaData();
-            String[] tableTypes = { "TABLE" };
-            ResultSet rs = dbm.getTables(null, null, "DOMAINS", tableTypes);// Caps
-                                                                            // required
-                                                                            // in
-                                                                            // get
-                                                                            // Tables
-            if (rs.next()) {
-                LOG.debug("DOMAINS Table already exists.");
-            } else {
-                LOG.info("in dbConnect, domains Table does not exist, creating table");
-                Statement stmt = null;
-                stmt = conn.createStatement();
-                String sql = "CREATE TABLE DOMAINS " + "(domainid   VARCHAR(128)      PRIMARY KEY,"
-                        + "name        VARCHAR(128)      UNIQUE NOT NULL, "
-                        + "description VARCHAR(128)      , "
-                        + "enabled     INTEGER           NOT NULL)";
-                stmt.executeUpdate(sql);
-                stmt.close();
-            }
-        } catch (SQLException sqe) {
-            throw new StoreException("Cannot connect to database server " + sqe);
-        }
-        return conn;
-    }
-
-    protected void dbClose() {
-        if (dbConnection != null) {
-            try {
-                dbConnection.close();
-            } catch (Exception e) {
-                LOG.error("Cannot close Database Connection", e);
-            }
-        }
+        super(TABLE_NAME);
     }
 
     @Override
-    protected void finalize() throws Throwable {
-        dbClose();
-        super.finalize();
+    protected String getTableCreationStatement() {
+        return "CREATE TABLE DOMAINS "
+                + "(domainid   VARCHAR(128)      PRIMARY KEY,"
+                + "name        VARCHAR(128)      UNIQUE NOT NULL, "
+                + "description VARCHAR(128)      , "
+                + "enabled     INTEGER           NOT NULL)";
     }
 
-    protected Domain rsToDomain(ResultSet rs) throws SQLException {
+    @Override
+    protected Domain fromResultSet(ResultSet rs) throws SQLException {
         Domain domain = new Domain();
-        try {
-            domain.setDomainid(rs.getString(SQL_ID));
-            domain.setName(rs.getString(SQL_NAME));
-            domain.setDescription(rs.getString(SQL_DESCR));
-            domain.setEnabled(rs.getInt(SQL_ENABLED) == 1 ? true : false);
-        } catch (SQLException sqle) {
-            LOG.error("SQL Exception: ", sqle);
-            throw sqle;
-        }
+        domain.setDomainid(rs.getString(SQL_ID));
+        domain.setName(rs.getString(SQL_NAME));
+        domain.setDescription(rs.getString(SQL_DESCR));
+        domain.setEnabled(rs.getInt(SQL_ENABLED) == 1);
         return domain;
     }
 
     protected Domains getDomains() throws StoreException {
         Domains domains = new Domains();
-        List<Domain> domainList = new ArrayList<Domain>();
-        Connection conn = dbConnect();
-        Statement stmt = null;
-        String query = "SELECT * FROM DOMAINS";
-        try {
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            while (rs.next()) {
-                Domain domain = rsToDomain(rs);
-                domainList.add(domain);
-            }
-            rs.close();
-            stmt.close();
-        } catch (SQLException s) {
-            throw new StoreException("SQL Exception : " + s);
-        } finally {
-            dbClose();
-        }
-        domains.setDomains(domainList);
+        domains.setDomains(listAll());
         return domains;
     }
 
     protected Domains getDomains(String domainName) throws StoreException {
         LOG.debug("getDomains for: {}", domainName);
         Domains domains = new Domains();
-        List<Domain> domainList = new ArrayList<Domain>();
-        Connection conn = dbConnect();
-        try {
-            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM DOMAINS WHERE name = ?");
+        try (Connection conn = dbConnect();
+             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM DOMAINS WHERE name = ?")) {
             pstmt.setString(1, domainName);
             LOG.debug("query string: {}", pstmt.toString());
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
-                Domain domain = rsToDomain(rs);
-                domainList.add(domain);
-            }
-            rs.close();
-            pstmt.close();
-        } catch (SQLException s) {
-            throw new StoreException("SQL Exception : " + s);
-        } finally {
-            dbClose();
+            domains.setDomains(listFromStatement(pstmt));
+        } catch (SQLException e) {
+            LOG.error("Error listing domains matching {}", domainName, e);
+            throw new StoreException("Error listing domains", e);
         }
-        domains.setDomains(domainList);
         return domains;
     }
 
     protected Domain getDomain(String id) throws StoreException {
-        Connection conn = dbConnect();
-        try {
-            PreparedStatement pstmt = conn
-                    .prepareStatement("SELECT * FROM DOMAINS WHERE domainid = ? ");
+        try (Connection conn = dbConnect();
+             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM DOMAINS WHERE domainid = ? ")) {
             pstmt.setString(1, id);
             LOG.debug("query string: {}", pstmt.toString());
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                Domain domain = rsToDomain(rs);
-                rs.close();
-                pstmt.close();
-                return domain;
-            } else {
-                rs.close();
-                pstmt.close();
-                return null;
-            }
-        } catch (SQLException s) {
-            throw new StoreException("SQL Exception : " + s);
-        } finally {
-            dbClose();
+            return firstFromStatement(pstmt);
+        } catch (SQLException e) {
+            LOG.error("Error retrieving domain {}", id, e);
+            throw new StoreException("Error loading domain", e);
         }
     }
 
@@ -195,10 +94,9 @@ public class DomainStore {
         Preconditions.checkNotNull(domain);
         Preconditions.checkNotNull(domain.getName());
         Preconditions.checkNotNull(domain.isEnabled());
-        Connection conn = dbConnect();
-        try {
-            String query = "insert into DOMAINS (domainid,name,description,enabled) values(?, ?, ?, ?)";
-            PreparedStatement statement = conn.prepareStatement(query);
+        String query = "insert into DOMAINS (domainid,name,description,enabled) values(?, ?, ?, ?)";
+        try (Connection conn = dbConnect();
+             PreparedStatement statement = conn.prepareStatement(query)) {
             statement.setString(1, domain.getName());
             statement.setString(2, domain.getName());
             statement.setString(3, domain.getDescription());
@@ -209,10 +107,9 @@ public class DomainStore {
             }
             domain.setDomainid(domain.getName());
             return domain;
-        } catch (SQLException s) {
-            throw new StoreException("SQL Exception : " + s);
-        } finally {
-            dbClose();
+        } catch (SQLException e) {
+            LOG.error("Error creating domain {}", domain.getName(), e);
+            throw new StoreException("Error creating domain", e);
         }
     }
 
@@ -232,19 +129,16 @@ public class DomainStore {
             savedDomain.setEnabled(domain.isEnabled());
         }
 
-        Connection conn = dbConnect();
-        try {
-            String query = "UPDATE DOMAINS SET description = ?, enabled = ? WHERE domainid = ?";
-            PreparedStatement statement = conn.prepareStatement(query);
+        String query = "UPDATE DOMAINS SET description = ?, enabled = ? WHERE domainid = ?";
+        try (Connection conn = dbConnect();
+             PreparedStatement statement = conn.prepareStatement(query)) {
             statement.setString(1, savedDomain.getDescription());
             statement.setInt(2, savedDomain.isEnabled() ? 1 : 0);
             statement.setString(3, savedDomain.getDomainid());
             statement.executeUpdate();
-            statement.close();
-        } catch (SQLException s) {
-            throw new StoreException("SQL Exception : " + s);
-        } finally {
-            dbClose();
+        } catch (SQLException e) {
+            LOG.error("Error updating domain {}", domain.getDomainid(), e);
+            throw new StoreException("Error updating domain", e);
         }
 
         return savedDomain;
@@ -255,19 +149,16 @@ public class DomainStore {
         if (deletedDomain == null) {
             return null;
         }
-        Connection conn = dbConnect();
-        try {
-            String query = "DELETE FROM DOMAINS WHERE domainid = ?";
-            PreparedStatement statement = conn.prepareStatement(query);
+        String query = "DELETE FROM DOMAINS WHERE domainid = ?";
+        try (Connection conn = dbConnect();
+             PreparedStatement statement = conn.prepareStatement(query)) {
             statement.setString(1, deletedDomain.getDomainid());
             int deleteCount = statement.executeUpdate();
             LOG.debug("deleted {} records", deleteCount);
-            statement.close();
             return deletedDomain;
-        } catch (SQLException s) {
-            throw new StoreException("SQL Exception : " + s);
-        } finally {
-            dbClose();
+        } catch (SQLException e) {
+            LOG.error("Error deleting domain {}", domainid, e);
+            throw new StoreException("Error deleting domain", e);
         }
     }
 }
--- aaa-h2-store/src/main/java/org/opendaylight/aaa/h2/persistence/RoleStore.java
+++ aaa-h2-store/src/main/java/org/opendaylight/aaa/h2/persistence/RoleStore.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2015 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+ * Copyright (c) 2014, 2016 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -11,13 +11,9 @@ package org.opendaylight.aaa.h2.persistence;
 import com.google.common.base.Preconditions;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.opendaylight.aaa.api.IDMStoreUtil;
 import org.opendaylight.aaa.api.model.Role;
@@ -30,77 +26,29 @@ import org.slf4j.LoggerFactory;
  * @author peter.mellquist@hp.com
  *
  */
-public class RoleStore {
+public class RoleStore extends AbstractStore<Role> {
     private static final Logger LOG = LoggerFactory.getLogger(RoleStore.class);
-    protected Connection dbConnection = null;
+
     protected final static String SQL_ID = "roleid";
     protected final static String SQL_DOMAIN_ID = "domainid";
     protected final static String SQL_NAME = "name";
     protected final static String SQL_DESCR = "description";
-    public final static int MAX_FIELD_LEN = 128;
+    private static final String TABLE_NAME = "ROLES";
 
     protected RoleStore() {
-    }
-
-    protected Connection getDBConnect() throws StoreException {
-        dbConnection = H2Store.getConnection(dbConnection);
-        return dbConnection;
-    }
-
-    protected void dbClean() throws StoreException, SQLException {
-        Connection c = dbConnect();
-        String sql = "delete from ROLES where true";
-        c.createStatement().execute(sql);
-        c.close();
-    }
-
-    protected Connection dbConnect() throws StoreException {
-        Connection conn;
-        try {
-            conn = getDBConnect();
-        } catch (StoreException se) {
-            throw se;
-        }
-        try {
-            DatabaseMetaData dbm = conn.getMetaData();
-            String[] tableTypes = { "TABLE" };
-            ResultSet rs = dbm.getTables(null, null, "ROLES", tableTypes);
-            if (rs.next()) {
-                LOG.debug("roles Table already exists");
-            } else {
-                LOG.info("roles Table does not exist, creating table");
-                Statement stmt = null;
-                stmt = conn.createStatement();
-                String sql = "CREATE TABLE ROLES " + "(roleid     VARCHAR(128)   PRIMARY KEY,"
-                        + "name        VARCHAR(128)   NOT NULL, "
-                        + "domainid    VARCHAR(128)   NOT NULL, "
-                        + "description VARCHAR(128)      NOT NULL)";
-                stmt.executeUpdate(sql);
-                stmt.close();
-            }
-        } catch (SQLException sqe) {
-            throw new StoreException("Cannot connect to database server " + sqe);
-        }
-        return conn;
-    }
-
-    protected void dbClose() {
-        if (dbConnection != null) {
-            try {
-                dbConnection.close();
-            } catch (Exception e) {
-                LOG.error("Cannot close Database Connection", e);
-            }
-        }
+        super(TABLE_NAME);
     }
 
     @Override
-    protected void finalize() throws Throwable {
-        dbClose();
-        super.finalize();
+    protected String getTableCreationStatement() {
+        return "CREATE TABLE ROLES "
+                + "(roleid     VARCHAR(128)   PRIMARY KEY,"
+                + "name        VARCHAR(128)   NOT NULL, "
+                + "domainid    VARCHAR(128)   NOT NULL, "
+                + "description VARCHAR(128)      NOT NULL)";
     }
 
-    protected Role rsToRole(ResultSet rs) throws SQLException {
+    protected Role fromResultSet(ResultSet rs) throws SQLException {
         Role role = new Role();
         try {
             role.setRoleid(rs.getString(SQL_ID));
@@ -116,50 +64,19 @@ public class RoleStore {
 
     protected Roles getRoles() throws StoreException {
         Roles roles = new Roles();
-        List<Role> roleList = new ArrayList<Role>();
-        Connection conn = dbConnect();
-        Statement stmt = null;
-        String query = "SELECT * FROM roles";
-        try {
-            stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-            while (rs.next()) {
-                Role role = rsToRole(rs);
-                roleList.add(role);
-            }
-            rs.close();
-            stmt.close();
-        } catch (SQLException s) {
-            throw new StoreException("SQL Exception : " + s);
-        } finally {
-            dbClose();
-        }
-        roles.setRoles(roleList);
+        roles.setRoles(listAll());
         return roles;
     }
 
     protected Role getRole(String id) throws StoreException {
-        Connection conn = dbConnect();
-        try {
-            PreparedStatement pstmt = conn
-                    .prepareStatement("SELECT * FROM ROLES WHERE roleid = ? ");
+        try (Connection conn = dbConnect();
+             PreparedStatement pstmt = conn
+                     .prepareStatement("SELECT * FROM ROLES WHERE roleid = ? ")) {
             pstmt.setString(1, id);
             LOG.debug("query string: {}", pstmt.toString());
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                Role role = rsToRole(rs);
-                rs.close();
-                pstmt.close();
-                return role;
-            } else {
-                rs.close();
-                pstmt.close();
-                return null;
-            }
+            return firstFromStatement(pstmt);
         } catch (SQLException s) {
             throw new StoreException("SQL Exception: " + s);
-        } finally {
-            dbClose();
         }
     }
 
@@ -167,10 +84,9 @@ public class RoleStore {
         Preconditions.checkNotNull(role);
         Preconditions.checkNotNull(role.getName());
         Preconditions.checkNotNull(role.getDomainid());
-        Connection conn = dbConnect();
-        try {
-            String query = "insert into roles (roleid,domainid,name,description) values(?,?,?,?)";
-            PreparedStatement statement = conn.prepareStatement(query);
+        String query = "insert into roles (roleid,domainid,name,description) values(?,?,?,?)";
+        try (Connection conn = dbConnect();
+             PreparedStatement statement = conn.prepareStatement(query)) {
             role.setRoleid(IDMStoreUtil.createRoleid(role.getName(), role.getDomainid()));
             statement.setString(1, role.getRoleid());
             statement.setString(2, role.getDomainid());
@@ -183,8 +99,6 @@ public class RoleStore {
             return role;
         } catch (SQLException s) {
             throw new StoreException("SQL Exception : " + s);
-        } finally {
-            dbClose();
         }
     }
 
@@ -202,18 +116,14 @@ public class RoleStore {
             savedRole.setName(role.getName());
         }
 
-        Connection conn = dbConnect();
-        try {
-            String query = "UPDATE roles SET description = ? WHERE roleid = ?";
-            PreparedStatement statement = conn.prepareStatement(query);
+        String query = "UPDATE roles SET description = ? WHERE roleid = ?";
+        try (Connection conn = dbConnect();
+             PreparedStatement statement = conn.prepareStatement(query)) {
             statement.setString(1, savedRole.getDescription());
             statement.setString(2, savedRole.getRoleid());
             statement.executeUpdate();
-            statement.close();
         } catch (SQLException s) {
             throw new StoreException("SQL Exception : " + s);
-        } finally {
-            dbClose();
         }
 
         return savedRole;
@@ -225,19 +135,15 @@ public class RoleStore {
             return null;
         }
 
-        Connection conn = dbConnect();
-        try {
-            String query = "DELETE FROM DOMAINS WHERE domainid = ?";
-            PreparedStatement statement = conn.prepareStatement(query);
+        String query = "DELETE FROM DOMAINS WHERE domainid = ?";
+        try (Connection conn = dbConnect();
+             PreparedStatement statement = conn.prepareStatement(query)) {
             statement.setString(1, savedRole.getRoleid());
             int deleteCount = statement.executeUpdate(query);
             LOG.debug("deleted {} records", deleteCount);
-            statement.close();
             return savedRole;
         } catch (SQLException s) {
             throw new StoreException("SQL Exception : " + s);
-        } finally {
-            dbClose();
         }
     }
 }
