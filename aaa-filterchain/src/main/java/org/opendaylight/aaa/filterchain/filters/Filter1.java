e030470988a5f33fe900c67ab41551ca154b0984
Ryan Goulding <ryandgoulding@gmail.com>
2016-03-10 08:35:15 -0500

    Dynamic Filter Injection for Java REST Servlets
    Add support to dynamically configure servlet Filter chains at runtime.
    Recreates the Filter chain-of-responsiblity pattern to allow injection of chain
    links on top of the CustomFilterAdapter javax.servlet.Filter.  Thus, web.xml
    creators can use org.opendaylight.aaa.filterchian.CustomFilterAdapater to
    dynamically adjust links in the chain at runtime.  This framework allows
    pre/post-processing on HTTP/S requests from REST endpoints.  Importantly,
    since the Filter is added to the Servlet definition, the requests are viewed
    after SSL decryption, allowing for true inspection.  An example of how to
    configure this for a REST endpoint is illustrated in this patch for the AAA
    idmlight endpoints at aaa-idmlight/src/main/resources/web.xml.
    
    A configuration admin managed service is introduced to track changes to the
    "etc/org.opendaylight.aaa.filterchain.cfg" file.  This file supports one
    key/value combination, namely;
    
    customFilterList=a.b.c.Filter1,c.d.e.Filter2,x.y.zFilterN
    
    The value is a csv list of filters.  Optionally, the user may specify a Filter
    configuration file to introduce key/value init-params normally specified in
    web.xml.  An example is:
    
    customFilterList=a.b.c.Filter1$etc/filter1.cfg,d.e.f.Filter2
    
    If the desired filter is not included in the Imported aaa packages, it may be
    dynamically imported to allow access:
    
    karaf> bundle:dynamic-import <ID>
    
    Where <ID> refers to the bundle ID of the bundle that houses the desired
    Filter implementation.
    
    This patch contains several unit tests, and boasts 88% line unit coverage.  The
    testing includes null chains, small chains, and quite large chains.
    
    Change-Id: Ifa2994f4c10ae504763f704fa8dc19fd11093108
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

---
+++ aaa-filterchain/src/main/java/org/opendaylight/aaa/filterchain/filters/Filter1.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.aaa.filterchain.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Used to help guide configuration of a deployment.  If an operator is confused
+ * whether a Filter is reached/traversed, he or she can use this class to produce
+ * some basic karaf log output.
+ *
+ * This functionality is particularly useful when developing new Filter(s);  for
+ * example, you can sandwich the newly created filter in between filter1 and filter2
+ * to ensure that the chain is traversed correctly and in order.
+ *
+ * @author Ryan Goulding (ryandgoulding@gmail.com)
+ *
+ */
+public class Filter1 implements Filter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Filter1.class);
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response,
+            final FilterChain filterChain) throws IOException, ServletException {
+        LOG.error("INGRESS FILTER1");
+        filterChain.doFilter(request, response);
+        LOG.error("EGRESS FILTER1");
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+}
