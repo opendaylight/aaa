b065009aff2d08819a1a087d391695b623672387
Ryan Goulding <ryandgoulding@gmail.com>
2016-03-10 08:35:15 -0500

    Dynamic Filter Injection for Java REST Servlets
    Add support to dynamically configure servlet Filter chains at runtime.
    Recreates the Filter chain-of-responsiblity pattern to allow injection of chain
    links on top of the CustomFilterAdapter javax.servlet.Filter.  Thus, web.xml
    creators can use org.opendaylight.aaa.filterchian.CustomFilterAdapater to
    dynamically adjust links in the chain at runtime.  This framework allows
    pre/post-processing on HTTP/S requests from REST endpoints.  Importantly,
    since the Filter is added to the Servlet definition, the requests are viewed
    after SSL decryption, allowing for true inspection.  An example of how to
    configure this for a REST endpoint is illustrated in this patch for the AAA
    idmlight endpoints at aaa-idmlight/src/main/resources/web.xml.
    
    A configuration admin managed service is introduced to track changes to the
    "etc/org.opendaylight.aaa.filterchain.cfg" file.  This file supports one
    key/value combination, namely;
    
    customFilterList=a.b.c.Filter1,c.d.e.Filter2,x.y.zFilterN
    
    The value is a csv list of filters.  Optionally, the user may specify a Filter
    configuration file to introduce key/value init-params normally specified in
    web.xml.  An example is:
    
    customFilterList=a.b.c.Filter1$etc/filter1.cfg,d.e.f.Filter2
    
    If the desired filter is not included in the Imported aaa packages, it may be
    dynamically imported to allow access:
    
    karaf> bundle:dynamic-import <ID>
    
    Where <ID> refers to the bundle ID of the bundle that houses the desired
    Filter implementation.
    
    This patch contains several unit tests, and boasts 88% line unit coverage.  The
    testing includes null chains, small chains, and quite large chains.
    
    Change-Id: Ifa2994f4c10ae504763f704fa8dc19fd11093108
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

---
+++ aaa-filterchain/src/main/java/org/opendaylight/aaa/filterchain/filters/AAAFilterChain.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.aaa.filterchain.filters;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Recreates the <code>javax.servlet.Filter</code> chain of responsibility
+ * Pattern to allow for programatic injection of Filters.  Essentially, the
+ * links of the injected chain are traversed, and if the Requests makes it
+ * through all of the injected Filters, then the original, existing chain
+ * is maintained.
+ *
+ * This revision of code assumes that the url-pattern for injected filters
+ * is exactly the same as the one specified for <code>CustomFilterAdapter</code>.
+ *
+ * @author Ryan Goulding (ryandgoulding@gmail.com)
+ *
+ */
+public final class AAAFilterChain implements FilterChain {
+
+    // Must be stored locally to be used within javax.servlet.FilterChain.doFilter(
+    // ServletRequest, ServletResponse) due to rigid API contract.
+    private volatile Iterator<Filter> injectedFilterChainIterator;
+    private volatile FilterChain existingFilterChain;
+
+    private AAAFilterChain() {
+    }
+
+    public static AAAFilterChain createAAAFilterChain() {
+        return new AAAFilterChain();
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response)
+            throws IOException, ServletException {
+
+        // Recursive call using the next available link in the chain iterator If a "next" link does
+        // not exist and we have traversed the chain thus far, that means the Request has successfully
+        // traversed the injected filter chain links and we can invoke filtering on the existing chain.
+        if (injectedFilterChainIterator.hasNext()) {
+            injectedFilterChainIterator.next().doFilter(request, response, this);
+        } else {
+            existingFilterChain.doFilter(request, response);
+        }
+    }
+
+    /**
+     * A wrapper method used to inject a new Filter chain.  Essentially, this just adds links to the existing chain.
+     *
+     * @param request Wrapped parameter passed directly to <code>doFilter(ServletRequest, ServletResponse)</code>
+     * @param response Wrapped parameter passed directly to <code>doFilter(ServletRequest, ServletResponse)</code>
+     * @param existingFilterChain The chain provided from Jersey as defined in the Servlet's <code>web.xml</code>
+     * @param injectedFilterChain The programmatically injected chain, which may be empty
+     * @throws IOException Wrapped exception handling from <code>doFilter(ServletRequest, ServletResponse)</code>
+     * @throws ServletException Wrapped exception handling from <code>doFilter(ServletRequest, ServletResponse)</code>
+     */
+    public void doFilter(final ServletRequest request, final ServletResponse response,
+            final FilterChain existingFilterChain, final List<Filter> injectedFilterChain)
+                    throws IOException, ServletException {
+
+        this.existingFilterChain = existingFilterChain;
+        this.injectedFilterChainIterator = injectedFilterChain.iterator();
+        doFilter(request, response);
+    }
+}
