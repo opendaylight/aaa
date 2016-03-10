c882c46eb36a54ee7d4964c8ee177e9187d7dd54
Ryan Goulding <ryandgoulding@gmail.com>
2016-03-10 08:35:15 -0500

    Dynamic Filter Injection for Java REST Servlets
    Add support to dynamically configure servlet Filter chains at runtime.
    Recreates the Filter chain-of-responsiblity pattern to allow injection of chain
    links on top of the CustomFilterAdapter javax.servlet.Filter.  Thus, web.xml
    creators can use org.opendaylight.aaa.filterchian.CustomFilterAdapater to
    dynamically adjust links in the chain at runtime.  This framework allows
    pre/post-processing on HTTP/S requests from REST endpoints.  Importantly,
    since the Filter is added to the Servlet definition, the requests are viewed
    after SSL decryption, allowing for true inspection.  An example of how to
    configure this for a REST endpoint is illustrated in this patch for the AAA
    idmlight endpoints at aaa-idmlight/src/main/resources/web.xml.
    
    A configuration admin managed service is introduced to track changes to the
    "etc/org.opendaylight.aaa.filterchain.cfg" file.  This file supports one
    key/value combination, namely;
    
    customFilterList=a.b.c.Filter1,c.d.e.Filter2,x.y.z.FilterN
    
    The value is a csv list of filters.  Optionally, the user may specify a Filter
    configuration file to introduce key/value init-params normally specified in
    web.xml.  An example is:
    
    a.b.c.Filter1.paramKey=paramValue
    
    If the desired filter is not exported by the a.b.c bundle, it may be
    dynamically imported to allow access:
    
    karaf> bundle:dynamic-import <ID>
    
    Where <ID> refers to the bundle ID of the bundle that houses the desired
    Filter implementation.
    
    This patch contains several unit tests, and boasts 88% line unit coverage.  The
    testing includes null chains, small chains, and quite large chains.
    
    Change-Id: Ifa2994f4c10ae504763f704fa8dc19fd11093108
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

---
+++ aaa-filterchain/src/main/java/org/opendaylight/aaa/filterchain/Activator.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.aaa.filterchain;
+
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
+import org.opendaylight.aaa.filterchain.configuration.CustomFilterAdapterConfiguration;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.ManagedService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Activator for <code>aaa-filterchain</code>, a bundle which provides the ability
+ * to inject custom <code>Filter</code>(s) in front of servlets.
+ *
+ * This class is also responsible for offering contextual <code>DEBUG</code>
+ * level clues concerning the activation of the <code>aaa-filterchain</code> bundle.
+ * To enable these debug messages, issue the following command in the karaf
+ * shell: <code>log:set debug org.opendaylight.aaa.filterchain.Activator</code>
+ *
+ * @author Ryan Goulding (ryandgoulding@gmail.com)
+ */
+public class Activator extends DependencyActivatorBase {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
+
+    private ServiceRegistration<ManagedService> managedServiceServiceRegistration;
+
+    @Override
+    public void destroy(final BundleContext bc, final DependencyManager dm) throws Exception {
+        LOG.debug("Destroying the aaa-filterchain bundle");
+        managedServiceServiceRegistration.unregister();
+    }
+
+    @Override
+    public void init(final BundleContext bc, final DependencyManager dm) throws Exception {
+        LOG.debug("Initializing the aaa-filterchain bundle");
+        // Register the CustomFilterAdapterConfiguration ManagedService with the
+        // BundleContext so config values can be loaded from the config admin
+        managedServiceServiceRegistration = bc.registerService(ManagedService.class,
+                CustomFilterAdapterConfiguration.getInstance(),
+                CustomFilterAdapterConfiguration.getInstance().getDefaultProperties());
+    }
+
+}
