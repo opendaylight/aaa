4913f7715576435b9d0143a15346d550afc5b749
Mohamed El-Serngawy <melserngawy@inocybe.com>
2016-03-21 16:48:23 -0400

    WIP (don't merge) using mdsal to store certificates
    Change-Id: I29ea84e4f2be9d66f7da74727baaf9ba343d1f9f
    Signed-off-by: Mohamed El-Serngawy <melserngawy@inocybe.com>

---
+++ aaa-cert/src/main/java/org/opendaylight/aaa/cert/api/IAaaCertMdsalProvider.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2015 Inocybe Technologies. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.aaa.cert.api;
+
+import java.security.KeyStore;
+
+/**
+ * @author mserngawy
+ *
+ */
+public interface IAaaCertMdsalProvider {
+
+    boolean addCertificateODLKeyStore(String storePasswd, String alias, String certificate);
+
+    boolean addCertificateTrustStore(String storePasswd, String alias, String certificate);
+
+    String createODLKeyStore(String keyStore, String storePasswd, String alias, String dName, int validity);
+
+    String createTrustKeyStore(String keyStore, String storePasswd, String alias);
+
+    String genODLKeyStorCertificateReq(String storePasswd, String alias);
+
+    String getCertificateTrustStore(String storePasswd, String aliase);
+
+    String getODLKeyStorCertificate(String storePasswd, String alias);
+
+    KeyStore getODLKeyStore();
+
+    KeyStore getTrustKeyStore();
+}
