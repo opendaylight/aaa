57367d1904913b396ac19b232e943e6b421074b0
Sharon Aicler <saichler@cisco.com>
2016-04-26 17:38:13 -0700

    Encryption Service For AAA that uses a unified key to encryp and decrypt string for usage in ODL
    Change-Id: Ic2d576c3c8ed42f3f7fc42afeac3af78a847febd
    Signed-off-by: Sharon Aicler <saichler@cisco.com>

---
+++ aaa-encrypt-service/src/main/java/org/opendaylight/aaa/encrypt/AAAEncryptionServiceImpl.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.encrypt;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import org.opendaylight.yang.gen.v1.config.aaa.authn.encrypt.service.rev160426.AAAEncryptServiceModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*
+ *  @author - Sharon Aicler (saichler@gmail.com)
+ */
+public class AAAEncryptionServiceImpl implements AAAEncryptionService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AAAEncryptionServiceImpl.class);
+
+    private final SecretKey key;
+    private final IvParameterSpec ivspec;
+    private final Cipher encryptCipher;
+    private final Cipher decryptCipher;
+    private final String encryptTag;
+
+    public AAAEncryptionServiceImpl(AAAEncryptServiceModule module) {
+        SecretKey tempKey = null;
+        IvParameterSpec tempIvSpec = null;
+        this.encryptTag = module.getEncryptTag();
+        final byte[] enryptionKeySalt = module.getEncryptionKeySalt();
+        try {
+            final SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(module.getEncryptMethod());
+            final KeySpec spec = new PBEKeySpec(module.getEncryptKey().toCharArray(), enryptionKeySalt, module.getEncryptIterationCount(), module.getEncryptKeyLength());
+            tempKey = keyFactory.generateSecret(spec);
+            tempKey = new SecretKeySpec(tempKey.getEncoded(), module.getEncryptType());
+            tempIvSpec = new IvParameterSpec(enryptionKeySalt);
+        } catch(NoSuchAlgorithmException | InvalidKeySpecException e) {
+            LOG.error("Failed to initialize secret key", e);
+        }
+
+        key = tempKey;
+        ivspec = tempIvSpec;
+        Cipher c = null;
+        try {
+            c = Cipher.getInstance(module.getCipherTransforms());
+            c.init(Cipher.ENCRYPT_MODE, key, ivspec);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException | InvalidKeyException e) {
+            LOG.error("Failed to create encrypt cipher.", e);
+        }
+        this.encryptCipher = c;
+        c = null;
+        try {
+            c = Cipher.getInstance(module.getCipherTransforms());
+            c.init(Cipher.DECRYPT_MODE, key, ivspec);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException | InvalidKeyException e) {
+            LOG.error("Failed to create decrypt cipher.", e);
+        }
+        this.decryptCipher = c;
+    }
+
+    @Override
+    public String encrypt(String data) {
+        //We could not instantiate the encryption key, hence no encryption or decryption will be done.
+        if (key == null) {
+            LOG.warn("Encryption Key is NULL, will not encrypt data.");
+            return data;
+        }
+
+        try {
+            synchronized(encryptCipher) {
+                byte[] cryptobytes = encryptCipher.doFinal(data.getBytes());
+                String cryptostring = DatatypeConverter.printBase64Binary(cryptobytes);
+                return encryptTag + cryptostring;
+            }
+        } catch (IllegalBlockSizeException | BadPaddingException e) {
+            LOG.error("Failed to encrypt data.",e);
+        }
+
+        return data;
+    }
+
+    @Override
+    public String decrypt(String encData) {
+        if (key == null || encData == null || encData.length() == 0 || !encData.startsWith(encryptTag)) {
+            LOG.warn("String {} was not decrypted.",encData);
+            return encData;
+        }
+
+        try{
+            byte[] cryptobytes = DatatypeConverter.parseBase64Binary(encData.substring(encryptTag.length()));
+            byte[] clearbytes = decryptCipher.doFinal(cryptobytes);
+            return new String(clearbytes);
+        } catch (IllegalBlockSizeException | BadPaddingException e){
+            LOG.error("Failed to decrypt encoded data",e);
+        }
+        return encData;
+    }
+}
---
+++ aaa-encrypt-service/src/test/java/org/opendaylight/aaa/encrypt/AAAEncryptServiceImplTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2016 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.encrypt;
+
+import static org.mockito.Mockito.mock;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.opendaylight.yang.gen.v1.config.aaa.authn.encrypt.service.rev160426.AAAEncryptServiceModule;
+
+
+/*
+ *  @author - Sharon Aicler (saichler@gmail.com)
+ */
+public class AAAEncryptServiceImplTest {
+
+    private AAAEncryptServiceModule module = mock(AAAEncryptServiceModule.class);
+    private AAAEncryptionServiceImpl impl = null;
+
+    private static final String ENCRYPT_KEY = "Test Key";
+    private static final byte[] ENCRYPT_SALT = new byte[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+    private static final String ENCRYPTED_TAG = "Encrypted:";
+    private static final String ENCRYPTION_METHOD = "PBKDF2WithHmacSHA1";
+    private static final String ENCRYPTION_TYPE = "AES";
+    private static final int ENCRYPTION_ITERATION_COUNT = 32768;
+    private static final int ENCRYPTION_KEY_LENGTH = 128;
+    private static final String CIPHER_TRANSFORMS = "AES/CBC/PKCS5Padding";
+
+    @Before
+    public void setup(){
+
+        Mockito.when(module.getEncryptionKeySalt()).thenReturn(ENCRYPT_SALT);
+        Mockito.when(module.getEncryptKey()).thenReturn(ENCRYPT_KEY);
+        Mockito.when(module.getEncryptTag()).thenReturn(ENCRYPTED_TAG);
+        Mockito.when(module.getEncryptMethod()).thenReturn(ENCRYPTION_METHOD);
+        Mockito.when(module.getEncryptType()).thenReturn(ENCRYPTION_TYPE);
+        Mockito.when(module.getEncryptIterationCount()).thenReturn(ENCRYPTION_ITERATION_COUNT);
+        Mockito.when(module.getEncryptKeyLength()).thenReturn(ENCRYPTION_KEY_LENGTH);
+        Mockito.when(module.getCipherTransforms()).thenReturn(CIPHER_TRANSFORMS);
+        impl = new AAAEncryptionServiceImpl(module);
+    }
+
+    @Test
+    public void testShortString(){
+        String before = "shortone";
+        String encrypt = impl.encrypt(before);
+        Assert.assertNotEquals(before,encrypt);
+        String after = impl.decrypt(encrypt);
+        Assert.assertEquals(before,after);
+    }
+
+    @Test
+    public void testLongString(){
+        String before = "This is a very long string to encrypt for testing 1...2...3";
+        String encrypt = impl.encrypt(before);
+        Assert.assertNotEquals(before,encrypt);
+        String after = impl.decrypt(encrypt);
+        Assert.assertEquals(before,after);
+    }
+}
