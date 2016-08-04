4b3be060c9b21c5ca297f3bbd7933a57f223ec60
Ryan Goulding <ryandgoulding@gmail.com>
2016-08-04 04:45:30 -0400

    Add groupRolesMap configuration option for ODLJndiLdapRealm
    Shiro provides a nice configuration option called groupRolesMap for
    ActiveDirectoryRealm.  Since JndiLdapRealm provides a default
    getAuthorizationInfo() that just returns null, it does not perform
    any authorization.  ODLJndiLdapRealm was designed to add a useful
    getAuthorizationInfo() implementation, which performs LDAP queries
    to determine LDAP membership information.
    
    This patch adds the groupRolesMap functionality to ODLJndiLdapRealm
    so that raw LDAP results can be mapped to ODL roles.  This essentially
    allows existing systems to be utilized without either recreating the
    group structure in LDAP or role structure in ODL in order to map
    correctly.
    
    Change-Id: Id9f3bf5ca8f171e3c51e0c39867e70341eda1901
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

---
+++ aaa-shiro/src/main/java/org/opendaylight/aaa/shiro/realm/mapping/impl/BestAttemptGroupToRolesMappingStrategy.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.aaa.shiro.realm.mapping.impl;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import org.opendaylight.aaa.shiro.realm.mapping.api.GroupsToRolesMappingStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Maps groups to roles if any association exists.
+ *
+ * @author Ryan Goulding (ryandgoulding@gmail.com)
+ */
+public class BestAttemptGroupToRolesMappingStrategy implements GroupsToRolesMappingStrategy {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BestAttemptGroupToRolesMappingStrategy.class);
+
+    @Override
+    public Map<String, Set<String>> mapGroupsToRoles(final Collection<String> groupNames, final String delimiter,
+                                                     final Map<String, String> groupRolesMap) {
+
+        final ImmutableMap.Builder<String, Set<String>> roleNamesBuilder = ImmutableMap.builder();
+
+        if (groupRolesMap != null) {
+            for (String groupName : groupNames) {
+                final String roleNamesString = groupRolesMap.get(groupName);
+
+                LOG.debug("association discovered: groupName={} and roleNamesString={}", groupName, roleNamesString);
+                if (roleNamesString != null) {
+                    final String[] roleNames = roleNamesString.split(delimiter);
+                    final ImmutableSet.Builder<String> rolesSet = ImmutableSet.builder();
+                    for (String roleName : roleNames) {
+                        rolesSet.add(roleName);
+                    }
+                    roleNamesBuilder.put(groupName, rolesSet.build());
+                }
+            }
+        }
+
+        return roleNamesBuilder.build();
+
+    }
+}
