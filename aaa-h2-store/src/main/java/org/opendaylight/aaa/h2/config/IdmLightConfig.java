9daa103815cbbb6149968fd00e45a1a5f29be8c9
Ryan Goulding <ryandgoulding@gmail.com>
2016-08-08 02:21:09 -0400

    Make the h2 database properties configurable
    Hitherto, AAA has had the limitation of static h2 database configuration.
    Since the H2Config is stored in a Java file, the only way to modify h2
    properties is to recompile the bundle with the new properties specified,
    which is obviously not too user friendly.  Note, the credentials here are
    not ODL AAA credentials;  rather, they are the credentials for the backing
    h2 data store.  In most cases, they may be divergent from the credentials you
    use to access ODL's restful web services!
    
    This change introduces a new concept called h2.cfg, which is located in
    the etc folder.  The idea behind h2.cfg is to expose some of the h2 configuration
    so the user can modify as appropriate to his/her setup.  h2.cfg was designed
    as a simple key/value list, and utilizes java.util.Properties.loadProperties(...)
    to load and parse property values.  This approach was preferred over the
    ManagedService approach, which was tried initially, but found to be inadequate
    since the configuration ManagedService did not necessarily load before the h2
    service causing the defaults to be used.  This level of non-determinism made
    the java.util.Properties approach preferred and the obvious choice.
    
    This change allows configuration of the following database properties:
    
    1) username  (kept the same by default for backwards compatibility)
    2) password  (kept the same by default for backwards compatibility)
    3) connectionString  (kept the same by default for backwards compatability)
    
    The first two values are trivial to set.  The third value ("connectionString")
    is a bit trickier;  the recommendation is to keep this value in the form:
    
    connectionString = jdbc:h2:[[INSERT_YOUR_IDMLIGHT_DB_PATH_HERE]]
    
    for example:
    
    connectionString = jdbc:h2:./aaa.db
    
    The first two parts should not be altered;  if you wish to use a different JDBC
    implementation, then you should consider pursuing configuration based on
    org.opendaylight.aaa.shiro.realm.ODLJdbcRealm instead.
    
    An important note is that if these values are changed, then you need to make the
    corresponding changes to your database (i.e., add the user, change the password,
    etc.).  If you wish to not use defaults, it is recommended to either:
    
    1) lay down the h2.cfg file as part of an external installer prior to installing
       odl-restconf or odl-aaa-shiro.
    
    or
    
    2) After changing the values in h2.cfg, stop the karaf process, delete the database
    file (idmlight.db.mv.db by default), then start the karaf process again.  The
    h2 initialization code will automatically rebuild a default version of the database.
    
    This adds a necessary level of security abstraction on top of the existing
    implementation;  hard-coded credentials are never good, even if they are local
    to a file-based database.
    
    Change-Id: I158b45066b40f9e06d0b132e4946c4a55fe767f0
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- aaa-h2-store/src/main/java/org/opendaylight/aaa/h2/config/IdmLightConfig.java
+++ aaa-h2-store/src/main/java/org/opendaylight/aaa/h2/config/IdmLightConfig.java
@@ -8,24 +8,37 @@
 
 package org.opendaylight.aaa.h2.config;
 
+import com.google.common.collect.ImmutableMap;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Responsible for providing configuration properties for the IDMLight/H2
- * data store implementation.
+ * Responsible for providing configuration properties for the H2 data store
+ * implementation.  IdmLightConfig only exposes properties that are allowed to
+ * be configured within the scope of ODL.  Namely, the following are exposed:
+ * 1) username
+ * 2) password
+ * 3) connectionString
  *
  * @author peter.mellquist@hp.com
- *
+ * @author Ryan Goulding (ryandgoulding@gmail.com)
  */
 public class IdmLightConfig {
 
     private static final Logger LOG = LoggerFactory.getLogger(IdmLightConfig.class);
 
     /**
-     * The default timeout for db connections in seconds.
+     * The key used to represent h2 database password in the external configuration (<code>etc/h2.cfg</code>)
+     * as well as internally in the properties map.
      */
-    private static final int DEFAULT_DB_TIMEOUT = 3;
+    private static final String PASSWORD_KEY = "password";
 
     /**
      * The default password for the database
@@ -33,101 +46,118 @@ public class IdmLightConfig {
     private static final String DEFAULT_PASSWORD = "bar";
 
     /**
+     * The key used to represent h2 database username in the external configuration (<code>etc/h2.cfg</code>)
+     * as well as internally in the properties map.
+     */
+    private static final String USERNAME_KEY = "username";
+
+    /**
      * The default username for the database
      */
     private static final String DEFAULT_USERNAME = "foo";
 
     /**
-     * The default driver for the databse is H2;  a pure-java implementation
-     * of JDBC.
+     * The key used to represent h2 database connectionString in the external configuration (<code>etc/h2.cfg</code>)
+     * as well as internally in the properties map.
      */
-    private static final String DEFAULT_JDBC_DRIVER = "org.h2.Driver";
+    private static final String CONNECTION_STRING_KEY = "connectionString";
 
     /**
      * The default connection string includes the intention to use h2 as
      * the JDBC driver, and the path for the file is located relative to
      * KARAF_HOME.
      */
-    private static final String DEFAULT_CONNECTION_STRING = "jdbc:h2:./";
+    private static final String DEFAULT_CONNECTION_STRING = "jdbc:h2:./idmlight.db";
 
     /**
-     * The default filename for the database file.
+     * The configuration file is located at <code>etc/h2.cfg</code> relative to the karaf home directory.
      */
-    private static final String DEFAULT_IDMLIGHT_DB_FILENAME = "idmlight.db";
+    private static final String H2_PROPERTIES_FILE = "etc" + File.separator + "h2.cfg";
 
     /**
-     * The database filename
+     * Map of all h2 related properties that are exposed to the user for configuration.
      */
-    private String dbName;
+    private volatile Map<String, String> properties = new HashMap<>();
 
     /**
-     * the database connection string
-     */
-    private String dbPath;
-
-    /**
-     * The database driver (i.e., H2)
-     */
-    private String dbDriver;
-
-    /**
-     * The database password.  This is not the same as AAA credentials!
-     */
-    private String dbUser;
-
-    /**
-     * The database username.  This is not the same as AAA credentials!
-     */
-    private String dbPwd;
-
-    /**
-     * Timeout for database connections in seconds
-     */
-    private int dbValidTimeOut;
-
-    /**
-     * Creates an valid database configuration using default values.
+     * Creates the database configuration using properties supplied via etc/h2.cfg, or defaults
+     * if no such properties exist.
      */
     public IdmLightConfig() {
-        // TODO make this configurable
-        dbName = DEFAULT_IDMLIGHT_DB_FILENAME;
-        dbPath = DEFAULT_CONNECTION_STRING + dbName;
-        dbDriver = DEFAULT_JDBC_DRIVER;
-        dbUser = DEFAULT_USERNAME;
-        dbPwd = DEFAULT_PASSWORD;
-        dbValidTimeOut = DEFAULT_DB_TIMEOUT;
+        synchronized (this) {
+            final Properties properties = getProperties();
+            final ImmutableMap.Builder<String, String> map = ImmutableMap.builder();
+            if (properties.isEmpty()) {
+                LOG.info("No properties were found/loaded from {}", H2_PROPERTIES_FILE);
+            }
+
+            setupProperty(properties, map, USERNAME_KEY, DEFAULT_USERNAME, false);
+            setupProperty(properties, map, PASSWORD_KEY, DEFAULT_PASSWORD, true);
+            setupProperty(properties, map, CONNECTION_STRING_KEY, DEFAULT_CONNECTION_STRING, false);
+
+            this.properties = map.build();
+        }
     }
 
     /**
-     * Outputs some debugging information surrounding idmlight config
+     * Helper method to set up a property.  Sets the value if it exists in <code>etc/h2.cfg</code> or
+     * to the default otherwise.
+     *
+     * @param properties read from <code>etc/h2.cfg</code>
+     * @param map the partial map which is side-effected with appropriate values
+     * @param key the key for the parsed property
+     * @param defaultValue the default value if the parsed property is not provided
+     * @param obscure set <code>true</code> to prevent secure data from being logged
      */
-    public void log() {
-        LOG.info("DB Path                 : {}", dbPath);
-        LOG.info("DB Driver               : {}", dbDriver);
-        LOG.info("DB Valid Time Out       : {}", dbValidTimeOut);
+    private static void setupProperty(final Properties properties, final ImmutableMap.Builder<String, String> map,
+                                      final String key, final String defaultValue, final boolean obscure) {
+
+        final String extracted = properties.getProperty(key);
+        final String setValue;
+        boolean useDefault = false;
+        if (extracted != null) {
+            setValue = extracted;
+        } else {
+            setValue = defaultValue;
+            useDefault = true;
+        }
+        map.put(key, setValue);
+        if (!obscure) {
+            LOG.info("The H2 database will use {} {}={}", (useDefault ? "default" : "extracted"), key, setValue);
+        } else {
+            LOG.info("The H2 database will use {} value for {}", (useDefault ? "default" : "extracted"), key);
+        }
     }
 
-    public String getDbName() {
-        return this.dbName;
+    /**
+     * Load properties from <code>etc/h2.cfg</code>.
+     *
+     * @return All parsed properties, even ones that do not apply to h2 configuration.
+     */
+    private Properties getProperties() {
+        final Properties properties = new Properties();
+        final InputStream inputStream;
+        try {
+            final File inputFile = new File(H2_PROPERTIES_FILE);
+            inputStream = new FileInputStream(inputFile);
+            properties.load(inputStream);
+            inputStream.close();
+        } catch(final IOException e) {
+            LOG.info("Unable to load properties from config path: {}", H2_PROPERTIES_FILE, e);
+        }
+        return properties;
     }
 
-    public String getDbPath() {
-        return this.dbPath;
+    public synchronized String getDbPath() {
+        return properties.get(CONNECTION_STRING_KEY);
     }
 
-    public String getDbDriver() {
-        return this.dbDriver;
+    public synchronized String getDbUser() {
+        return properties.get(USERNAME_KEY);
     }
 
-    public String getDbUser() {
-        return this.dbUser;
+    public synchronized String getDbPwd() {
+        return properties.get(PASSWORD_KEY);
     }
 
-    public String getDbPwd() {
-        return this.dbPwd;
-    }
-
-    public int getDbValidTimeOut() {
-        return this.dbValidTimeOut;
-    }
 }
