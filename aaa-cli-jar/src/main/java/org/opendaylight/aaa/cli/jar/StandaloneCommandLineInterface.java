6336466e964b90b9ac63c04f2f821479a0ce0a87
Michael Vorburger <vorburger@redhat.com>
2016-11-15 19:04:37 +0100

    New AAA CLI standalone JAR to create users and set passwords
    Change-Id: I0f9f991520128b53460b3ee80dbbe0b4b824ca5b
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ aaa-cli-jar/src/main/java/org/opendaylight/aaa/cli/jar/StandaloneCommandLineInterface.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.cli.jar;
+
+import com.google.common.base.Preconditions;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.opendaylight.aaa.api.IDMStoreException;
+import org.opendaylight.aaa.api.IDMStoreUtil;
+import org.opendaylight.aaa.api.IIDMStore;
+import org.opendaylight.aaa.api.model.User;
+import org.opendaylight.aaa.h2.config.IdmLightConfig;
+import org.opendaylight.aaa.h2.config.IdmLightConnectionFactory;
+import org.opendaylight.aaa.h2.persistence.H2Store;
+
+/**
+ * AAA CLI interface.
+ * This is for a "standalone Java" environment (i.e. plain JSE; non-OSGi, no Karaf).
+ *
+ * @author Michael Vorburger
+ */
+public class StandaloneCommandLineInterface {
+
+    private final IIDMStore identityStore;
+
+    public StandaloneCommandLineInterface(File directoryWithDatabaseFile) throws IOException {
+        IdmLightConfig config = new IdmLightConfig();
+        config.setDbDirectory(directoryWithDatabaseFile.getCanonicalPath());
+
+        H2Store h2Store = new H2Store(new IdmLightConnectionFactory(config));
+        this.identityStore = h2Store;
+    }
+
+    public List<String> getAllUserNames() throws IDMStoreException {
+        List<User> users = identityStore.getUsers().getUsers();
+        return users.stream().map(user -> user.getName()).collect(Collectors.toList());
+    }
+
+    public boolean resetPassword(String userIdWithoutDomain, String newPassword) throws IDMStoreException {
+        Preconditions.checkNotNull(userIdWithoutDomain, "userIdWithoutDomain == null");
+        String userId = IDMStoreUtil.createUserid(userIdWithoutDomain, IIDMStore.DEFAULT_DOMAIN);
+        User user = identityStore.readUser(userId);
+        if (user != null) {
+            user.setPassword(newPassword);
+            identityStore.updateUser(user);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void createNewUser(String userName, String newPassword) throws IDMStoreException {
+        Preconditions.checkNotNull(userName, "userName == null");
+        // TODO The following code should ideally be shared better between here, StoreBuilder & UserHandler
+        User newUser = new User();
+        newUser.setEnabled(true);
+        // do NOT setUserid here, because UserStore.createUser() does that
+        // by concatenating name & domainId using IDMStoreUtil.createUserid()
+        newUser.setName(userName);
+        newUser.setDomainid(IIDMStore.DEFAULT_DOMAIN);
+        newUser.setDescription(
+                userName + " user (created by " + StandaloneCommandLineInterface.class.getSimpleName() + ")");
+        newUser.setEmail("");
+        newUser.setPassword(newPassword);
+        identityStore.writeUser(newUser);
+    }
+}
---
+++ aaa-cli-jar/src/test/java/org/opendaylight/aaa/cli/jar/AbstractMainTest.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.cli.jar;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.any;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.opendaylight.yangtools.testutils.mockito.MoreAnswers;
+
+/**
+ * Unit Test of Main class with the argument parsing.
+ *
+ * @author Michael Vorburger
+ */
+public class AbstractMainTest {
+
+    private AbstractMain mockedMain() {
+        return Mockito.mock(AbstractMain.class, MoreAnswers.realOrException());
+    }
+
+    @Test
+    public void noArguments() throws Exception {
+        AbstractMain main = Mockito.spy(AbstractMain.class);
+        assertThat(main.parseArguments(new String[] {})).isEqualTo(-1);
+        Mockito.verify(main).printHelp(any());
+    }
+
+    @Test
+    public void unrecognizedArgument() throws Exception {
+        AbstractMain main = Mockito.spy(AbstractMain.class);
+        assertThat(main.parseArguments(new String[] { "saywhat" })).isEqualTo(-1);
+        Mockito.verify(main).unrecognizedOptions(Collections.singletonList("saywhat"));
+        Mockito.verify(main).printHelp(any());
+    }
+
+    /**
+     * Verify that allowsUnrecognizedOptions() is used, and "bad" arguments
+     * print help message instead of causing an UnrecognizedOptionException.
+     */
+    @Test
+    public void parsingError() throws Exception {
+        AbstractMain main = Mockito.spy(AbstractMain.class);
+        assertThat(main.parseArguments(new String[] { "-d" })).isEqualTo(-1);
+        Mockito.verify(main).printHelp(any());
+    }
+
+    @Test
+    public void exceptionWithoutX() throws Exception {
+        AbstractMain main = mockedMain();
+        Mockito.doThrow(new IllegalStateException()).when(main).printHelp(any());
+        assertThat(main.parseArguments(new String[] { "-?" })).isEqualTo(-2);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void exceptionWithX() throws Exception {
+        AbstractMain main = mockedMain();
+        Mockito.doThrow(new IllegalStateException()).when(main).printHelp(any());
+        assertThat(main.parseArguments(new String[] { "-hX" })).isEqualTo(-2);
+    }
+
+    @Test
+    public void onlyAUser() throws Exception {
+        assertThat(mockedMain().parseArguments(new String[] { "-X", "--nu", "admin" })).isEqualTo(-3);
+    }
+
+    @Test
+    public void onlyTwoUsers() throws Exception {
+        assertThat(mockedMain().parseArguments(new String[] { "-X", "-cu", "admin", "-cu", "auser" }))
+                .isEqualTo(-3);
+    }
+
+    @Test
+    public void userOptionWithoutArgument() throws Exception {
+        assertThat(mockedMain().parseArguments(new String[] { "-nu" })).isEqualTo(-2);
+    }
+
+    @Test
+    public void ifPasswordsThenEitherCreateOrChangeUser() throws Exception {
+        assertThat(mockedMain().parseArguments(new String[] { "-X", "-p", "newpass" })).isEqualTo(-6);
+    }
+
+    @Test
+    public void changeUserAndPassword() throws Exception {
+        AbstractMain main = Mockito.spy(AbstractMain.class);
+        assertThat(main.parseArguments(new String[] { "-X", "-cu", "admin", "-p", "newpass" })).isEqualTo(0);
+        Mockito.verify(main).setDbDirectory(new File("."));
+        Mockito.verify(main).resetPasswords(Collections.singletonList("admin"), Collections.singletonList("newpass"));
+    }
+
+    @Test
+    public void addNewUser() throws Exception {
+        AbstractMain main = Mockito.spy(AbstractMain.class);
+        assertThat(main.parseArguments(new String[] { "-X", "-nu", "admin", "-p", "newpass" })).isEqualTo(0);
+        Mockito.verify(main).setDbDirectory(new File("."));
+        Mockito.verify(main).addNewUsers(Collections.singletonList("admin"), Collections.singletonList("newpass"));
+    }
+
+    @Test
+    public void changeUserAndPasswordInNonDefaultDatabase() throws Exception {
+        AbstractMain main = Mockito.spy(AbstractMain.class);
+        assertThat(main.parseArguments(new String[] { "-X", "--dbd", "altDbDir", "-cu", "admin", "-p", "newpass" }))
+                .isEqualTo(0);
+        Mockito.verify(main).setDbDirectory(new File("altDbDir"));
+        Mockito.verify(main).resetPasswords(Collections.singletonList("admin"), Collections.singletonList("newpass"));
+    }
+
+    @Test
+    public void changeTwoUsersAndPasswords() throws Exception {
+        AbstractMain main = Mockito.spy(AbstractMain.class);
+        assertThat(main.parseArguments(
+                new String[] { "-X", "-cu", "admin", "-p", "newpass1", "-cu", "auser", "-p", "newpass2" }))
+                        .isEqualTo(0);
+        Mockito.verify(main).resetPasswords(Arrays.asList("admin", "auser"), Arrays.asList("newpass1", "newpass2"));
+    }
+
+    @Test
+    public void morePasswordsThanUsers() throws Exception {
+        assertThat(mockedMain().parseArguments(new String[] { "-X", "-cu", "admin", "-p", "newpass1", "-cu", "auser",
+            "-p", "newpass2", "-p", "newpass3" })).isEqualTo(-3);
+    }
+
+    @Test
+    public void cantAddAndChangeUsersTogether() throws Exception {
+        assertThat(
+                mockedMain().parseArguments(new String[] { "-X", "-cu", "admin", "-nu", "admin2", "-p", "newpass1" }))
+                        .isEqualTo(-5);
+    }
+
+}
