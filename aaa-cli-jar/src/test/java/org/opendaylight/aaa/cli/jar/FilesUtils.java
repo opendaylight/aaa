1b7fbc8aad6f55d987163f0eef48da6b00fd14f0
Michael Vorburger <vorburger@redhat.com>
2016-11-15 19:04:37 +0100

    New AAA CLI standalone JAR to create users and set passwords
    Change-Id: I0f9f991520128b53460b3ee80dbbe0b4b824ca5b
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ aaa-cli-jar/src/test/java/org/opendaylight/aaa/cli/jar/FilesUtils.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.cli.jar;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/**
+ * Utilities for Files.
+ *
+ * @author Michael Vorburger
+ */
+public final class FilesUtils {
+    private FilesUtils() {
+    }
+
+    public static void delete(String directory) throws IOException {
+        Path path = Paths.get(directory);
+        if (!path.toFile().exists()) {
+            return;
+        }
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                file.toFile().delete();
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                dir.toFile().delete();
+                if (exc != null) {
+                    throw exc;
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+}
