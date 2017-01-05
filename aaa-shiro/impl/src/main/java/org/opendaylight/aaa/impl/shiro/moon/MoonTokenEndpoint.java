2d90c601d1da7f936552e2469f58275db96be8cc
Ryan Goulding <ryandgoulding@gmail.com>
2017-01-05 15:52:47 -0500

    cherry-pick Migrate aaa-shiro to utilize archetype setup
    This change addresses the fact that the archetype was not used to
    create the aaa-shiro module.  This is due to the fact that it was
    much heavier weight than what was needed at the time.  However,
    utilization of the archetype does allow for many advantages including:
    
    1) Explicit separation of api and impl into two separate locations.
       Since prior to this change mostly Apache Shiro abstractions were used
       instead of homegrown ones, this wasn't particularly useful.  However,
       with aaa-shiro growing, this is becoming increasingly necessary.
    
    2) Dependence on config-parent for bundles.  This gets us a lot for
       free, including genericizising on best practices.
    
    3) The possibility to create aaa-shiro features/karaf/IT/commands more
       easily.  For now, this patch comments out the features & karaf
       section as they are pretty much duplicates of the existing top-level
       odl-aaa-shiro feature which is already defined.  In the future, it
       would be nice to enumerate some of the archetype-oriented features:
       - ui
       - rest
       - api
    
    This change is mostly cleaning up and preparing for expansion of the
    aaa-shiro bundle.  Existing functionality was stuck in the aaa-shiro module,
    but the package names were not updated to utilize impl.  Likewise, the module
    is called "aaa-shiro" and not "aaa-shiro-impl" since other projects already depend
    on the former name.  The package names were not updated to utilize impl
    as the names are used in configuration of the module itself, and
    changing them would cause forwards/backwards compatibility issues.  In the future,
    we may want to move them but provide existing classes that extend the impl ones.
    
    This change is used with a few others to add in dynamic authorization.  Because
    the change is adding in a significant security enhancement, it is a good
    candidate to cherry pick back into stable/boron.
    
    Change-Id: I16f1efed8b83e764362ae6d19b0a69d1b1c6cbec
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/impl/shiro/moon/MoonTokenEndpoint.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016 Orange and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.shiro.moon;
+
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MoonTokenEndpoint extends HttpServlet {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MoonTokenEndpoint.class);
+    private static final long serialVersionUID = 4980356362831585417L;
+
+    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
+        LOG.debug("MoonTokenEndpoint Servlet doPost");
+    }
+
+}
