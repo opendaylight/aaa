fb76b7acd7bb5dacdf230d81a2c6a89b03bae889
Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>
2017-02-07 19:14:50 +0100

    Enable keystone authentication
    Add Keystone authentication support through a new Shiro realm:
    KeystoneAuthRealm.
    
    At this point only Basic Authentication is supported. The user
    credentials support the format user@domain to specify a domain.
    The authentication is 'unscoped'.
    
    For SSL verification in the https handshake with keystone
    endpoint, SSL context provided by odl-aaa certificate manager
    is used. Jersey v1 is used as HTTP client and Jackson v2 for the
    serialization.
    
    The following parameters are configurable through shiro.ini for this
    realm:
    
    - url: The endpoint url for Keystone instance (API v3), can be http
      or https.
    - sslVerification: if url is https, whether to perform ssl
      verification.
    - defaultDomain: if no domain is available through the provided
      credentials, this default domain will be used. Defaults to 'Default'.
    
    Change-Id: Ifcf5e6252fb8884adbe4127c63b04a28f263d02b
    Signed-off-by: Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>

---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/impl/shiro/realm/util/http/SimpleHttpRequest.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2017 Ericsson Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.aaa.impl.shiro.realm.util.http;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.client.urlconnection.HTTPSProperties;
+import java.net.URI;
+import java.util.HashSet;
+import java.util.Set;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+/**
+ * Basic utility to do an HTTP request.
+ *
+ * @param <T> the return type of the request.
+ */
+public class SimpleHttpRequest<T> {
+    private URI uri;
+    private String path;
+    private SSLContext sslContext;
+    private HostnameVerifier hostnameVerifier;
+    private String method;
+    private MediaType mediaType;
+    private Object entity;
+    private Set<Class<?>> providers = new HashSet<>();
+    private Class<T> outputType;
+
+    private SimpleHttpRequest() {}
+
+    /**
+     * Executes the http request.
+     *
+     * @return the result of the http request.
+     */
+    public T execute() {
+        ClientConfig clientConfig = new DefaultClientConfig();
+        clientConfig.getClasses().addAll(providers);
+        clientConfig.getProperties().put(
+                HTTPSProperties.PROPERTY_HTTPS_PROPERTIES,
+                new HTTPSProperties(hostnameVerifier, sslContext));
+        Client client = Client.create(clientConfig);
+        WebResource webResource = client.resource(uri);
+        if (outputType == Response.class) {
+            ClientResponse output = webResource.path(path).type(mediaType).method(method, ClientResponse.class, entity);
+            return outputType.cast(clientResponseToResponse(output));
+        } else {
+            return webResource.path(path).type(mediaType).method(method, outputType, entity);
+        }
+    }
+
+    /**
+     * Obtain a builder for SimpleHttpRequest.
+     *
+     * @param outputType the return type of the request.
+     * @param <T> the return type of the request.
+     *
+     * @return the builder.
+     */
+    public static <T> Builder<T> builder(Class<T> outputType) {
+        return new Builder<>(outputType);
+    }
+
+    private static Response clientResponseToResponse(final ClientResponse clientResponse) {
+        Response.ResponseBuilder rb = Response.status(clientResponse.getStatus());
+        clientResponse.getHeaders().forEach(rb::header);
+        rb.entity(clientResponse.getEntityInputStream());
+        return rb.build();
+    }
+
+    public static final class Builder<T> {
+        private SimpleHttpRequest<T> request;
+
+        private Builder(Class<T> outputType) {
+            request = new SimpleHttpRequest<>();
+            request.outputType = outputType;
+        }
+
+        /**
+         * Sets the URI the request is made to.
+         * @param uri the URI.
+         * @return self, the request builder.
+         */
+        public Builder<T> uri(URI uri) {
+            request.uri = uri;
+            return this;
+        }
+
+        /**
+         * Sets the relative path the request is made to.
+         * @param path the path.
+         * @return self, the request builder.
+         */
+        public Builder<T> path(String path) {
+            request.path = path;
+            return this;
+        }
+
+        /**
+         * Sets the SSLContext to be used for SSL requests.
+         * @param sslContext the SSLContext.
+         * @return self, the request builder.
+         */
+        public Builder<T> sslContext(SSLContext sslContext) {
+            request.sslContext = sslContext;
+            return this;
+        }
+
+        /**
+         * Sets the hostname verifier the request is made with.
+         * @param hostnameVerifier the hortname verifier.
+         * @return self, the request builder.
+         */
+        public Builder<T> hostnameVerifier(HostnameVerifier hostnameVerifier) {
+            request.hostnameVerifier = hostnameVerifier;
+            return this;
+        }
+
+        /**
+         * Sets the method invoked in the request.
+         * @param method the method.
+         * @return self, the request builder.
+         */
+        public Builder<T> method(String method) {
+            request.method = method;
+            return this;
+        }
+
+        /**
+         * Sets the media type of the request payload.
+         * @param mediaType the media type.
+         * @return self, the request builder.
+         */
+        public Builder<T> mediaType(MediaType mediaType) {
+            request.mediaType = mediaType;
+            return this;
+        }
+
+        /**
+         * Sets the input payload of the request.
+         * @param input the input payload.
+         * @return self, the request builder.
+         */
+        public Builder<T> entity(Object input) {
+            request.entity = input;
+            return this;
+        }
+
+        /**
+         * Sets a JAX-RS provider to use for this request. Can be called
+         * multiple times to add multiple providers.
+         * @param provider the provider.
+         * @return self, the request builder.
+         */
+        public Builder<T> provider(Class<?> provider) {
+            request.providers.add(provider);
+            return this;
+        }
+
+        /**
+         * Build the request.
+         * @return the request.
+         */
+        public SimpleHttpRequest<T> build() {
+            return request;
+        }
+    }
+
+}
