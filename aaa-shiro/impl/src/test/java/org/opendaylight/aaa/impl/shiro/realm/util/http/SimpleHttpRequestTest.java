4cd4eab895fea7c68a9aeace116cda289f63a599
Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>
2017-02-07 19:14:50 +0100

    Enable keystone authentication
    Add Keystone authentication support through a new Shiro realm:
    KeystoneAuthRealm.
    
    At this point only Basic Authentication is supported. The user
    credentials support the format user@domain to specify a domain.
    The authentication is 'unscoped'.
    
    For SSL verification in the https handshake with keystone
    endpoint, SSL context provided by odl-aaa certificate manager
    is used. Jersey v1 is used as HTTP client and Jackson v2 for the
    serialization.
    
    The following parameters are configurable through shiro.ini for this
    realm:
    
    - url: The endpoint url for Keystone instance (API v3), can be http
      or https.
    - sslVerification: if url is https, whether to perform ssl
      verification.
    - defaultDomain: if no domain is available through the provided
      credentials, this default domain will be used. Defaults to 'Default'.
    
    Change-Id: Ifcf5e6252fb8884adbe4127c63b04a28f263d02b
    Signed-off-by: Jaime Caamaño Ruiz <jaime.caamano.ruiz@ericsson.com>

---
+++ aaa-shiro/impl/src/test/java/org/opendaylight/aaa/impl/shiro/realm/util/http/SimpleHttpRequestTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2017 Ericsson Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.aaa.impl.shiro.realm.util.http;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
+import java.net.URI;
+import java.net.URL;
+import java.util.Collections;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleHttpRequestTest {
+
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
+    private Client client;
+
+    @Mock
+    private ClientResponse clientResponse;
+
+    @Test
+    public void execute() throws Exception {
+        URI uri = new URL("http://example.com").toURI();
+        String path = "path";
+        MultivaluedMap<String, String> headers = new MultivaluedMapImpl();
+        headers.put("header1", Collections.singletonList("value1"));
+        headers.put("header2", Collections.singletonList("value2"));
+        SimpleHttpRequest<Response> request = SimpleHttpRequest.builder(Response.class)
+                .uri(uri)
+                .path(path)
+                .sslContext(UntrustedSSL.getSSLContext())
+                .hostnameVerifier(UntrustedSSL.getHostnameVerifier())
+                .mediaType(MediaType.APPLICATION_JSON_TYPE)
+                .method("POST")
+                .entity("entity")
+                .build();
+        when(client
+                .resource(uri)
+                .path(path)
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .method("POST", ClientResponse.class, "entity"))
+            .thenReturn(clientResponse);
+        when(clientResponse.getStatus()).thenReturn(200);
+        when(clientResponse.getHeaders()).thenReturn(headers);
+
+        SimpleHttpRequest<Response> spiedRequest = spy(request);
+        doReturn(client).when(spiedRequest).createClient(any());
+
+        Response response = spiedRequest.execute();
+
+        verify(spiedRequest).createClient(any());
+
+        assertThat(response.getStatus(), is(200));
+        assertThat(response.getMetadata(), is(headers));
+    }
+
+}
