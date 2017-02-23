ce6780732269230324b95832bb68b3b245f6818b
melserngawy <melserngawy@inocybe.com>
2017-02-23 15:24:03 -0500

    Generate Random password for the keystores
    Generate random password for the keystores
    at the installation time to enhance and secure
    Opendaylight deployment.
    
    Change-Id: I93dc2cc292f2c3dfac9cff87b6222a994936125b
    Signed-off-by: melserngawy <melserngawy@inocybe.com>

--- aaa-cert/src/main/java/org/opendaylight/aaa/cert/impl/CertificateManagerService.java
+++ aaa-cert/src/main/java/org/opendaylight/aaa/cert/impl/CertificateManagerService.java
@@ -8,6 +8,8 @@
 
 package org.opendaylight.aaa.cert.impl;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -17,13 +19,31 @@ import java.security.UnrecoverableKeyException;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.opendaylight.aaa.cert.api.IAaaCertProvider;
 import org.opendaylight.aaa.cert.api.ICertificateManager;
 import org.opendaylight.aaa.encrypt.AAAEncryptionService;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.yang.aaa.cert.rev151126.AaaCertServiceConfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.yang.aaa.cert.rev151126.AaaCertServiceConfigBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.yang.aaa.cert.rev151126.aaa.cert.service.config.CtlKeystore;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.yang.aaa.cert.rev151126.aaa.cert.service.config.CtlKeystoreBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.yang.aaa.cert.rev151126.aaa.cert.service.config.TrustKeystore;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.yang.aaa.cert.rev151126.aaa.cert.service.config.TrustKeystoreBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  * CertificateManagerService implements ICertificateManager and work as adapter to which AaaCertProvider is used.
@@ -35,10 +55,31 @@ public class CertificateManagerService implements ICertificateManager {
 
     private final static Logger LOG = LoggerFactory.getLogger(CertificateManagerService.class);
 
+    private final String DEFAULT_CONFIG_FILE_PATH = "etc" + File.separator + "opendaylight" + File.separator
+            + "datastore" + File.separator + "initial" + File.separator + "config" + File.separator + "aaa-cert-config.xml";
+    private final int pwdLength = 12;
     private final IAaaCertProvider aaaCertProvider;
 
-    public CertificateManagerService(final AaaCertServiceConfig aaaCertServiceConfig, final DataBroker dataBroker, final AAAEncryptionService encryptionSrv) {
-       if (aaaCertServiceConfig.isUseConfig()) {
+    public CertificateManagerService(AaaCertServiceConfig aaaCertServiceConfig, final DataBroker dataBroker, final AAAEncryptionService encryptionSrv) {
+        if(aaaCertServiceConfig == null) {
+            throw new IllegalArgumentException("Certificate Manager service configuration is null: " + aaaCertServiceConfig.toString());
+        }
+        if (aaaCertServiceConfig.isUseConfig()) {
+            if (aaaCertServiceConfig.getCtlKeystore() != null && aaaCertServiceConfig.getCtlKeystore().getStorePassword() != null
+                    && aaaCertServiceConfig.getCtlKeystore().getStorePassword().isEmpty()) {
+                LOG.debug("Set keystores password");
+                final String ctlPwd = RandomStringUtils.random(pwdLength, true, true);
+                final String trustPwd = RandomStringUtils.random(pwdLength, true, true);
+                updateCertManagerSrvConfig(ctlPwd, trustPwd);
+                final CtlKeystore ctlKeystore = new CtlKeystoreBuilder(aaaCertServiceConfig.getCtlKeystore())
+                        .setStorePassword(ctlPwd).build();
+                final TrustKeystore trustKeystore = new TrustKeystoreBuilder(aaaCertServiceConfig.getTrustKeystore())
+                        .setStorePassword(trustPwd).build();
+                aaaCertServiceConfig = new AaaCertServiceConfigBuilder(aaaCertServiceConfig)
+                        .setCtlKeystore(ctlKeystore)
+                        .setTrustKeystore(trustKeystore)
+                        .build();
+            }
             if (aaaCertServiceConfig.isUseMdsal()) {
                 aaaCertProvider = new DefaultMdsalSslData(new AaaCertMdsalProvider(dataBroker, encryptionSrv), aaaCertServiceConfig.getBundleName(),
                         aaaCertServiceConfig.getCtlKeystore(), aaaCertServiceConfig.getTrustKeystore());
@@ -111,4 +152,39 @@ public class CertificateManagerService implements ICertificateManager {
     public String[] getTlsProtocols() {
         return aaaCertProvider.getTlsProtocols();
     }
+
+    private void updateCertManagerSrvConfig(String ctlPwd, String trustPwd) {
+        try {
+            LOG.debug("Update Certificate manager service config file");
+            final File configFile = new File(DEFAULT_CONFIG_FILE_PATH);
+            if (configFile.exists()) {
+                final String storePwdTag = "store-password";
+                final String ctlStoreTag = "ctlKeystore";
+                final String trustStoreTag = "trustKeystore";
+                final DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+                final DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+                final Document doc = docBuilder.parse(configFile);
+                final NodeList ndList = doc.getElementsByTagName(storePwdTag);
+                for (int i = 0; i < ndList.getLength(); i++) {
+                    final Node nd = ndList.item(i);
+                    if (nd.getParentNode() != null && nd.getParentNode().getNodeName().equals(ctlStoreTag)) {
+                        nd.setTextContent(ctlPwd);
+                    }
+                    else if (nd.getParentNode() != null && nd.getParentNode().getNodeName().equals(trustStoreTag)) {
+                        nd.setTextContent(trustPwd);
+                    }
+                }
+                final TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                final Transformer transformer = transformerFactory.newTransformer();
+                final DOMSource source = new DOMSource(doc);
+                final StreamResult result = new StreamResult(new File(DEFAULT_CONFIG_FILE_PATH));
+                transformer.transform(source, result);
+            } else {
+                LOG.warn("The Certificate manager service config file does not exist {}", DEFAULT_CONFIG_FILE_PATH);
+            }
+        } catch (ParserConfigurationException | TransformerException | SAXException | IOException e) {
+            LOG.error("Error while updating Certificate manager service config file", e);
+        }
+    }
+
 }
