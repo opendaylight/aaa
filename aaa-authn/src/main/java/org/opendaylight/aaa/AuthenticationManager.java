e053e3af9505d330c37b9223dd8381e0dacff127
David <david.suarez.fuentes@ericsson.com>
2017-03-01 00:25:08 +0100

    Minor changes
    - Reorder modifiers
    - Private constructor to avoid instantiation
    
    Change-Id: I201648759962387c40e8f62960db16e530612e70
    Signed-off-by: David <david.suarez.fuentes@ericsson.com>

--- aaa-authn/src/main/java/org/opendaylight/aaa/AuthenticationManager.java
+++ aaa-authn/src/main/java/org/opendaylight/aaa/AuthenticationManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+ * Copyright (c) 2014, 2017 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -31,7 +31,7 @@ public class AuthenticationManager implements AuthenticationService, ManagedServ
     // In non-Karaf environments, authEnabled is set to false by default
     private static volatile boolean authEnabled = false;
 
-    private final static AuthenticationManager am = new AuthenticationManager();
+    private static final AuthenticationManager am = new AuthenticationManager();
     private final ThreadLocal<Authentication> auth = new InheritableThreadLocal<>();
 
     private AuthenticationManager() {
--- aaa-authn/src/main/java/org/opendaylight/aaa/EqualUtil.java
+++ aaa-authn/src/main/java/org/opendaylight/aaa/EqualUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+ * Copyright (c) 2014, 2017 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -16,27 +16,31 @@ package org.opendaylight.aaa;
  * <code>Arrays.equals</code> methods should be used for array fields.
  */
 public final class EqualUtil {
-    static public boolean areEqual(boolean aThis, boolean aThat) {
+
+    private EqualUtil () {
+    }
+
+    public static boolean areEqual(boolean aThis, boolean aThat) {
         return aThis == aThat;
     }
 
-    static public boolean areEqual(char aThis, char aThat) {
+    public static boolean areEqual(char aThis, char aThat) {
         return aThis == aThat;
     }
 
-    static public boolean areEqual(long aThis, long aThat) {
+    public static boolean areEqual(long aThis, long aThat) {
         return aThis == aThat;
     }
 
-    static public boolean areEqual(float aThis, float aThat) {
+    public static boolean areEqual(float aThis, float aThat) {
         return Float.floatToIntBits(aThis) == Float.floatToIntBits(aThat);
     }
 
-    static public boolean areEqual(double aThis, double aThat) {
+    public static boolean areEqual(double aThis, double aThat) {
         return Double.doubleToLongBits(aThis) == Double.doubleToLongBits(aThat);
     }
 
-    static public boolean areEqual(Object aThis, Object aThat) {
+    public static boolean areEqual(Object aThis, Object aThat) {
         return aThis == null ? aThat == null : aThis.equals(aThat);
     }
 }
