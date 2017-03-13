c9662292961ec9cc05c3c4d580633c9b99dacf3b
matthieu <mcauffiez@inocybe.com>
2017-03-13 10:45:53 -0400

    add Mdsal provider Tests
    Change-Id: I77838c8c97b572490fdb9e853ce3ffa62e43484f
    Signed-off-by: matthieu <mcauffiez@inocybe.com>

---
+++ aaa-cert/src/test/java/org/opendaylight/aaa/cert/test/TestUtils.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2016 Inocybe Technologies. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.cert.test;
+
+import org.opendaylight.aaa.cert.impl.KeyStoreConstant;
+
+public class TestUtils {
+    public static String dummyAlias = "fooAlias";
+    public static String dummyCert = KeyStoreConstant.BEGIN_CERTIFICATE +
+            "MIICKTCCAZKgAwIBAgIECMgzyzANBgkqhkiG9w0BAQUFADBZMQwwCgYDV" +
+            "QQDDANPREwxDDAKBgNVBAsMA0RldjEYMBYGA1UECgwPTGludXhGb3VuZG" +
+            "F0aW9uMRQwEgYDVQQHDAtRQyBNb250cmVhbDELMAkGA1UEBhMCQ0EwHhc" +
+            "NMTcwMzAzMTYyMDA1WhcNMTgwMzAzMTYyMDA1WjBZMQwwCgYDVQQDDANP" +
+            "REwxDDAKBgNVBAsMA0RldjEYMBYGA1UECgwPTGludXhGb3VuZGF0aW9uM" +
+            "RQwEgYDVQQHDAtRQyBNb250cmVhbDELMAkGA1UEBhMCQ0EwgZ8wDQYJKo" +
+            "ZIhvcNAQEBBQADgY0AMIGJAoGBAJrQxIfdU230tedhXnM25r3ht5UQ5Jo" +
+            "G7+9H9b2WcrrkehJ++AZ2zq6SJDLVVnjgXh/YgFo3L6DOKnVTwnXUXGLk" +
+            "NiJhqL2ndu49zI63CxQ2EjBR8tlD5HctNH4SKj1RqmYvt0H3LUZSBKH8Y" +
+            "XGL0U0Qyxwe3flRh2Y6sMb3o47rAgMBAAEwDQYJKoZIhvcNAQEFBQADgY" +
+            "EAVBWCNC+bbJftOTfpL3sL3YO+aQSmPt5ICgrz7wXDkzpf+0FwSqt+kiR" +
+            "Wfw65RTgmn2hmYPh2QW4SaIN50ftLfUHgkf2zeMlodAQVYmBAd/woE3s7" +
+            "fkSa9vQkUowgHAxW//7NOOTonnQPi2gH6ubaOCG4ZeXTwqHy47DGA0c8z" +
+            "2Q="+
+            KeyStoreConstant.END_CERTIFICATE;
+}
