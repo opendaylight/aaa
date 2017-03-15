29d16c8ffc213cc4a16606fbb89f80e39526b2d4
David <david.suarez.fuentes@ericsson.com>
2017-03-15 10:13:57 +0100

    Make IDMStoreUtil a final class
    Make IDMStoreUtil a final class since it declares a private
    constructor.
    
    Change-Id: I318a8d5f72de47636f3eecc5837ea199c9a4c5b5
    Signed-off-by: David <david.suarez.fuentes@ericsson.com>

--- aaa-authn-api/src/main/java/org/opendaylight/aaa/api/IDMStoreUtil.java
+++ aaa-authn-api/src/main/java/org/opendaylight/aaa/api/IDMStoreUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ * Copyright (c) 2015, 2017 Cisco Systems, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -16,7 +16,7 @@ import javax.naming.OperationNotSupportedException;
  *
  *  @author - Sharon Aicler (saichler@cisco.com)
  */
-public class IDMStoreUtil {
+public final class IDMStoreUtil {
 
     private IDMStoreUtil() throws OperationNotSupportedException {
         throw new OperationNotSupportedException();
