d8e12cbcee275f0d9d84b128b47769e6b4e2567c
melserngawy <melserngawy@inocybe.com>
2017-04-17 16:19:17 -0400

    Bug 8062: Fix sharing the encryption service config across the cluster nodes
    Change-Id: Ie53cbde91c4388cb45459424283ef7a90f3ae25a
    Signed-off-by: melserngawy <melserngawy@inocybe.com>
    (cherry picked from commit e3046a2b5479e4ad0c7e826ee596bdd74c518714)

--- aaa-encrypt-service/src/test/java/org/opendaylight/aaa/encrypt/AAAEncryptServiceImplTest.java
+++ aaa-encrypt-service/src/test/java/org/opendaylight/aaa/encrypt/AAAEncryptServiceImplTest.java
@@ -12,53 +12,62 @@ import static org.mockito.Mockito.mock;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.yang.gen.v1.config.aaa.authn.encrypt.service.config.rev160915.AaaEncryptServiceConfig;
+import org.opendaylight.yang.gen.v1.config.aaa.authn.encrypt.service.config.rev160915.AaaEncryptServiceConfigBuilder;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /*
  *  @author - Sharon Aicler (saichler@gmail.com)
  */
+
+@RunWith(PowerMockRunner.class)
+@PowerMockIgnore("javax.crypto.*")
+@PrepareForTest(MdsalUtils.class)
 public class AAAEncryptServiceImplTest {
 
     private AAAEncryptionServiceImpl impl = null;
-    private AaaEncryptServiceConfig module = mock(AaaEncryptServiceConfig.class);
-
-    private static final String ENCRYPT_KEY = "";
-    private static final String ENCRYPT_SALT = "";
-    private static final String ENCRYPTION_METHOD = "PBKDF2WithHmacSHA1";
-    private static final String ENCRYPTION_TYPE = "AES";
-    private static final int ENCRYPTION_ITERATION_COUNT = 32768;
-    private static final int ENCRYPTION_KEY_LENGTH = 128;
-    private static final String CIPHER_TRANSFORMS = "AES/CBC/PKCS5Padding";
+    private DataBroker dataBroker = mock(DataBroker.class);
 
     @Before
     public void setup(){
+        AaaEncryptServiceConfig module = new AaaEncryptServiceConfigBuilder()
+                .setCipherTransforms("AES/CBC/PKCS5Padding")
+                .setEncryptIterationCount(32768)
+                .setEncryptKey("")
+                .setEncryptKeyLength(128)
+                .setEncryptMethod("PBKDF2WithHmacSHA1")
+                .setEncryptSalt("")
+                .setEncryptType("AES")
+                .setPasswordLength(12)
+                .build();
 
-        Mockito.when(module.getEncryptSalt()).thenReturn(ENCRYPT_SALT);
-        Mockito.when(module.getEncryptKey()).thenReturn(ENCRYPT_KEY);
-        Mockito.when(module.getEncryptMethod()).thenReturn(ENCRYPTION_METHOD);
-        Mockito.when(module.getEncryptType()).thenReturn(ENCRYPTION_TYPE);
-        Mockito.when(module.getEncryptIterationCount()).thenReturn(ENCRYPTION_ITERATION_COUNT);
-        Mockito.when(module.getEncryptKeyLength()).thenReturn(ENCRYPTION_KEY_LENGTH);
-        Mockito.when(module.getCipherTransforms()).thenReturn(CIPHER_TRANSFORMS);
-        impl = new AAAEncryptionServiceImpl(module);
+        PowerMockito.mockStatic(MdsalUtils.class);
+        PowerMockito.when(MdsalUtils.read(dataBroker, LogicalDatastoreType.CONFIGURATION,
+                MdsalUtils.getEncryptionSrvConfigIid())).thenReturn(module);
+        impl = new AAAEncryptionServiceImpl(module, dataBroker);
     }
 
     @Test
     public void testShortString(){
         String before = "shortone";
         String encrypt = impl.encrypt(before);
-        Assert.assertNotEquals(before,encrypt);
+        Assert.assertNotEquals(before, encrypt);
         String after = impl.decrypt(encrypt);
-        Assert.assertEquals(before,after);
+        Assert.assertEquals(before, after);
     }
 
     @Test
     public void testLongString(){
         String before = "This is a very long string to encrypt for testing 1...2...3";
         String encrypt = impl.encrypt(before);
-        Assert.assertNotEquals(before,encrypt);
+        Assert.assertNotEquals(before, encrypt);
         String after = impl.decrypt(encrypt);
-        Assert.assertEquals(before,after);
+        Assert.assertEquals(before, after);
     }
 }
