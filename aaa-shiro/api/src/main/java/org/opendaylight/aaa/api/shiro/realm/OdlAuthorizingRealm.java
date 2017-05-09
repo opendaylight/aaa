9609bd1abe5df1ba5872132241b1d6e28b2342ce
Ryan Goulding <ryandgoulding@gmail.com>
2017-05-09 15:30:05 -0400

    MDSAL based AuthorizingRealm API only
    Adds an MDSALRealm API only.  None of the impl changes are
    in this patch.
    
    The MDSALRealm will be a clustered data store implementation
    for IDM.  The old store MD-SAL based store will be removed
    in future patches, but will need to be done carefully since
    it appears (from mailing lists) that people somehow still
    use it.  We will need to publish some sort of transition
    plan for people.
    
    Change-Id: I84aebf4c172dbaf63ad8f24c5b4d380a1e7baa3b
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

---
+++ aaa-shiro/api/src/main/java/org/opendaylight/aaa/api/shiro/realm/OdlAuthorizingRealm.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2017 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.aaa.api.shiro.realm;
+
+import org.apache.shiro.realm.AuthorizingRealm;
+
+/**
+ * A generic, extensible realm with Authorization support for ODL based
+ * realms.
+ *
+ * @author Ryan Goulding (ryandgoulding@gmail.com)
+ */
+public abstract class OdlAuthorizingRealm extends AuthorizingRealm {
+}
