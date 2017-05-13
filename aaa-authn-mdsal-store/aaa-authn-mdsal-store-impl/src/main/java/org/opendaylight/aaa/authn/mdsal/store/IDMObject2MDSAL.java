9808d5d9e1f7474a076635b701fea50684807bb2
David Suarez <david.suarez.fuentes@ericsson.com>
2017-05-13 19:08:08 +0200

    Fix checkstyle issues in module aaa-authn-mdsal-store-impl
    - Copyright sections
    - Remove @SuppressWarnings("checkstyle:IllegalCatch")
      by fixing exception handling
    - Enforcement in pom.xml
    
    Change-Id: I9564db1b11f2166551cb4b8fbd627fd453ab71fa
    Signed-off-by: David Suarez <david.suarez.fuentes@ericsson.com>

--- aaa-authn-mdsal-store/aaa-authn-mdsal-store-impl/src/main/java/org/opendaylight/aaa/authn/mdsal/store/IDMObject2MDSAL.java
+++ aaa-authn-mdsal-store/aaa-authn-mdsal-store-impl/src/main/java/org/opendaylight/aaa/authn/mdsal/store/IDMObject2MDSAL.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.opendaylight.aaa.api.IDMStoreException;
 import org.opendaylight.aaa.api.model.Domain;
 import org.opendaylight.aaa.api.model.Grant;
 import org.opendaylight.aaa.api.model.Role;
@@ -25,9 +26,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This class is a codec to convert between MDSAL objects and IDM model
- * objects. It is doing so via reflection when it assumes that the MDSAL Object
- * and the IDM model object has the same method names.
+ * This class is a codec to convert between MDSAL objects and IDM model objects.
+ * It is doing so via reflection when it assumes that the MDSAL Object and the
+ * IDM model object has the same method names.
  *
  * @author Sharon Aicler - saichler@cisco.com
  *
@@ -43,84 +44,85 @@ public abstract class IDMObject2MDSAL {
     // This method generically via reflection receive a MDSAL object and the
     // corresponding IDM model object class type and
     // creates an IDM model element from the MDSAL element
-    @SuppressWarnings("checkstyle:IllegalCatch")
-    private static Object fromMDSALObject(Object mdsalObject, Class<?> type) throws Exception {
+    private static Object fromMDSALObject(Object mdsalObject, Class<?> type) throws IDMStoreException {
         if (mdsalObject == null) {
             return null;
         }
-        Object result = type.newInstance();
-        ConvertionMethods cm = TYPES_METHODS.get(type);
-        if (cm == null) {
-            cm = new ConvertionMethods();
-            TYPES_METHODS.put(type, cm);
-            Method[] methods = type.getMethods();
-            for (Method m : methods) {
-                if (m.getName().startsWith("set")) {
-                    cm.setMethods.add(m);
-                    Method gm = null;
-                    if (m.getParameterTypes()[0].equals(Boolean.class)
-                            || m.getParameterTypes()[0].equals(boolean.class)) {
-                        gm = ((DataObject) mdsalObject).getImplementedInterface()
-                                .getMethod("is" + m.getName().substring(3), (Class[]) null);
-                    } else {
-                        try {
+        try {
+            Object result = type.newInstance();
+            ConvertionMethods cm = TYPES_METHODS.get(type);
+            if (cm == null) {
+                cm = new ConvertionMethods();
+                TYPES_METHODS.put(type, cm);
+                Method[] methods = type.getMethods();
+                for (Method m : methods) {
+                    if (m.getName().startsWith("set")) {
+                        cm.setMethods.add(m);
+                        Method gm;
+                        if (m.getParameterTypes()[0].equals(Boolean.class)
+                                || m.getParameterTypes()[0].equals(boolean.class)) {
                             gm = ((DataObject) mdsalObject).getImplementedInterface()
-                                    .getMethod("get" + m.getName().substring(3), (Class[]) null);
-                        } catch (Exception err) {
-                            LOG.error("Error associating get call", err);
+                                    .getMethod("is" + m.getName().substring(3), (Class[]) null);
+                        } else {
+                            gm = ((DataObject) mdsalObject).getImplementedInterface()
+                                        .getMethod("get" + m.getName().substring(3), (Class[]) null);
                         }
+                        cm.getMethods.put(m.getName(), gm);
                     }
-                    cm.getMethods.put(m.getName(), gm);
                 }
             }
-        }
-        for (Method m : cm.setMethods) {
-            try {
+            for (Method m : cm.setMethods) {
                 m.invoke(result, new Object[] { cm.getMethods.get(m.getName()).invoke(mdsalObject, (Object[]) null) });
-            } catch (Exception err) {
-                LOG.error("Error invoking reflection method", err);
             }
+            return result;
+        } catch (ReflectiveOperationException e) {
+            LOG.error("Reflection error", e);
+            throw new IDMStoreException(e);
         }
-        return result;
     }
 
     // This method generically use reflection to receive an IDM model object and
     // the corresponding MDSAL object and creates
     // a MDSAL object out of the IDM model object
-    private static Object toMDSALObject(Object object, Class<?> mdSalBuilderType) throws Exception {
+    private static Object toMDSALObject(Object object, Class<?> mdSalBuilderType) throws IDMStoreException {
         if (object == null) {
             return null;
         }
-        Object result = mdSalBuilderType.newInstance();
-        ConvertionMethods cm = TYPES_METHODS.get(mdSalBuilderType);
-        if (cm == null) {
-            cm = new ConvertionMethods();
-            TYPES_METHODS.put(mdSalBuilderType, cm);
-            Method[] methods = mdSalBuilderType.getMethods();
-            for (Method m : methods) {
-                if (m.getName().startsWith("set")) {
-                    try {
-                        Method gm;
-                        if (m.getParameterTypes()[0].equals(Boolean.class)
-                                || m.getParameterTypes()[0].equals(boolean.class)) {
-                            gm = object.getClass().getMethod("is" + m.getName().substring(3), (Class[]) null);
-                        } else {
-                            gm = object.getClass().getMethod("get" + m.getName().substring(3), (Class[]) null);
+        try {
+            Object result = mdSalBuilderType.newInstance();
+            ConvertionMethods cm = TYPES_METHODS.get(mdSalBuilderType);
+            if (cm == null) {
+                cm = new ConvertionMethods();
+                TYPES_METHODS.put(mdSalBuilderType, cm);
+                Method[] methods = mdSalBuilderType.getMethods();
+                for (Method m : methods) {
+                    if (m.getName().startsWith("set")) {
+                        try {
+                            Method gm;
+                            if (m.getParameterTypes()[0].equals(Boolean.class)
+                                    || m.getParameterTypes()[0].equals(boolean.class)) {
+                                gm = object.getClass().getMethod("is" + m.getName().substring(3), (Class[]) null);
+                            } else {
+                                gm = object.getClass().getMethod("get" + m.getName().substring(3), (Class[]) null);
+                            }
+                            cm.getMethods.put(m.getName(), gm);
+                            cm.setMethods.add(m);
+                        } catch (NoSuchMethodException e) {
+                            LOG.error("No such method exception", e);
                         }
-                        cm.getMethods.put(m.getName(), gm);
-                        cm.setMethods.add(m);
-                    } catch (NoSuchMethodException e) {
-                        LOG.error("No such method exception", e);
                     }
                 }
+                cm.builderMethod = mdSalBuilderType.getMethod("build", (Class[]) null);
             }
-            cm.builderMethod = mdSalBuilderType.getMethod("build", (Class[]) null);
-        }
-        for (Method m : cm.setMethods) {
-            m.invoke(result, new Object[] { cm.getMethods.get(m.getName()).invoke(object, (Object[]) null) });
-        }
+            for (Method m : cm.setMethods) {
+                m.invoke(result, new Object[] { cm.getMethods.get(m.getName()).invoke(object, (Object[]) null) });
+            }
 
-        return cm.builderMethod.invoke(result, (Object[]) null);
+            return cm.builderMethod.invoke(result, (Object[]) null);
+        } catch (ReflectiveOperationException e) {
+            LOG.error("Reflection error", e);
+            throw new IDMStoreException(e);
+        }
     }
 
     // A structure class to hold the getters & setters of each type to speed
@@ -132,100 +134,91 @@ public abstract class IDMObject2MDSAL {
     }
 
     // Convert Domain
-    @SuppressWarnings("checkstyle:IllegalCatch")
-    public static org.opendaylight.yang.gen.v1.urn.aaa.yang
-            .authn.claims.rev141029.authentication.Domain toMDSALDomain(
+    public static org.opendaylight.yang.gen.v1.urn.aaa.yang.authn.claims.rev141029.authentication.Domain toMDSALDomain(
             Domain domain) {
         try {
-            return (org.opendaylight.yang.gen.v1.urn.aaa
-                        .yang.authn.claims.rev141029.authentication.Domain) toMDSALObject(
+            return (org.opendaylight.yang.gen.v1.urn
+                    .aaa.yang.authn.claims.rev141029.authentication.Domain) toMDSALObject(
                     domain, DomainBuilder.class);
-        } catch (Exception err) {
-            LOG.error("Error converting domain to MDSAL object", err);
+        } catch (IDMStoreException e) {
+            LOG.error("Error converting domain to MDSAL object", e);
             return null;
         }
     }
 
-    @SuppressWarnings("checkstyle:IllegalCatch")
     public static Domain toIDMDomain(
             org.opendaylight.yang.gen.v1.urn.aaa.yang.authn.claims.rev141029.authentication.Domain domain) {
         try {
             return (Domain) fromMDSALObject(domain, Domain.class);
-        } catch (Exception err) {
-            LOG.error("Error converting domain from MDSAL to IDM object", err);
+        } catch (IDMStoreException e) {
+            LOG.error("Error converting domain from MDSAL to IDM object", e);
             return null;
         }
     }
 
     // Convert Role
-    @SuppressWarnings("checkstyle:IllegalCatch")
-    public static org.opendaylight.yang.gen.v1.urn.aaa.yang
-            .authn.claims.rev141029.authentication.Role toMDSALRole(Role role) {
+    public static org.opendaylight.yang.gen.v1.urn.aaa.yang.authn.claims.rev141029.authentication.Role toMDSALRole(
+            Role role) {
         try {
             return (org.opendaylight.yang.gen.v1.urn.aaa.yang.authn.claims.rev141029.authentication.Role) toMDSALObject(
                     role, RoleBuilder.class);
-        } catch (Exception err) {
-            LOG.error("Error converting role to MDSAL object", err);
+        } catch (IDMStoreException e) {
+            LOG.error("Error converting role to MDSAL object", e);
             return null;
         }
     }
 
-    @SuppressWarnings("checkstyle:IllegalCatch")
     public static Role toIDMRole(
             org.opendaylight.yang.gen.v1.urn.aaa.yang.authn.claims.rev141029.authentication.Role role) {
         try {
             return (Role) fromMDSALObject(role, Role.class);
-        } catch (Exception err) {
-            LOG.error("Error converting role fom MDSAL to IDM object", err);
+        } catch (IDMStoreException e) {
+            LOG.error("Error converting role fom MDSAL to IDM object", e);
             return null;
         }
     }
 
     // Convert User
-    @SuppressWarnings("checkstyle:IllegalCatch")
     public static org.opendaylight.yang.gen.v1.urn.aaa.yang.authn.claims.rev141029.authentication.User toMDSALUser(
             User user) {
         try {
             return (org.opendaylight.yang.gen.v1.urn.aaa.yang.authn.claims.rev141029.authentication.User) toMDSALObject(
                     user, UserBuilder.class);
-        } catch (Exception err) {
-            LOG.error("Error converting user to MDSAL object", err);
+        } catch (IDMStoreException e) {
+            LOG.error("Error converting user to MDSAL object", e);
             return null;
         }
     }
 
-    @SuppressWarnings("checkstyle:IllegalCatch")
     public static User toIDMUser(
             org.opendaylight.yang.gen.v1.urn.aaa.yang.authn.claims.rev141029.authentication.User user) {
         try {
             return (User) fromMDSALObject(user, User.class);
-        } catch (Exception err) {
-            LOG.error("Error converting user from MDSAL to IDM object", err);
+        } catch (IDMStoreException e) {
+            LOG.error("Error converting user from MDSAL to IDM object", e);
             return null;
         }
     }
 
     // Convert Grant
-    @SuppressWarnings("checkstyle:IllegalCatch")
     public static org.opendaylight.yang.gen.v1.urn.aaa.yang.authn.claims.rev141029.authentication.Grant toMDSALGrant(
             Grant grant) {
         try {
-            return (org.opendaylight.yang.gen.v1.urn.aaa
-                    .yang.authn.claims.rev141029.authentication.Grant) toMDSALObject(
+            return (org.opendaylight.yang.gen.v1.urn
+                    .aaa.yang.authn.claims.rev141029.authentication.Grant) toMDSALObject(
                     grant, GrantBuilder.class);
-        } catch (Exception err) {
-            LOG.error("Error converting grant to MDSAL object", err);
+        } catch (IDMStoreException e) {
+            LOG.error("Error converting grant to MDSAL object", e);
             return null;
         }
     }
 
-    @SuppressWarnings("checkstyle:IllegalCatch")
     public static Grant toIDMGrant(
             org.opendaylight.yang.gen.v1.urn.aaa.yang.authn.claims.rev141029.authentication.Grant grant) {
         try {
             return (Grant) fromMDSALObject(grant, Grant.class);
-        } catch (Exception err) {
-            LOG.error("Error converting grant from MDSAL to IDM object", err);
+        } catch (IDMStoreException e) {
+            LOG.error("Error converting grant from MDSAL to IDM object", e);
             return null;
         }
     }
