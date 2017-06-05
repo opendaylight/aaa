277f5a72a4274623dc485d99cf2212cd8ca50b3d
Ryan Goulding <ryandgoulding@gmail.com>
2017-06-05 12:26:32 -0400

    Bug 8437: Collapse Services and transition to blueprint
    One of the major focuses of the Nitrogen release surrounds service stability
    in Karaf.  Hitherto, AAA has made use of a wide variety of service wiring
    techniques, some effective, some not so great.  The service wiring really
    was composed of three forms:
    1) blueprint (good)
    2) CSS (removed in previous patches and replaced by #1)
    3) felix dependency management, which is really old and required long waits
       to properly wire various components
    
    Since #2 was already removed, this patch removes #3, which is the wiring of
    services through ServiceTrackerCustomizers and the singleton ServiceLocator.
    ServiceLocator still exists in this patch;  to remove it here would make the
    patch much too large, but it will be refactored in a followup patch.
    
    For now, what is done is the following:
    1) removal of aaa-authn bundle.  This bundle contained Builders and utility
    classes common to aaa-idmlight and aaa-mdsal-store bundles.  They are
    implementation details, and thus not candidates for odl-aaa-api.  They were
    put in odl-aaa-shiro, as eventually odl-aaa-mdsal-cluster will eventually be
    removed and replaced with functionality built directly into odl-aaa-shiro
    (see https://git.opendaylight.org/gerrit/#/c/56744/)
    
    2) removal of aaa-authn-basic.  This bundle really didn't provide much
    more than HttpAuthBasic, which is really the only common impl of TokenAuth
    used in AAA.  This bundle's functionality was relocated to odl-aaa-shiro.
    
    3) removal of aaa-authn-sts.  STS stands for "single token store".  This
    bundle also provided limited functionality, and really didn't make sense as
    a completely separate service.
    
    This patch focuses on the consolidation of several aaa bundles and services
    into something more manageable directly in aaa-shiro.
    
    Future work will:
    1) remove the existing mdsal store in favor of the MDSAL based
       AuthorizingRealm.  Basically, the old mdsal store utilizes a lot of hacky
       code, reflection, etc. which can be cleaned and redefined to be more
       managable.  This will be removed in Oxygen, as we forgot to deprecate
       it in Nitrogen.  Messaging will be delivered to ensure that people use
       the new Realm approach once it is available.  This is again, not going
       to be the default store anyway in Nitrogen.
    
    2) further refactoring of Services so that the aaa-idmlight and aaa-h2-store
       bundles are included directly in aaa-shiro.  Basically, it is really
       against OSGi architecture to have this many services providing such
       limited functionality.  Instead, we ought to provide them as part of a
       single service, and make configuration through clustered-app-config,
       cfg files, or some other more sane way than service injection.  The
       fact remains that we really want a more statically wired configuration
       that is consistent and easy to understand.
    
    This is just part of many patches to clean the existing AAA implementation.
    
    Change-Id: I740a667278f2ff64daff5e3602ddc5586fe23733
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/impl/shiro/tokenauthrealm/auth/ClaimBuilder.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2014, 2017 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.impl.shiro.tokenauthrealm.auth;
+
+import static org.opendaylight.aaa.impl.shiro.tokenauthrealm.util.EqualUtil.areEqual;
+import static org.opendaylight.aaa.impl.shiro.tokenauthrealm.util.HashCodeUtil.hash;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableSet;
+import org.opendaylight.aaa.impl.shiro.tokenauthrealm.util.HashCodeUtil;
+import org.opendaylight.aaa.api.Claim;
+
+import java.io.Serializable;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+
+/**
+ * Builder for a {@link Claim}. The userId, user, and roles information is
+ * mandatory.
+ *
+ * @author liemmn
+ *
+ */
+public class ClaimBuilder {
+    private String userId = "";
+    private String user = "";
+    private final Set<String> roles = new LinkedHashSet<>();
+    private String clientId = "";
+    private String domain = "";
+
+    public ClaimBuilder() {
+    }
+
+    public ClaimBuilder(Claim claim) {
+        clientId = claim.clientId();
+        userId = claim.userId();
+        user = claim.user();
+        domain = claim.domain();
+        roles.addAll(claim.roles());
+    }
+
+    public ClaimBuilder setClientId(String clientId) {
+        this.clientId = Strings.nullToEmpty(clientId).trim();
+        return this;
+    }
+
+    public ClaimBuilder setUserId(String userId) {
+        this.userId = Strings.nullToEmpty(userId).trim();
+        return this;
+    }
+
+    public ClaimBuilder setUser(String userName) {
+        user = Strings.nullToEmpty(userName).trim();
+        return this;
+    }
+
+    public ClaimBuilder setDomain(String domain) {
+        this.domain = Strings.nullToEmpty(domain).trim();
+        return this;
+    }
+
+    public ClaimBuilder addRoles(Set<String> roles) {
+        for (String role : roles) {
+            addRole(role);
+        }
+        return this;
+    }
+
+    public ClaimBuilder addRole(String role) {
+        roles.add(Strings.nullToEmpty(role).trim());
+        return this;
+    }
+
+    public Claim build() {
+        return new ImmutableClaim(this);
+    }
+
+    protected static class ImmutableClaim implements Claim, Serializable {
+        private static final long serialVersionUID = -8115027645190209129L;
+        private int hashCode = 0;
+        protected String clientId;
+        protected String userId;
+        protected String user;
+        protected String domain;
+        protected ImmutableSet<String> roles;
+
+        protected ImmutableClaim(ClaimBuilder base) {
+            clientId = base.clientId;
+            userId = base.userId;
+            user = base.user;
+            domain = base.domain;
+            roles = ImmutableSet.<String>builder().addAll(base.roles).build();
+
+            if (userId.isEmpty() || user.isEmpty() || roles.isEmpty() || roles.contains("")) {
+                throw new IllegalStateException(
+                        "The Claim is missing one or more of the required fields.");
+            }
+        }
+
+        @Override
+        public String clientId() {
+            return clientId;
+        }
+
+        @Override
+        public String userId() {
+            return userId;
+        }
+
+        @Override
+        public String user() {
+            return user;
+        }
+
+        @Override
+        public String domain() {
+            return domain;
+        }
+
+        @Override
+        public Set<String> roles() {
+            return roles;
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (this == object) {
+                return true;
+            }
+            if (!(object instanceof Claim)) {
+                return false;
+            }
+            Claim claim = (Claim) object;
+            return areEqual(roles, claim.roles()) && areEqual(domain, claim.domain())
+                    && areEqual(userId, claim.userId()) && areEqual(user, claim.user())
+                    && areEqual(clientId, claim.clientId());
+        }
+
+        @Override
+        public int hashCode() {
+            if (hashCode == 0) {
+                int result = HashCodeUtil.SEED;
+                result = hash(result, clientId);
+                result = hash(result, userId);
+                result = hash(result, user);
+                result = hash(result, domain);
+                result = hash(result, roles);
+                hashCode = result;
+            }
+            return hashCode;
+        }
+
+        @Override
+        public String toString() {
+            return "clientId:" + clientId + "," + "userId:" + userId + "," + "userName:" + user
+                    + "," + "domain:" + domain + "," + "roles:" + roles;
+        }
+    }
+}
