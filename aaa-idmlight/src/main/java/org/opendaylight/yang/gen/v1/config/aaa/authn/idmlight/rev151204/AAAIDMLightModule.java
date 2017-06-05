1ce30b1320a9056787a251cb45924a03784d2fb9
Ryan Goulding <ryandgoulding@gmail.com>
2017-06-05 12:26:32 -0400

    Bug 8437: Collapse Services and transition to blueprint
    One of the major focuses of the Nitrogen release surrounds service stability
    in Karaf.  Hitherto, AAA has made use of a wide variety of service wiring
    techniques, some effective, some not so great.  The service wiring really
    was composed of three forms:
    1) blueprint (good)
    2) CSS (removed in previous patches and replaced by #1)
    3) felix dependency management, which is really old and required long waits
       to properly wire various components
    
    Since #2 was already removed, this patch removes #3, which is the wiring of
    services through ServiceTrackerCustomizers and the singleton ServiceLocator.
    ServiceLocator still exists in this patch;  to remove it here would make the
    patch much too large, but it will be refactored in a followup patch.
    
    For now, what is done is the following:
    1) removal of aaa-authn bundle.  This bundle contained Builders and utility
    classes common to aaa-idmlight and aaa-mdsal-store bundles.  They are
    implementation details, and thus not candidates for odl-aaa-api.  They were
    put in odl-aaa-shiro, as eventually odl-aaa-mdsal-cluster will eventually be
    removed and replaced with functionality built directly into odl-aaa-shiro
    (see https://git.opendaylight.org/gerrit/#/c/56744/)
    
    2) removal of aaa-authn-basic.  This bundle really didn't provide much
    more than HttpAuthBasic, which is really the only common impl of TokenAuth
    used in AAA.  This bundle's functionality was relocated to odl-aaa-shiro.
    
    3) removal of aaa-authn-sts.  STS stands for "single token store".  This
    bundle also provided limited functionality, and really didn't make sense as
    a completely separate service.
    
    This patch focuses on the consolidation of several aaa bundles and services
    into something more manageable directly in aaa-shiro.
    
    Future work will:
    1) remove the existing mdsal store in favor of the MDSAL based
       AuthorizingRealm.  Basically, the old mdsal store utilizes a lot of hacky
       code, reflection, etc. which can be cleaned and redefined to be more
       managable.  This will be removed in Oxygen, as we forgot to deprecate
       it in Nitrogen.  Messaging will be delivered to ensure that people use
       the new Realm approach once it is available.  This is again, not going
       to be the default store anyway in Nitrogen.
    
    2) further refactoring of Services so that the aaa-idmlight and aaa-h2-store
       bundles are included directly in aaa-shiro.  Basically, it is really
       against OSGi architecture to have this many services providing such
       limited functionality.  Instead, we ought to provide them as part of a
       single service, and make configuration through clustered-app-config,
       cfg files, or some other more sane way than service injection.  The
       fact remains that we really want a more statically wired configuration
       that is consistent and easy to understand.
    
    This is just part of many patches to clean the existing AAA implementation.
    
    Change-Id: I740a667278f2ff64daff5e3602ddc5586fe23733
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- aaa-idmlight/src/main/java/org/opendaylight/yang/gen/v1/config/aaa/authn/idmlight/rev151204/AAAIDMLightModule.java
+++ aaa-idmlight/src/main/java/org/opendaylight/yang/gen/v1/config/aaa/authn/idmlight/rev151204/AAAIDMLightModule.java
@@ -7,37 +7,39 @@ import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Activates h2 based data store for AAA through Aries Blueprint.
+ */
 public class AAAIDMLightModule {
 
     private static final Logger LOG = LoggerFactory.getLogger(AAAIDMLightModule.class);
-    private BundleContext bundleContext = null;
+
     private static volatile IIDMStore store = null;
 
-    public AAAIDMLightModule(IIDMStore iidmStore) {
+    public AAAIDMLightModule(final IIDMStore iidmStore) {
         store = iidmStore;
-        LOG.info("AAAIDMLight has been initalized ");
+        LOG.info("AAAIDMLight initialized with class of type {}", store.getClass().getName());
     }
 
+    /**
+     * Called by blueprint to for store initialization (i.e., add initial IDM data).
+     */
     public void initializeStore() {
         try {
             if (store != null) {
-                LOG.info("IIDMStore service {} was found", store.getClass());
+                LOG.info("IIDMStore service {} was found", store.getClass().getName());
                 new StoreBuilder(store).init();
             }
-        } catch (IDMStoreException e) {
+        } catch (final IDMStoreException e) {
             LOG.error("Failed to initialize data in store", e);
         }
-    }
-
-    public void setBundleContext(BundleContext b){
-        this.bundleContext = b;
     }
 
     public static final IIDMStore getStore(){
         return store;
     }
 
-    public static final void setStore(IIDMStore s){
+    public static final void setStore(final IIDMStore s){
         store = s;
     }
 }
---
+++ aaa-shiro/impl/src/test/java/org/opendaylight/aaa/impl/shiro/tokenauthrealm/auth/AuthenticationManagerTest.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2014, 2017 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.aaa.impl.shiro.tokenauthrealm.auth;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import org.junit.Test;
+import org.opendaylight.aaa.api.Authentication;
+import org.opendaylight.aaa.api.AuthenticationService;
+import org.osgi.service.cm.ConfigurationException;
+
+public class AuthenticationManagerTest {
+    @Test
+    public void testAuthenticationCrudSameThread() {
+        Authentication auth = new AuthenticationBuilder(new ClaimBuilder().setUser("Bob")
+                .setUserId("1234").addRole("admin").addRole("guest").build()).build();
+        AuthenticationService as = AuthenticationManager.instance();
+
+        assertNotNull(as);
+
+        as.set(auth);
+        assertEquals(auth, as.get());
+
+        as.clear();
+        assertNull(as.get());
+    }
+
+    @Test
+    public void testAuthenticationCrudSpawnedThread() throws InterruptedException,
+            ExecutionException {
+        AuthenticationService as = AuthenticationManager.instance();
+        Authentication auth = new AuthenticationBuilder(new ClaimBuilder().setUser("Bob")
+                .setUserId("1234").addRole("admin").addRole("guest").build()).build();
+
+        as.set(auth);
+        Future<Authentication> future = Executors.newSingleThreadExecutor().submit(new Worker());
+        assertEquals(auth, future.get());
+
+        as.clear();
+        future = Executors.newSingleThreadExecutor().submit(new Worker());
+        assertNull(future.get());
+    }
+
+    @Test
+    public void testAuthenticationCrudSpawnedThreadPool() throws InterruptedException,
+            ExecutionException {
+        AuthenticationService as = AuthenticationManager.instance();
+        Authentication auth = new AuthenticationBuilder(new ClaimBuilder().setUser("Bob")
+                .setUserId("1234").addRole("admin").addRole("guest").build()).build();
+
+        as.set(auth);
+        List<Future<Authentication>> fs = Executors.newFixedThreadPool(2).invokeAll(
+                Arrays.asList(new Worker(), new Worker()));
+        for (Future<Authentication> f : fs) {
+            assertEquals(auth, f.get());
+        }
+
+        as.clear();
+        fs = Executors.newFixedThreadPool(2).invokeAll(Arrays.asList(new Worker(), new Worker()));
+        for (Future<Authentication> f : fs) {
+            assertNull(f.get());
+        }
+    }
+
+    @Test
+    public void testUpdatedValid() throws ConfigurationException {
+        Dictionary<String, String> props = new Hashtable<>();
+        AuthenticationManager as = AuthenticationManager.instance();
+
+        assertFalse(as.isAuthEnabled());
+
+        props.put(AuthenticationManager.AUTH_ENABLED, "TrUe");
+        as.updated(props);
+        assertTrue(as.isAuthEnabled());
+
+        props.put(AuthenticationManager.AUTH_ENABLED, "FaLsE");
+        as.updated(props);
+        assertFalse(as.isAuthEnabled());
+    }
+
+    @Test
+    public void testUpdatedNullProperty() throws ConfigurationException {
+        AuthenticationManager as = AuthenticationManager.instance();
+
+        assertFalse(as.isAuthEnabled());
+        as.updated(null);
+        assertFalse(as.isAuthEnabled());
+    }
+
+    @Test(expected = ConfigurationException.class)
+    public void testUpdatedInvalidValue() throws ConfigurationException {
+        AuthenticationManager as = AuthenticationManager.instance();
+        Dictionary<String, String> props = new Hashtable<>();
+
+        props.put(AuthenticationManager.AUTH_ENABLED, "yes");
+        as.updated(props);
+    }
+
+    @Test(expected = ConfigurationException.class)
+    public void testUpdatedInvalidKey() throws ConfigurationException {
+        AuthenticationManager as = AuthenticationManager.instance();
+        Dictionary<String, String> props = new Hashtable<>();
+
+        props.put("Invalid Key", "true");
+        as.updated(props);
+    }
+
+    private class Worker implements Callable<Authentication> {
+        @Override
+        public Authentication call() throws Exception {
+            AuthenticationService as = AuthenticationManager.instance();
+            return as.get();
+        }
+    }
+}
