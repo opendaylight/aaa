59e3f84f8bad3e43be84c963515870b31c282160
Ryan Goulding <ryandgoulding@gmail.com>
2017-06-20 11:36:03 -0400

    DO NOT MERGE: shiro.ini needs to die
    Change-Id: Ie87886dd57958957dfd0a642fec434862903b509
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/impl/shiro/web/env/KarafIniWebEnvironment.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/impl/shiro/web/env/KarafIniWebEnvironment.java
@@ -10,12 +10,17 @@ package org.opendaylight.aaa.shiro.web.env;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.List;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.config.Ini;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.util.Factory;
 import org.apache.shiro.web.env.IniWebEnvironment;
+import org.opendaylight.aaa.impl.AAAShiroProvider;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.aaa.shiro.config.rev170619.ShiroConfiguration;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.aaa.shiro.config.rev170619.shiro.configuration.Main;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.aaa.shiro.config.rev170619.shiro.configuration.Urls;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,23 +45,49 @@ public class KarafIniWebEnvironment extends IniWebEnvironment {
      */
     static final String SHIRO_FILE_PREFIX = "file:" + File.separator;
 
+    private static final String MAIN_SECTION_HEADER = "main";
+    private static final String URLS_SECTION_HEADER = "urls";
+
     public KarafIniWebEnvironment() {
         LOG.info("Initializing the Web Environment using {}",
                 KarafIniWebEnvironment.class.getName());
     }
 
+    private static Ini createIniFromClusteredAppConfig() {
+        final Ini ini = new Ini();
+
+        final Ini.Section mainSection = ini.addSection(MAIN_SECTION_HEADER);
+        final ShiroConfiguration shiroConfiguration = AAAShiroProvider.getInstance().getShiroConfiguration();
+        final List<Main> mains = shiroConfiguration.getMain();
+        for (final Main main : mains) {
+            mainSection.put(main.getK(), main.getV());
+        }
+
+        final Ini.Section urlsSection = ini.addSection(URLS_SECTION_HEADER);
+        final List<Urls> urls = shiroConfiguration.getUrls();
+        for (final Urls url : urls) {
+            urlsSection.put(url.getK(), url.getV());
+        }
+
+        final Factory<SecurityManager> factory = new IniSecurityManagerFactory(ini);
+        final SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        return ini;
+    }
+
     @Override
     public void init() {
-        // Initialize the Shiro environment from etc/shiro.ini then delegate to
-        // the parent class
-        final Ini ini;
+        // Initialize the Shiro environment from clustered-app-config
+        final Ini ini;//createIniFromClusteredAppConfig();
         try {
             ini = createDefaultShiroIni();
             setIni(ini);
-        } catch (final FileNotFoundException e) {
-            LOG.error("Could not find etc/shiro.ini", e);
+            super.init();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-        super.init();
+
     }
 
     /**
