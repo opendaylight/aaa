f820602b04c982ded068a407a5766ea08dcd6f3c
Ryan Goulding <ryandgoulding@gmail.com>
2017-11-21 13:46:34 -0500

    AAA-151:  Invalidate claim cache for CLI initiated changes
    The AAA IDM REST handlers already invoke the clearing of the
    IdmLightProxy claimCache upon user update, but the CLI Handler did
    not handle this properly.  This change manually invokes the claim
    invalidation.
    
    Access to AAA CLI commands requires access to the karaf console via
    client or SSH.  Thus, system level access is needed to some degree
    to invoke such commands.  The major threat involves the action of
    an administrator changing a user password via CLI, and then still
    being able to invoke the old password because the claimCache is not
    purged.  This is why a reboot of karaf solves this issue;  the
    claimCache is flushed.  We should not expect our users/administrators
    to assume this in the future;  this change explicitly invokes
    the claimCache flush.
    
    AAA-151 affects Carbon, Nitrogen, and master.
    
    Change-Id: Ie3101d4dc2bbb6d0db91ddad4d567a41d041150d
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- aaa-cli/src/main/java/org/opendaylight/aaa/cli/dmstore/ChangeUserPassword.java
+++ aaa-cli/src/main/java/org/opendaylight/aaa/cli/dmstore/ChangeUserPassword.java
@@ -16,6 +16,7 @@ import org.opendaylight.aaa.api.SHA256Calculator;
 import org.opendaylight.aaa.api.model.User;
 import org.opendaylight.aaa.api.model.Users;
 import org.opendaylight.aaa.cli.utils.CliUtils;
+import org.opendaylight.aaa.impl.shiro.idm.IdmLightProxy;
 
 @Command(name = "change-user-pwd", scope = "aaa", description = "Change the user password.")
 
@@ -48,6 +49,7 @@ public class ChangeUserPassword extends OsgiCommandSupport {
         for (User usr : users.getUsers()) {
             final String realPwd = SHA256Calculator.getSHA256(currentPwd, usr.getSalt());
             if (usr.getName().equals(userName) && usr.getPassword().equals(realPwd)) {
+                IdmLightProxy.clearClaimCache();
                 usr.setPassword(newPwd);
                 identityStore.updateUser(usr);
                 return userName + "'s password has been changed";
