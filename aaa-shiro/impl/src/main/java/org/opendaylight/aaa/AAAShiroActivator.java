6d534af2fa1703c7ea479c075c7193dacd119eaf
Tom Pantelis <tompantelis@gmail.com>
2018-02-13 20:01:13 -0500

    Eliminate infinite busy wait in KarafIniWebEnvironment
    We've seen SFT's hang on shut down due to the busy wait if the
    blueprint container for aaa-shiro fails or is destroyed while
    being created. The busy wait blocks startup of the web app and also
    blocks on shut down causing the hang. To alleviate this, use a
    Future to obtain the AAAShiroProvider. Also added a BundleActivator
    to register a blueprint event listener that fails the Future
    exceptionally when the blueprint container is destroyed.
    
    Change-Id: I9198579aa026ab685af73ee624c353d7097ba86e
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/AAAShiroActivator.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2018 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa;
+
+import java.util.Hashtable;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.blueprint.container.BlueprintEvent;
+import org.osgi.service.blueprint.container.BlueprintListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Bundle Activator.
+ *
+ * @author Thomas Pantelis
+ */
+public class AAAShiroActivator implements BundleActivator, BlueprintListener {
+    private static final Logger LOG = LoggerFactory.getLogger(AAAShiroActivator.class);
+
+    private ServiceRegistration<?> eventHandlerReg;
+    private Bundle thisBundle;
+
+    @Override
+    public void start(BundleContext context) {
+        thisBundle = context.getBundle();
+        eventHandlerReg = context.registerService(BlueprintListener.class.getName(), this, new Hashtable<>());
+    }
+
+    @Override
+    public void stop(BundleContext context) {
+        AAAShiroProvider.getInstanceFuture().completeExceptionally(
+                new RuntimeException(String.format("%s bundle stopped", thisBundle)));
+
+        if (eventHandlerReg != null) {
+            try {
+                eventHandlerReg.unregister();
+            } catch (IllegalStateException e) {
+                // This can be safely ignored
+            }
+        }
+    }
+
+    @Override
+    public void blueprintEvent(BlueprintEvent event) {
+        if (!thisBundle.equals(event.getBundle())) {
+            return;
+        }
+
+        LOG.debug("Blueprint container event for bundle {}: {}", thisBundle, event.getType());
+
+        if (event.getType() == BlueprintEvent.DESTROYING) {
+            AAAShiroProvider.getInstanceFuture().completeExceptionally(new RuntimeException(
+                    String.format("Blueprint container for bundle %s is being destroyed", thisBundle)));
+        }
+    }
+}
