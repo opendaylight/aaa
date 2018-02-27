9d798e4816c4ee70871e94edc12082168a31142f
Michael Vorburger <vorburger@redhat.com>
2018-02-27 19:43:56 +0100

    add new API for programmatic registration of web Servlet, Filter, etc.
    implementation & usage of this can be seen in the next "chained" commits
    
    The purpose of this API is to let projects with web components, such as
    neutron, aaa or restconf, ditch their respective web.xml.  This will have
    a number of advantages, some of which are documented in the JavaDoc of
    the new WebServer interface and WebContext class.
    
    see also discussion and interest from project neutron re. adoption on:
    https://lists.opendaylight.org/pipermail/neutron-dev/2018-February/001587.html
    
    This is the change originally raised in infrautils as
    Ib2df87ca31a2bde547efbf73e0475a1cd64ea6ea, but now instead first proposed
    to aaa, as discussed during the Kernel Projects call on 2018/02/27;
    it should still be moved to infrautils instead soon.
    
    Change-Id: Ib2fb02aa19e49aa482062f18ba84124a9a623364
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ web/api/src/main/java/org/opendaylight/aaa/web/ServletDetails.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.web;
+
+import com.google.common.annotations.Beta;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.Servlet;
+import org.immutables.value.Value;
+import org.immutables.value.Value.Default;
+
+/**
+ * Details about a {@link Servlet}.
+ *
+ * @author Michael Vorburger.ch
+ */
+@Value.Immutable
+@Value.Style(visibility = Value.Style.ImplementationVisibility.PRIVATE, depluralize = true)
+@Beta // This only is in AAA for reasons of political asylum, but it would like to move to infrautils instead
+public interface ServletDetails {
+
+    static ServletDetailsBuilder builder() {
+        return new ServletDetailsBuilder();
+    }
+
+    Servlet servlet();
+
+    @Default default String name() {
+        return servlet().getClass().getName();
+    }
+
+    List<String> urlPatterns();
+
+    Map<String, String> initParams();
+
+}
---
+++ web/api/src/test/java/org/opendaylight/aaa/web/tests/WebContextApiTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.web.tests;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.opendaylight.infrautils.testutils.Asserts.assertThrows;
+
+import javax.servlet.Filter;
+import javax.servlet.Servlet;
+import javax.servlet.ServletContextListener;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.opendaylight.aaa.web.FilterDetails;
+import org.opendaylight.aaa.web.ServletDetails;
+import org.opendaylight.aaa.web.WebContext;
+import org.opendaylight.aaa.web.WebServer;
+
+/**
+ * Tests for Web Server {@link WebContext} API. These tests don't test an
+ * {@link WebServer} implementation; the purpose is just to compile time check
+ * against the API signatures, notably incl. their generated code.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class WebContextApiTest {
+
+    @Test
+    public void testEmptyBuilder() {
+        assertThrows(IllegalStateException.class, () -> WebContext.builder().build());
+    }
+
+    @Test
+    public void testMinimalBuilder() {
+        assertThat(WebContext.builder().contextPath("test").build().hasSessions()).isTrue();
+        assertThat(WebContext.builder().contextPath("test").hasSessions(false).build().contextPath()).isEqualTo("test");
+    }
+
+    @Test
+    public void testAddSimpleServlet() {
+        WebContext webContext = WebContext.builder().contextPath("test")
+                .addServlet(ServletDetails.builder().servlet(mock(Servlet.class)).addUrlPattern("test").build())
+                .build();
+        assertThat(webContext.servlets()).hasSize(1);
+        ServletDetails firstServletDetail = webContext.servlets().get(0);
+        assertThat(firstServletDetail.name())
+                .startsWith("$javax.servlet.Servlet$$EnhancerByMockitoWithCGLIB$$");
+        assertThat(firstServletDetail.initParams()).isEmpty();
+    }
+
+    @Test
+    public void testAddFullServlet() {
+        WebContext.builder().contextPath("test").addServlet(ServletDetails.builder().servlet(mock(Servlet.class))
+                .addUrlPattern("test").addUrlPattern("another").name("custom").putInitParam("key", "value").build())
+                .build();
+    }
+
+    @Test
+    public void testAddFilter() {
+        WebContext.builder().contextPath("test")
+            .addFilter(FilterDetails.builder().filter(mock(Filter.class)).addUrlPattern("test").build()).build();
+    }
+
+    @Test
+    public void testAddListener() {
+        assertThat(WebContext.builder().contextPath("test").addListener(mock(ServletContextListener.class)).build()
+                .listeners()).isNotEmpty();
+    }
+
+    @Test
+    public void testContextParam() {
+        assertThat(WebContext.builder().contextPath("test").putContextParam("key", "value").build().contextParams())
+                .containsExactly("key", "value");
+    }
+
+    @Test
+    @Ignore // TODO
+    public void testBadContextPath() {
+        assertThrows(IllegalArgumentException.class, () -> WebContext.builder().contextPath("test/sub").build());
+        assertThrows(IllegalArgumentException.class, () -> WebContext.builder().contextPath("test space").build());
+        assertThrows(IllegalArgumentException.class, () -> WebContext.builder().contextPath("/test").build());
+        assertThrows(IllegalArgumentException.class, () -> WebContext.builder().contextPath("test/").build());
+    }
+
+    @Test
+    @Ignore // TODO
+    public void testBadServletWithoutAnyURL() {
+        assertThrows(IllegalArgumentException.class, () -> WebContext.builder().contextPath("test")
+                .addServlet(ServletDetails.builder().servlet(mock(Servlet.class)).build()).build());
+    }
+
+    @Test
+    @Ignore // TODO
+    public void testBadFilterWithoutAnyURL() {
+        assertThrows(IllegalArgumentException.class, () -> WebContext.builder().contextPath("test")
+                .addFilter(FilterDetails.builder().filter(mock(Filter.class)).build()).build());
+    }
+
+}
