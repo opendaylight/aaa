39f43a702e1b3504122bdc2f0f1a67b8f02cb853
Tom Pantelis <tompantelis@gmail.com>
2018-03-15 12:40:17 -0400

    New shiro EnvironmentLoaderListener
    Added ShiroWebEnvironmentLoaderListener and AAAIniWebEnvironment
    that inject the required instances instead of obtaining statically.
    
    Change-Id: I5979342b7463a3634e9208eb813f32174c2a4cb4
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/KarafIniWebEnvironment.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/KarafIniWebEnvironment.java
@@ -7,57 +7,29 @@
  */
 package org.opendaylight.aaa.shiro.web.env;
 
-import java.util.List;
 import java.util.concurrent.ExecutionException;
-import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.config.Ini;
-import org.apache.shiro.config.IniSecurityManagerFactory;
-import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.util.Factory;
 import org.apache.shiro.web.env.IniWebEnvironment;
 import org.opendaylight.aaa.AAAShiroProvider;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.aaa.app.config.rev170619.ShiroConfiguration;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.aaa.app.config.rev170619.shiro.configuration.Main;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.aaa.app.config.rev170619.shiro.configuration.Urls;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Identical to <code>IniWebEnvironment</code> except the Ini is loaded from
  * <code>${KARAF_HOME}/etc/shiro.ini</code>.
+ *
+ * @deprecated in favor of {@link AAAIniWebEnvironment}. This class is kept for compatibility for other projects that
+ *             reference it in a web.xml file where it's instantiated via reflection and the dependencies must be
+ *             accessed statically.
  */
+@Deprecated
 public class KarafIniWebEnvironment extends IniWebEnvironment {
 
     private static final Logger LOG = LoggerFactory.getLogger(KarafIniWebEnvironment.class);
 
-    private static final String MAIN_SECTION_HEADER = "main";
-    private static final String URLS_SECTION_HEADER = "urls";
-
     public KarafIniWebEnvironment() {
         LOG.info("Initializing the Web Environment using {}",
                 KarafIniWebEnvironment.class.getName());
-    }
-
-    private static Ini createIniFromClusteredAppConfig(final ShiroConfiguration shiroConfiguration) {
-        final Ini ini = new Ini();
-
-        final Ini.Section mainSection = ini.addSection(MAIN_SECTION_HEADER);
-        final List<Main> mains = shiroConfiguration.getMain();
-        for (final Main main : mains) {
-            mainSection.put(main.getPairKey(), main.getPairValue());
-        }
-
-        final Ini.Section urlsSection = ini.addSection(URLS_SECTION_HEADER);
-        final List<Urls> urls = shiroConfiguration.getUrls();
-        for (final Urls url : urls) {
-            urlsSection.put(url.getPairKey(), url.getPairValue());
-        }
-
-        final Factory<SecurityManager> factory = new IniSecurityManagerFactory(ini);
-        final SecurityManager securityManager = factory.getInstance();
-        SecurityUtils.setSecurityManager(securityManager);
-
-        return ini;
     }
 
     @Override
@@ -69,7 +41,7 @@ public class KarafIniWebEnvironment extends IniWebEnvironment {
             ThreadLocals.CERT_MANAGER_TL.set(provider.getCertificateManager());
 
             // Initialize the Shiro environment from clustered-app-config
-            final Ini ini = createIniFromClusteredAppConfig(provider.getShiroConfiguration());
+            final Ini ini = AAAIniWebEnvironment.createIniFromClusteredAppConfig(provider.getShiroConfiguration());
             setIni(ini);
             super.init();
         } catch (InterruptedException | ExecutionException e) {
