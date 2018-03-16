0db27f86e42b012c53225faab4ef34c49e02b1cb
Michael Vorburger <vorburger@redhat.com>
2018-03-16 15:43:08 +0100

    introduce WebContextSecurer service API
    This API allows other projects to secure their web context, but without
    directly relying on AAA Shiro internals.  Using this, other applications
    will be able to significantly reduce their dependencies, Package-Import
    etc. to AAA Shiro internals.  (This opens the door both to more
    independently evolve aaa-shiro internals, and allows for possible
    alternative implementations, later.)
    
    This also makes aaa-shiro secure its own IdmLightApplication REST
    endpoints using the same approach, which avoids copy/paste of the
    AAAShiroFilter and the KarafIniWebEnvironmentLoaderListener it needs
    between the WebInitializer and the ShiroWebContextSecurer.
    
    Change-Id: Ia3a16df71384610a75acf3d28205c973c554d477
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/ShiroWebContextSecurer.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.shiro.web.env;
+
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import org.opendaylight.aaa.shiro.filters.AAAShiroFilter;
+import org.opendaylight.aaa.web.FilterDetails;
+import org.opendaylight.aaa.web.WebContext;
+import org.opendaylight.aaa.web.WebContextBuilder;
+import org.opendaylight.aaa.web.WebContextSecurer;
+
+/**
+ * Secures a {@link WebContext} using Shiro.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class ShiroWebContextSecurer implements WebContextSecurer {
+
+    @Override
+    public void requireAuthentication(WebContextBuilder webContextBuilder, String... urlPatterns) {
+        webContextBuilder
+            .addListener(new KarafIniWebEnvironmentLoaderListener())
+
+            // AAA filter in front of these REST web services as well as for moon endpoints
+            .addFilter(FilterDetails.builder().filter(new AAAShiroFilter()).addUrlPattern(urlPatterns).build())
+
+            // CORS filter
+            .addFilter(FilterDetails.builder().filter(new CrossOriginFilter()).addUrlPattern(urlPatterns)
+               .putInitParam("allowedOrigins", "*")
+               .putInitParam("allowedMethods", "GET,POST,OPTIONS,DELETE,PUT,HEAD")
+               .putInitParam("allowedHeaders", "origin, content-type, accept, authorization")
+               .build());
+    }
+
+}
