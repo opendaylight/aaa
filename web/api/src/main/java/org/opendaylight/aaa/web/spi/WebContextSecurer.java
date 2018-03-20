20e7dc7fe6b4cea8c55c5ac95e69962ce099d345
Michael Vorburger <vorburger@redhat.com>
2018-03-20 13:44:18 +0100

    make WebContextSecurer an SPI instead of an end-user API
    personally I think this design is worse and less clear & modular and
    simple than the original one, but at least 1 reviewer in orig Change-Id:
    Ia3a16df71384610a75acf3d28205c973c554d477 seemed to insist that this is
    better, so in the interest of unblocking and somehow moving forward, I'm
    proposing this follow-up for voting and/or comments.
    
    Change-Id: I65c98fa22e6352995892ce709a596ca3cb82deeb
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

--- web/api/src/main/java/org/opendaylight/aaa/web/spi/WebContextSecurer.java
+++ web/api/src/main/java/org/opendaylight/aaa/web/spi/WebContextSecurer.java
@@ -5,10 +5,22 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.aaa.web;
+package org.opendaylight.aaa.web.spi;
+
+import java.util.List;
+import org.opendaylight.aaa.web.WebContext;
+import org.opendaylight.aaa.web.WebContextBuilder;
+import org.opendaylight.aaa.web.WebServer;
 
 /**
  * Secures a {@link WebContextBuilder}.
+ *
+ * <p>This is a Service Provider Interface (SPI) which must be used by
+ * implementations of {@link WebServer} to be able to implement the
+ * {@link WebContext#urlPatternsRequiringAuthentication()}.
+ *
+ * <p>This interface is NOT to be used directly by code in projects
+ * which create a {@link WebContext} to register on a {@link WebServer}.
  *
  * @author Michael Vorburger.ch
  */
@@ -19,13 +31,6 @@ public interface WebContextSecurer {
      * authentication to access the given URL Patterns.  Typically, this will be done by
      * adding a <code>javax.servlet.Filter</code> (or several, and whatever else they need).
      */
-    void requireAuthentication(WebContextBuilder webContextBuilder, String... urlPatterns);
+    void requireAuthentication(WebContextBuilder webContextBuilder, List<String> urlPatterns);
 
-    /**
-     * Configures the WebContext so that all its URL patterns (<code>/**</code>) require authentication.
-     * @see #requireAuthentication(WebContextBuilder, String...)
-     */
-    default void requireAuthentication(WebContextBuilder webContextBuilder) {
-        requireAuthentication(webContextBuilder, "/*");
-    }
 }
