1f41c70c0fb1dce125292056afc56cc7847c488a
Robert Varga <robert.varga@pantheon.tech>
2018-04-13 17:08:40 +0200

    Add web/servlet-api and jersey2 implementation
    web/servlet-api provides implementation-agnostic entrypoints for
    creating Clients and HttpServlets.
    
    web/servlet-jersey2 provides an implementation based on jersey-2.25.1.
    
    This split allows us to have applications independent of the
    implementation.
    
    Change-Id: I77d92fb8764aa28817d5dcac9f8450dc42017429
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ web/servlet-jersey2/src/main/java/org/opendaylight/aaa/web/servlet/jersey2/JerseyServletSupport.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.web.servlet.jersey2;
+
+import com.google.common.annotations.Beta;
+import javax.annotation.concurrent.ThreadSafe;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Application;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+import org.kohsuke.MetaInfServices;
+import org.opendaylight.aaa.web.servlet.HttpServletBuilder;
+import org.opendaylight.aaa.web.servlet.ServletSupport;
+
+@Beta
+@MetaInfServices
+@NonNullByDefault
+@ThreadSafe
+public final class JerseyServletSupport implements ServletSupport {
+    @Override
+    public HttpServletBuilder createHttpServletBuilder(final Application application) {
+        return new JerseyHttpServletBuilder(application);
+    }
+
+    @Override
+    public ClientBuilder newClientBuilder() {
+        return new JerseyClientBuilder();
+    }
+}
