4ccd34655eef63bd6a1ca74fd7212f5cbde59f0c
Ryan Goulding <ryandgoulding@gmail.com>
2018-05-17 16:13:26 -0400

    password service api
    A generic one-way password service api.
    
    Change-Id: I6c8cb72a5cf83108b29232b6c1a8b8ae1cee21e8
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

---
+++ aaa-password-service/api/src/main/java/org/opendaylight/aaa/api/password/service/PasswordHash.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2018 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.api.password.service;
+
+/**
+ * Four-tuple representing a <code>PasswordHash</code>.
+ */
+public interface PasswordHash {
+
+    /**
+     * The algorithm name used to generate this hash.
+     *
+     * @return algorithm name used to generate this hash
+     */
+    String getAlgorithmName();
+
+    /**
+     * The salt used to generate this hash.
+     *
+     * @return salt used to generate this hash
+     */
+    String getSalt();
+
+    /**
+     * The number of iterations used to generate this hash.
+     *
+     * @return number of iterations used to generate this hash
+     */
+    int getIterations();
+
+    /**
+     * The hashed password.
+     *
+     * @return hashed password
+     */
+    String getHashedPassword();
+}
