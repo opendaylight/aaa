054d897b5a977705c7306173f2b2febcfb35384e
Ryan Goulding <ryandgoulding@gmail.com>
2018-05-17 16:13:26 -0400

    password service api
    A generic one-way password service api.
    
    Change-Id: I6c8cb72a5cf83108b29232b6c1a8b8ae1cee21e8
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

---
+++ aaa-password-service/api/src/main/java/org/opendaylight/aaa/api/password/service/PasswordService.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2018 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.aaa.api.password.service;
+
+/**
+ * Service responsible for generating and comparing passwords using one-way hashing.
+ */
+public interface PasswordService {
+
+    /**
+     * Extract a hashed password using a randomly generated salt.
+     *
+     * @param password a plaintext password
+     * @return the result of hashing the password
+     */
+    PasswordHash getHashedPassword(String password);
+
+    /**
+     * Extract a hashed password using an input salt.
+     *
+     * @param password a plaintext password
+     * @param salt the hash for <code>password</code>
+     * @return the result of hashing the password
+     */
+    PasswordHash getHashedPassword(String password, String salt);
+
+    /**
+     * Password comparison.
+     *
+     * @param plaintext the &quotinput&quot password in plaintext
+     * @param stored the Base64-encoded stored password
+     * @param salt the salt used to originally encode <code>stored</code>
+     * @return
+     */
+    boolean passwordsMatch(String plaintext, String stored, String salt);
+}
