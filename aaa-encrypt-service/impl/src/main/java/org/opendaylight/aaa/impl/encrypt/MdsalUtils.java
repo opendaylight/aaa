aaf1f173015dcd40d53078bf7e1c005eda02f3d6
Ryan Goulding <ryandgoulding@gmail.com>
2018-05-22 16:18:01 -0400

    Split aaa-encryption-service api and impl
    The original contributor jammed api and impl into one bundle.  This is
    bad practice for SOA, so this change separates out api and impl.  The
    API class package remains the same for outside consumers.
    
    Change-Id: I0e2fca345501deaf9645b4b044dbc549b222c69b
    Signed-off-by: Ryan Goulding <ryandgoulding@gmail.com>

--- aaa-encrypt-service/impl/src/main/java/org/opendaylight/aaa/impl/encrypt/MdsalUtils.java
+++ aaa-encrypt-service/impl/src/main/java/org/opendaylight/aaa/impl/encrypt/MdsalUtils.java
@@ -6,7 +6,7 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-package org.opendaylight.aaa.encrypt;
+package org.opendaylight.aaa.impl.encrypt;
 
 import com.google.common.base.Optional;
 import com.google.common.util.concurrent.CheckedFuture;
