bb8753b497a94f03b735dc279120d6c3644a0380
Jakub Tóth <jakub.toth@pantheon.tech>
2018-05-22 23:42:04 +0200

    Split OSGi dependency from AAAShiroProvider into new class
     * lets specific settings for OSGi HttpService in separate class
     * remove hardcoded dependecies on OSGI
        * moved to blueprint
    
    Change-Id: Ia50d3ac614db0404e71120917a5e5eb47ef16de1
    Signed-off-by: Jakub Tóth <jakub.toth@pantheon.tech>

---
+++ aaa-filterchain/src/test/java/org/opendaylight/aaa/filterchain/configuration/impl/CustomFilterAdapterConfigurationImplTest.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) 2016 - 2017 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.aaa.filterchain.configuration.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.aaa.filterchain.configuration.CustomFilterAdapterListener;
+import org.opendaylight.aaa.filterchain.filters.ExtractFilterConfigFilter;
+
+/**
+ * Custom Filter Adapter Configuration Test Suite.
+ */
+public class CustomFilterAdapterConfigurationImplTest {
+
+    private static class TestCustomFilterAdapterListener implements CustomFilterAdapterListener {
+        volatile List<Filter> updatedInjectedFilters;
+
+        @Override
+        public void updateInjectedFilters(final List<Filter> injectedFilters) {
+            updatedInjectedFilters = injectedFilters;
+        }
+
+        @Override
+        public FilterConfig getFilterConfig() {
+            return null;
+        }
+    }
+
+    private final CustomFilterAdapterConfigurationImpl config = new CustomFilterAdapterConfigurationImpl();
+    private final TestCustomFilterAdapterListener listener = new TestCustomFilterAdapterListener();
+
+    @Before
+    public void setup() {
+        config.registerCustomFilterAdapterConfigurationListener(listener);
+    }
+
+    // also tests "getCustomFilterList()" and
+    // "registerCustomFilterAdapaterConfigurationListener".
+    @Test
+    public void testUpdatedUnresolvableClass() {
+        // test a class that won't resolve
+        final Map<String, String> oneUnresolvableFilterMap = new HashMap<>();
+        final String oneUnresolvableFilter = "org.opendaylight.aaa.filterchain.filters.TestFilter1,"
+                + "org.opendaylight.aaa.filterchain.filters.FilterDoesntExist";
+        oneUnresolvableFilterMap.put(CustomFilterAdapterConfigurationImpl.CUSTOM_FILTER_LIST_KEY,
+                oneUnresolvableFilter);
+        config.update(oneUnresolvableFilterMap);
+        assertEquals(1, listener.updatedInjectedFilters.size());
+    }
+
+    @Test
+    public void testUpdated() {
+        // test valid input
+        final Map<String, String> updatedDictionary = new HashMap<>();
+        final String customFilterListValue = "org.opendaylight.aaa.filterchain.filters.TestFilter1,"
+                + "org.opendaylight.aaa.filterchain.filters.TestFilter2";
+        updatedDictionary.put(CustomFilterAdapterConfigurationImpl.CUSTOM_FILTER_LIST_KEY, customFilterListValue);
+        config.update(updatedDictionary);
+        assertEquals(2, listener.updatedInjectedFilters.size());
+    }
+
+    @Test
+    public void testUpdatedWithNull() {
+        // test null for updated
+        config.update(null);
+        assertEquals(0, listener.updatedInjectedFilters.size());
+    }
+
+    @Test
+    public void testUpdatedWithNonInstantiableFilter() {
+        // test with a class that cannot be instantiated
+        final String cannotInstantiateClassName = "org.opendaylight.aaa.filterchain.filters.CannotInstantiate";
+        final Map<String, String> cannotInstantiateDictionary = new HashMap<>();
+        cannotInstantiateDictionary.put(CustomFilterAdapterConfigurationImpl.CUSTOM_FILTER_LIST_KEY,
+                cannotInstantiateClassName);
+        config.update(cannotInstantiateDictionary);
+        assertEquals(0, listener.updatedInjectedFilters.size());
+    }
+
+    @Test
+    public void testUpdatedWithNonFilterClass() throws Exception {
+        // test with a class that cannot be cast to a javax.servlet.Filter
+        final String notAFilterClassName = "org.opendaylight.aaa.filterchain.filters.NotAFilter";
+        final Map<String, String> notAFilterDictionary = new HashMap<>();
+        notAFilterDictionary.put(CustomFilterAdapterConfigurationImpl.CUSTOM_FILTER_LIST_KEY, notAFilterClassName);
+        config.update(notAFilterDictionary);
+        assertEquals(0, listener.updatedInjectedFilters.size());
+    }
+
+    @Test
+    public void testUpdatedWithCfgFile() throws Exception {
+        final String filterList = "org.opendaylight.aaa.filterchain.filters.ExtractFilterConfigFilter,"
+                + "org.opendaylight.aaa.filterchain.filters.TestFilter2";
+        final Map<String, String> filterWithCfgFileDictionary = new HashMap<>();
+        filterWithCfgFileDictionary.put(CustomFilterAdapterConfigurationImpl.CUSTOM_FILTER_LIST_KEY, filterList);
+        filterWithCfgFileDictionary.put("org.opendaylight.aaa.filterchain.filters.ExtractFilterConfigFilter.key",
+                "value");
+        filterWithCfgFileDictionary.put("badkey", "badkeyvalue");
+        config.update(filterWithCfgFileDictionary);
+        assertEquals(2, listener.updatedInjectedFilters.size());
+
+        final ExtractFilterConfigFilter extractableFilter = (ExtractFilterConfigFilter) listener.updatedInjectedFilters
+                .get(0);
+        final FilterConfig filterConfig = extractableFilter.getFilterConfig();
+        final String value = filterConfig.getInitParameter("key");
+        assertNotNull(value);
+        assertEquals("value", value);
+    }
+
+    @Test
+    public void testListenerWithNonNullServletConfig() throws Exception {
+        // just ensures a non-null ServletConfig is accepted.
+        config.registerCustomFilterAdapterConfigurationListener(new CustomFilterAdapterListener() {
+            @Override
+            public void updateInjectedFilters(List<Filter> injectedFilters) {
+            }
+
+            @Override
+            public FilterConfig getFilterConfig() {
+                return new FilterConfig() {
+
+                    @Override
+                    public String getFilterName() {
+                        return null;
+                    }
+
+                    @Override
+                    public ServletContext getServletContext() {
+                        return null;
+                    }
+
+                    @Override
+                    public String getInitParameter(String string) {
+                        return null;
+                    }
+
+                    @Override
+                    public Enumeration<String> getInitParameterNames() {
+                        return null;
+                    }
+                };
+            }
+        });
+
+        config.update(null);
+        assertEquals(0, listener.updatedInjectedFilters.size());
+    }
+
+    @Test
+    public void testUpdatedFilterInitThrowsException() throws Exception {
+        final Map<String, String> initThrowsException = new HashMap<>();
+        initThrowsException.put(CustomFilterAdapterConfigurationImpl.CUSTOM_FILTER_LIST_KEY,
+                "org.opendaylight.aaa.filterchain.filters.FilterInitThrowsException");
+        config.update(initThrowsException);
+        assertEquals(1, listener.updatedInjectedFilters.size());
+    }
+}
---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/osgi/OSGiHttpServiceProvider.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright © 2018 Pantheon Technologies s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.osgi;
+
+import com.google.common.base.Preconditions;
+import java.util.Objects;
+import javax.servlet.ServletException;
+import org.opendaylight.aaa.shiro.moon.MoonTokenEndpoint;
+import org.opendaylight.aaa.shiro.oauth2.OAuth2TokenServlet;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class OSGiHttpServiceProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OSGiHttpServiceProvider.class);
+
+    private final HttpService httpService;
+    private final String moonEndpointPath;
+    private final String oauth2EndpointPath;
+
+    private OSGiHttpServiceProvider(final HttpService httpService, final String moonEndpointPath,
+            final String oauth2EndpointPath) {
+        this.httpService = Objects.requireNonNull(httpService, "httpService");
+        this.moonEndpointPath = Objects.requireNonNull(moonEndpointPath, "moonEndpointPathString");
+        this.oauth2EndpointPath = Objects.requireNonNull(oauth2EndpointPath, "oauth2EndpointPathString");
+        try {
+            registerServletContexts(httpService, moonEndpointPath, oauth2EndpointPath);
+        } catch (final ServletException | NamespaceException e) {
+            LOG.warn("Could not initialize AAA servlet endpoints", e);
+        }
+    }
+
+    public static OSGiHttpServiceProvider newInstance(final HttpService httpService, final String moonEndpointPath,
+            final String oauth2EndpointPath) {
+        return new OSGiHttpServiceProvider(httpService, moonEndpointPath, oauth2EndpointPath);
+    }
+
+    /**
+     * Method called when the blueprint container is destroyed.
+     */
+    public void close() {
+        LOG.info("AAAShiroProvider Closed");
+        if (this.httpService != null) {
+            this.httpService.unregister(this.moonEndpointPath);
+            this.httpService.unregister(this.oauth2EndpointPath);
+        }
+    }
+
+    private static void registerServletContexts(final HttpService httpService, final String moonEndpointPath,
+            final String oauth2EndpointPath) throws ServletException, NamespaceException {
+        LOG.info("attempting registration of AAA moon, oauth2 and auth servlets");
+
+        Preconditions.checkNotNull(httpService, "httpService cannot be null");
+        httpService.registerServlet(moonEndpointPath, new MoonTokenEndpoint(), null,
+                null);
+        httpService.registerServlet(oauth2EndpointPath, new OAuth2TokenServlet(), null, null);
+    }
+}
