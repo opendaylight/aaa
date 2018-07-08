b38c242f1f0f885113cb98757a56d6018b45a9c6
Tom Pantelis <tompantelis@gmail.com>
2018-07-08 11:42:19 -0400

    Handle UnknownSessionException in ODLAuthenticator
    UnknownSessionException seems to indicate the internal stored/cached
    session has expired so do an explicit logout and retry the login.
    
    Since it appears we just use the subject/session for a one-shot
    authentication, perhaps we should logout immediately after login,
    however that can be investigated later.
    
    Change-Id: I72f5a418869e5cf480b21df81be4c1b2aebf4f60
    JIRA: AAA-176
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/authenticator/ODLAuthenticator.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/authenticator/ODLAuthenticator.java
@@ -8,17 +8,19 @@
 
 package org.opendaylight.aaa.authenticator;
 
+import java.nio.charset.Charset;
+import java.util.Base64;
+import javax.servlet.http.HttpServletRequest;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.ShiroException;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.UnknownSessionException;
 import org.apache.shiro.subject.Subject;
 import org.jolokia.osgi.security.Authenticator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.servlet.http.HttpServletRequest;
-import java.nio.charset.Charset;
-import java.util.Base64;
 
 /**
  * AAA hook for <code>odl-jolokia</code> configured w/ <code>org.jolokia.authMode=service-all</code>.
@@ -28,27 +30,59 @@ public class ODLAuthenticator implements Authenticator {
 
     @Override
     public boolean authenticate(HttpServletRequest httpServletRequest) {
-        LOG.trace("Incoming Jolokia Authentication Attempt");
+        final String authorization = httpServletRequest.getHeader("Authorization");
+
+        LOG.trace("Incoming Jolokia authentication attempt: {}", authorization);
+
+        if (authorization == null || !authorization.startsWith("Basic")) {
+            return false;
+        }
 
         try {
-            final String authorization = httpServletRequest.getHeader("Authorization");
-            if (authorization != null && authorization.startsWith("Basic")) {
-                final String base64Creds = authorization.substring("Basic".length()).trim();
-                String credentials = new String(Base64.getDecoder().decode(base64Creds),
-                        Charset.forName("UTF-8"));
-                final String[] values = credentials.split(":", 2);
-                final Subject subject = SecurityUtils.getSubject();
-                UsernamePasswordToken upt = new UsernamePasswordToken();
-                upt.setUsername(values[0]);
-                upt.setPassword(values[1].toCharArray());
-                subject.login(upt);
-                return true;
+            final String base64Creds = authorization.substring("Basic".length()).trim();
+            String credentials = new String(Base64.getDecoder().decode(base64Creds), Charset.forName("UTF-8"));
+            final String[] values = credentials.split(":", 2);
+            UsernamePasswordToken upt = new UsernamePasswordToken();
+            upt.setUsername(values[0]);
+            upt.setPassword(values[1].toCharArray());
+
+            try {
+                return login(upt);
+            } catch (UnknownSessionException e) {
+                LOG.debug("Couldn't log in {} - logging out and retrying...", upt, e);
+                logout();
+                return login(upt);
             }
-        } catch (final ArrayIndexOutOfBoundsException e) {
-            LOG.trace("Jolokia Authentication attempt unsuccessful; formatting issue basic auth credentials");
-        } catch(final AuthenticationException e) {
-            LOG.trace("Jolokia Authentication attempt unsuccessful; Couldn't authenticate the subject");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            // FIXME: who throws this above and why do we need to catch it? Should this be error or warn?
+            LOG.trace("Formatting issue with basic auth credentials: {}", authorization, e);
         }
+
+        return false;
+    }
+
+    private void logout() {
+        final Subject subject = SecurityUtils.getSubject();
+        try {
+            subject.logout();
+            Session session = subject.getSession(false);
+            if (session != null) {
+                session.stop();
+            }
+        } catch (ShiroException e) {
+            LOG.debug("Couldn't log out {}", subject, e);
+        }
+    }
+
+    private boolean login(UsernamePasswordToken upt) {
+        final Subject subject = SecurityUtils.getSubject();
+        try {
+            subject.login(upt);
+            return true;
+        } catch (AuthenticationException e) {
+            LOG.trace("Couldn't authenticate the subject: {}", subject, e);
+        }
+
         return false;
     }
 }
