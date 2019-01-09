6c4cc482490638397c93370f1eeb18ff8cafb8d4
Michael Vorburger <vorburger@redhat.com>
2019-01-10 00:08:42 +0100

    introduce aaa.web.testutils with WebTestModule & TestWebClient
    Change-Id: Ib8570f3998ee99d84d3742529379990a29fbe013
    Signed-off-by: Michael Vorburger <vorburger@redhat.com>

---
+++ web/testutils/src/test/java/org/opendaylight/aaa/web/testutils/test/WebTestModuleTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2019 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.web.testutils.test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import java.io.IOException;
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.Rule;
+import org.junit.Test;
+import org.opendaylight.aaa.web.ServletDetails;
+import org.opendaylight.aaa.web.WebContext;
+import org.opendaylight.aaa.web.WebContextBuilder;
+import org.opendaylight.aaa.web.WebContextRegistration;
+import org.opendaylight.aaa.web.WebServer;
+import org.opendaylight.aaa.web.testutils.TestWebClient;
+import org.opendaylight.aaa.web.testutils.WebTestModule;
+import org.opendaylight.infrautils.inject.guice.testutils.AnnotationsModule;
+import org.opendaylight.infrautils.inject.guice.testutils.GuiceRule;
+import org.opendaylight.infrautils.testutils.web.TestWebClient.Method;
+
+/**
+ * Test the {@link WebTestModule}.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class WebTestModuleTest {
+
+    public @Rule GuiceRule guice = new GuiceRule(WebTestModule.class, AnnotationsModule.class);
+
+    @Inject WebServer webServer;
+    @Inject TestWebClient webClient;
+
+    @Test
+    public void testServlet() throws ServletException, IOException {
+        WebContextBuilder webContextBuilder = WebContext.builder().contextPath("/test1");
+        webContextBuilder.addServlet(
+                ServletDetails.builder().addUrlPattern("/hello").name("Test").servlet(new TestServlet()).build());
+        try (WebContextRegistration webContextRegistration = webServer.registerWebContext(webContextBuilder.build())) {
+            assertThat(webClient.request(Method.GET, "hello").getBody()).isEqualTo("hello, world");
+        }
+    }
+
+    @SuppressWarnings("serial")
+    class TestServlet extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse response) throws IOException {
+            response.getOutputStream().println("hello, world");
+        }
+
+    }
+}
