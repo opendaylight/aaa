6f511a35cc78d938f23758e4a4be9f167e81b3ff
Robert Varga <robert.varga@pantheon.tech>
2019-09-27 15:34:35 +0200

    Ditch dependency on infrautils.testutils
    We are depending on testutils only for WebClient. Java 11 provides
    a simple HttpClient, which is way more powerful, use that rather
    than a random home-grown thing.
    
    Change-Id: Id406b7a774e54b325a5f05f5b8ca786eab2aa522
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- web/testutils/src/main/java/org/opendaylight/aaa/web/testutils/TestWebClient.java
+++ web/testutils/src/main/java/org/opendaylight/aaa/web/testutils/TestWebClient.java
@@ -8,11 +8,16 @@
 package org.opendaylight.aaa.web.testutils;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.opendaylight.aaa.web.WebServer;
-import org.opendaylight.infrautils.testutils.web.HttpResponse;
-import org.opendaylight.infrautils.testutils.web.TestWebClient.Method;
 
 /**
  * HTTP Client.
@@ -21,15 +26,19 @@ import org.opendaylight.infrautils.testutils.web.TestWebClient.Method;
  */
 @Singleton
 public class TestWebClient {
-
-    private final org.opendaylight.infrautils.testutils.web.TestWebClient webClient;
+    private final HttpClient webClient = HttpClient.newBuilder().build();
+    private final String baseUrl;
 
     @Inject
-    public TestWebClient(WebServer webServer) {
-        this.webClient = new org.opendaylight.infrautils.testutils.web.TestWebClient(webServer.getBaseURL());
+    public TestWebClient(final WebServer webServer) {
+        final String wsUrl = webServer.getBaseURL();
+        baseUrl = wsUrl.endsWith("/") ? wsUrl : wsUrl + "/";
     }
 
-    public HttpResponse request(Method httpMethod, String path) throws IOException {
-        return webClient.request(httpMethod, path);
+    public HttpResponse<String> request(final String httpMethod, final String path)
+                throws InterruptedException, IOException, URISyntaxException {
+        final URL url = new URL(baseUrl + (path.startsWith("/") ? path.substring(1) : path));
+        return webClient.send(HttpRequest.newBuilder(url.toURI()).method(httpMethod, BodyPublishers.noBody()).build(),
+            BodyHandlers.ofString());
     }
 }
