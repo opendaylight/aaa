aa6244003b0ebf98a07d783dde85e40eac34ac0b
Robert Varga <robert.varga@pantheon.tech>
2020-10-16 21:37:49 +0200

    Convert aaa-password-service to OSGi DS
    This patch serves as a blueprint of what a replacement for blueprint's
    clustered-app-config needs to look like. Since we are replicating
    the functionality of config DS -> component, this ends up being more
    complicated than we'd want it.
    
    Providing the infrastructure replacement for odl:clustered-config
    is part of MDSAL-598. This patch provides one possibility how that would
    be realized.
    
    JIRA: AAA-203
    JIRA: MDSAL-598
    Change-Id: I760a6514ed185fc60639f7d7195159d19bf60a76
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ aaa-password-service/impl/src/main/java/org/opendaylight/aaa/impl/password/service/OSGiPasswordHashService.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.impl.password.service;
+
+import org.opendaylight.aaa.api.password.service.PasswordHash;
+import org.opendaylight.aaa.api.password.service.PasswordHashService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.aaa.password.service.config.rev170619.PasswordServiceConfig;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+
+@Component(immediate = true, service = PasswordHashService.class, property = "type=default")
+public final class OSGiPasswordHashService implements PasswordHashService {
+    private volatile DefaultPasswordHashService delegate = null;
+
+    @Override
+    public PasswordHash getPasswordHash(final String password) {
+        return delegate.getPasswordHash(password);
+    }
+
+    @Override
+    public PasswordHash getPasswordHash(final String password, final String salt) {
+        return delegate.getPasswordHash(password, salt);
+    }
+
+    @Override
+    public boolean passwordsMatch(final String plaintext, final String stored, final String salt) {
+        return delegate.passwordsMatch(plaintext, stored, salt);
+    }
+
+    @Reference(policy = ReferencePolicy.DYNAMIC, cardinality = ReferenceCardinality.MANDATORY)
+    void bindConfig(final PasswordServiceConfig config) {
+        updatedConfig(config);
+    }
+
+    void unbindConfig(final PasswordServiceConfig config) {
+        delegate = null;
+    }
+
+    void updatedConfig(final PasswordServiceConfig config) {
+        delegate = new DefaultPasswordHashService(config);
+    }
+}
