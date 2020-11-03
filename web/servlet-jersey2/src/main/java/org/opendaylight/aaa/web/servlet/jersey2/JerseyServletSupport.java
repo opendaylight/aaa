e0dbac17dbb89f890ea7c98b3e8e6fd449ea0519
Robert Varga <robert.varga@pantheon.tech>
2020-11-03 23:29:11 +0100

    Make JerseyServletSupport a @Component
    Rather than going for the hard-core osgi-core, let's use declarative
    services to activate. This reduces the amount of wiring code we have
    and we end up advertizing support for ServletSupport.
    
    While we are at it, let's also add javax.inject compatibility for
    good measure.
    
    JIRA: AAA-211
    Change-Id: I0e6d4b7ff4de15e8bff054790409b00a258bed16
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- web/servlet-jersey2/src/main/java/org/opendaylight/aaa/web/servlet/jersey2/JerseyServletSupport.java
+++ web/servlet-jersey2/src/main/java/org/opendaylight/aaa/web/servlet/jersey2/JerseyServletSupport.java
@@ -8,6 +8,8 @@
 package org.opendaylight.aaa.web.servlet.jersey2;
 
 import com.google.common.annotations.Beta;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Application;
 import org.eclipse.jdt.annotation.NonNullByDefault;
@@ -15,11 +17,19 @@ import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.kohsuke.MetaInfServices;
 import org.opendaylight.aaa.web.servlet.HttpServletBuilder;
 import org.opendaylight.aaa.web.servlet.ServletSupport;
+import org.osgi.service.component.annotations.Component;
 
 @Beta
+@Component(immediate = true)
 @MetaInfServices
+@Singleton
 @NonNullByDefault
 public final class JerseyServletSupport implements ServletSupport {
+    @Inject
+    public JerseyServletSupport() {
+        // This is a stateless service
+    }
+
     @Override
     public HttpServletBuilder createHttpServletBuilder(final Application application) {
         return new JerseyHttpServletBuilder(application);
