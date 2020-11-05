4e584a6392ef100c55e22489798d03b9d599ffe3
Tomas Cere <tomas.cere@pantheon.tech>
2020-11-05 12:10:05 +0100

    Migrate IdmLightProxy to OSGI DS
    JIRA: AAA-209
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>
    Change-Id: Ife92b83c32a3e1e6743658d2f19e9f783905769e

---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/idm/OSGIIdmLightProxy.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.shiro.idm;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ForwardingObject;
+import java.util.List;
+import org.opendaylight.aaa.api.AuthenticationException;
+import org.opendaylight.aaa.api.Claim;
+import org.opendaylight.aaa.api.ClaimCache;
+import org.opendaylight.aaa.api.CredentialAuth;
+import org.opendaylight.aaa.api.IDMStoreException;
+import org.opendaylight.aaa.api.IIDMStore;
+import org.opendaylight.aaa.api.IdMService;
+import org.opendaylight.aaa.api.PasswordCredentialAuth;
+import org.opendaylight.aaa.api.PasswordCredentials;
+import org.opendaylight.aaa.api.password.service.PasswordHashService;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+@Beta
+@Component(immediate = true, service = {CredentialAuth.class, PasswordCredentialAuth.class, IdMService.class,
+        ClaimCache.class})
+public class OSGIIdmLightProxy extends ForwardingObject implements PasswordCredentialAuth, IdMService, ClaimCache {
+
+    @Reference
+    IIDMStore iidmStore;
+
+    @Reference
+    PasswordHashService passwordHashService;
+
+    private IdmLightProxy delegate;
+
+    @Activate
+    void activate() {
+        delegate = new IdmLightProxy(iidmStore, passwordHashService);
+    }
+
+    @Deactivate
+    void deactivate() {
+        delegate = null;
+    }
+
+    @Override
+    protected IdmLightProxy delegate() {
+        return delegate;
+    }
+
+    @Override
+    public void clear() {
+        delegate().clear();
+    }
+
+    @Override
+    public List<String> listDomains(String userId) {
+        return delegate().listDomains(userId);
+    }
+
+    @Override
+    public List<String> listRoles(String userId, String domainName) {
+        return delegate().listRoles(userId, domainName);
+    }
+
+    @Override
+    public List<String> listUserIDs() throws IDMStoreException {
+        return delegate().listUserIDs();
+    }
+
+    @Override
+    public Claim authenticate(PasswordCredentials cred) throws AuthenticationException {
+        return delegate().authenticate(cred);
+    }
+}
