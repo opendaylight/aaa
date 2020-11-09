8af5d9edf05cba5db22fddcf7e8c9a6538200690
Tomas Cere <tomas.cere@pantheon.tech>
2020-11-09 14:23:09 +0100

    WIP: Convert aaa-shiro to OSGI DS
    TODO: fill in default config values from xml
    
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>
    Change-Id: Ib5c5f72300fe7ffa2c4a3edd5dc1f925cb41b014

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/ShiroWebEnvironmentLoaderListener.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/ShiroWebEnvironmentLoaderListener.java
@@ -7,9 +7,12 @@
  */
 package org.opendaylight.aaa.shiro.web.env;
 
+import javax.inject.Inject;
 import javax.servlet.ServletContext;
+import javax.servlet.ServletContextListener;
 import org.apache.shiro.web.env.EnvironmentLoaderListener;
 import org.apache.shiro.web.env.WebEnvironment;
+import org.opendaylight.aaa.TokenProvider;
 import org.opendaylight.aaa.api.AuthenticationService;
 import org.opendaylight.aaa.api.TokenStore;
 import org.opendaylight.aaa.api.password.service.PasswordHashService;
@@ -17,6 +20,9 @@ import org.opendaylight.aaa.cert.api.ICertificateManager;
 import org.opendaylight.aaa.shiro.tokenauthrealm.auth.TokenAuthenticators;
 import org.opendaylight.mdsal.binding.api.DataBroker;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.aaa.app.config.rev170619.ShiroConfiguration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,6 +31,7 @@ import org.slf4j.LoggerFactory;
  *
  * @author Thomas Pantelis
  */
+@Component(immediate = true, service = ServletContextListener.class)
 public class ShiroWebEnvironmentLoaderListener extends EnvironmentLoaderListener {
     private static final Logger LOG = LoggerFactory.getLogger(ShiroWebEnvironmentLoaderListener.class);
 
@@ -36,17 +43,20 @@ public class ShiroWebEnvironmentLoaderListener extends EnvironmentLoaderListener
     private final TokenStore tokenStore;
     private final PasswordHashService passwordHashService;
 
-    public ShiroWebEnvironmentLoaderListener(final ShiroConfiguration shiroConfiguration, final DataBroker dataBroker,
-                                             final ICertificateManager certificateManager,
-                                             final AuthenticationService authenticationService,
-                                             final TokenAuthenticators tokenAuthenticators, final TokenStore tokenStore,
-                                             final PasswordHashService passwordHashService) {
+    @Inject
+    @Activate
+    public ShiroWebEnvironmentLoaderListener(final @Reference ShiroConfiguration shiroConfiguration,
+                                             final @Reference DataBroker dataBroker,
+                                             final @Reference ICertificateManager certificateManager,
+                                             final @Reference AuthenticationService authenticationService,
+                                             final @Reference TokenProvider tokenProvider,
+                                             final @Reference PasswordHashService passwordHashService) {
         this.shiroConfiguration = shiroConfiguration;
         this.dataBroker = dataBroker;
         this.certificateManager = certificateManager;
         this.authenticationService = authenticationService;
-        this.tokenAuthenticators = tokenAuthenticators;
-        this.tokenStore = tokenStore;
+        this.tokenAuthenticators = tokenProvider.getTokenAuthenticators();
+        this.tokenStore = tokenProvider.getTokenStore();
         this.passwordHashService = passwordHashService;
         LOG.debug("ShiroWebEnvironmentLoaderListenerImpl created");
     }
