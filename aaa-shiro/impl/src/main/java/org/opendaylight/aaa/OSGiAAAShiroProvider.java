8088821ade9a63c0adcb072f4508cd5d0c192f17
Tomas Cere <tomas.cere@pantheon.tech>
2020-11-16 14:28:49 +0100

    WIP: Convert aaa-shiro to OSGI DS
    TODO: fill in default config values from xml in OSGiAAAShiroConfigBootstrap
    TODO: improve documentation
    TODO: remove configuration YANG files?
    
    Change-Id: Ib5c5f72300fe7ffa2c4a3edd5dc1f925cb41b014
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/OSGiAAAShiroProvider.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa;
+
+import java.util.Objects;
+import org.opendaylight.aaa.api.AuthenticationService;
+import org.opendaylight.aaa.api.IIDMStore;
+import org.opendaylight.aaa.api.PasswordCredentialAuth;
+import org.opendaylight.aaa.api.TokenStore;
+import org.opendaylight.aaa.api.password.service.PasswordHashService;
+import org.opendaylight.aaa.cert.api.ICertificateManager;
+import org.opendaylight.aaa.tokenauthrealm.auth.TokenAuthenticators;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.aaa.app.config.rev170619.DatastoreConfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.aaa.app.config.rev170619.DatastoreConfigBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.aaa.app.config.rev170619.ShiroConfiguration;
+import org.opendaylight.yangtools.yang.common.Uint64;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component(immediate = true, service = TokenProvider.class)
+@Designate(ocd = OSGiAAAShiroProvider.DatastoreConfiguration.class)
+public class OSGiAAAShiroProvider implements TokenProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OSGiAAAShiroProvider.class);
+
+    @ObjectClassDefinition(description = "DatastoreConfiguration for aaa-shiro.")
+    public @interface DatastoreConfiguration {
+        @AttributeDefinition(description = "Store type")
+        String storeType() default "h2-data-store";
+
+        @AttributeDefinition(description = "Time to live for tokens in seconds")
+        long timeToLive() default 36000;
+
+        @AttributeDefinition(description = "Time to wait for tokens in seconds")
+        long timeToWait() default 3600;
+    }
+
+    private static final String MOON_ENDPOINT_PATH = "/moon";
+    private static final String OATH_ENDPOINT_PATH = "/oath2";
+
+    private AAAShiroProvider delegate;
+
+    @Activate
+    public OSGiAAAShiroProvider(final DatastoreConfiguration datastoreConfiguration,
+            final @Reference DataBroker dataBroker, final @Reference ICertificateManager certificateManager,
+            final @Reference PasswordCredentialAuth credentialAuth, final @Reference HttpService httpService,
+            final @Reference IIDMStore iidmStore, final @Reference AuthenticationService authenticationService,
+            final @Reference PasswordHashService passwordHashService,
+            final @Reference ShiroConfiguration shiroConfig) {
+        LOG.warn("Activate");
+
+        delegate = new AAAShiroProvider(dataBroker, certificateManager, credentialAuth, shiroConfig,
+                httpService, MOON_ENDPOINT_PATH, OATH_ENDPOINT_PATH,
+                processDatastoreConfiguration(datastoreConfiguration), iidmStore, authenticationService,
+                passwordHashService);
+        delegate.init();
+    }
+
+    @Override
+    public TokenAuthenticators getTokenAuthenticators() {
+        return delegate.getTokenAuthenticators();
+    }
+
+    @Override
+    public TokenStore getTokenStore() {
+        return delegate.getTokenStore();
+    }
+
+    private static DatastoreConfig processDatastoreConfiguration(DatastoreConfiguration datastoreConfiguration) {
+        Objects.requireNonNull(datastoreConfiguration);
+
+        return new DatastoreConfigBuilder()
+                .setStore(DatastoreConfig.Store.forName(datastoreConfiguration.storeType()))
+                .setTimeToLive(Uint64.valueOf(datastoreConfiguration.timeToLive()))
+                .setTimeToWait(Uint64.valueOf(datastoreConfiguration.timeToWait()))
+                .build();
+    }
+}
