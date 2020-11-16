3fa7b12827720383757da321f30232d6cabd60c6
Tomas Cere <tomas.cere@pantheon.tech>
2020-11-16 14:28:49 +0100

    WIP: Convert aaa-shiro to OSGI DS
    TODO: fill in default config values from xml
    
    Change-Id: Ib5c5f72300fe7ffa2c4a3edd5dc1f925cb41b014
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/WebInitializer.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/WebInitializer.java
@@ -23,6 +23,9 @@ import org.opendaylight.aaa.web.WebContextSecurer;
 import org.opendaylight.aaa.web.WebServer;
 import org.opendaylight.aaa.web.servlet.ServletSupport;
 import org.opendaylight.yangtools.concepts.Registration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * Initializer for web components.
@@ -33,13 +36,17 @@ import org.opendaylight.yangtools.concepts.Registration;
  * @author Michael Vorburger.ch
  */
 @Singleton
+@Component(immediate = true)
 public class WebInitializer {
     private final Registration registraton;
 
     @Inject
-    public WebInitializer(final WebServer webServer, final ClaimCache claimCache, final IIDMStore iidMStore,
-            final WebContextSecurer webContextSecurer, final ServletSupport servletSupport,
-            final CustomFilterAdapterConfiguration customFilterAdapterConfig) throws ServletException {
+    @Activate
+    public WebInitializer(@Reference WebServer webServer, @Reference ClaimCache claimCache,
+                          @Reference IIDMStore iidMStore, @Reference WebContextSecurer webContextSecurer,
+                          @Reference ServletSupport servletSupport,
+                          @Reference CustomFilterAdapterConfiguration customFilterAdapterConfig)
+            throws ServletException {
 
         final var webContextBuilder = WebContext.builder()
             .name("OpenDaylight IDM realm management")
