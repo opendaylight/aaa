107ab2f10f9ae9ae4c7e96e32b2f360bc084dc33
Tomas Cere <tomas.cere@pantheon.tech>
2020-11-16 14:28:49 +0100

    Migrate aaa-cert to OSGI DS
    Remove XML configuration and replace it with
    OSGi Configuration Admin
    
    JIRA: AAA-206
    Change-Id: I89f6e97f60846456b567e274e9b994ada65444a3
    Signed-off-by: Tomas Cere <tomas.cere@pantheon.tech>
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

---
+++ aaa-cert/src/main/java/org/opendaylight/aaa/cert/impl/OSGiAaaCertRpcServiceProvider.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.cert.impl;
+
+import static com.google.common.base.Verify.verifyNotNull;
+
+import org.opendaylight.aaa.encrypt.AAAEncryptionService;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.mdsal.binding.api.RpcProviderService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.yang.aaa.cert.rev151126.AaaCertServiceConfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.yang.aaa.cert.rpc.rev151215.AaaCertRpcService;
+import org.opendaylight.yangtools.concepts.ObjectRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+public class OSGiAaaCertRpcServiceProvider {
+    private static final Logger LOG = LoggerFactory.getLogger(OSGiAaaCertRpcServiceProvider.class);
+    private final DataBroker dataBroker;
+    private final AAAEncryptionService encryptionService;
+    private final RpcProviderService rpcService;
+
+    private volatile AaaCertRpcService delegate;
+
+    private ObjectRegistration<AaaCertRpcService> registration;
+
+    @Activate
+    public OSGiAaaCertRpcServiceProvider(@Reference final DataBroker dataBroker,
+            @Reference final AAAEncryptionService encryptionService, @Reference final RpcProviderService rpcService) {
+        this.dataBroker = dataBroker;
+        this.encryptionService = encryptionService;
+        this.rpcService = rpcService;
+    }
+
+    @Reference
+    void init(final AaaCertServiceConfig certServiceConfig) {
+        LOG.info("Initializing OSGiAaaCertRpcServiceProvider...");
+        delegate = new AaaCertRpcServiceImpl(certServiceConfig, verifyNotNull(dataBroker),
+                verifyNotNull(encryptionService));
+        if (registration != null) {
+            registration.close();
+        }
+        registration = verifyNotNull(rpcService).registerRpcImplementation(AaaCertRpcService.class, delegate);
+        LOG.info("AaaCertRpcService was successfully registered to RpcService");
+    }
+
+    @Deactivate
+    void close() {
+        LOG.info("Closing OSGiAaaCertRpcServiceProvider...");
+        if (registration != null) {
+            registration.close();
+            LOG.info("AaaCertRpcService was successfully unregistered from RpcService");
+        }
+    }
+}
---
+++ aaa-cert/src/main/java/org/opendaylight/aaa/cert/impl/OSGiCertServiceConfig.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2020 PANTHEON.tech, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.cert.impl;
+
+import static com.google.common.base.Verify.verifyNotNull;
+import static java.util.Objects.requireNonNull;
+
+import com.google.common.annotations.Beta;
+import com.google.common.collect.ForwardingObject;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.Map;
+import org.eclipse.jdt.annotation.NonNull;
+import org.eclipse.jdt.annotation.Nullable;
+import org.gaul.modernizer_maven_annotations.SuppressModernizer;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.yang.aaa.cert.rev151126.AaaCertServiceConfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.yang.aaa.cert.rev151126.aaa.cert.service.config.CtlKeystore;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.yang.aaa.cert.rev151126.aaa.cert.service.config.TrustKeystore;
+import org.opendaylight.yangtools.yang.binding.Augmentation;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+@Beta
+@Component(factory = OSGiCertServiceConfig.FACTORY_NAME)
+public final class OSGiCertServiceConfig extends ForwardingObject implements AaaCertServiceConfig {
+    // Keys to for activation properties
+    private static final String DELEGATE = ".delegate";
+    static final String FACTORY_NAME = "org.opendaylight.aaa.cert.impl.OSGiCertServiceConfig";
+
+    private final AaaCertServiceConfig delegate;
+
+    @Activate
+    public OSGiCertServiceConfig(final Map<String, AaaCertServiceConfig> properties) {
+        delegate = verifyNotNull(properties.get(DELEGATE));
+    }
+
+    @Override
+    public Boolean getUseConfig() {
+        return delegate().getUseConfig();
+    }
+
+    @Override
+    public Boolean getUseMdsal() {
+        return delegate().getUseMdsal();
+    }
+
+    @Override
+    public @Nullable String getBundleName() {
+        return delegate().getBundleName();
+    }
+
+    @Override
+    public @Nullable CtlKeystore getCtlKeystore() {
+        return delegate().getCtlKeystore();
+    }
+
+    @Override
+    public @NonNull CtlKeystore nonnullCtlKeystore() {
+        return delegate().nonnullCtlKeystore();
+    }
+
+    @Override
+    public @Nullable TrustKeystore getTrustKeystore() {
+        return delegate().getTrustKeystore();
+    }
+
+    @Override
+    public @NonNull TrustKeystore nonnullTrustKeystore() {
+        return delegate().nonnullTrustKeystore();
+    }
+
+    @Override
+    public @NonNull Map<Class<? extends Augmentation<AaaCertServiceConfig>>,
+            Augmentation<AaaCertServiceConfig>> augmentations() {
+        return delegate().augmentations();
+    }
+
+    @SuppressModernizer
+    static Dictionary<String, AaaCertServiceConfig> props(final AaaCertServiceConfig delegate) {
+        final var ret = new Hashtable<String, AaaCertServiceConfig>(2);
+        ret.put(DELEGATE, requireNonNull(delegate));
+        return ret;
+    }
+
+    @Override
+    protected AaaCertServiceConfig delegate() {
+        return verifyNotNull(delegate);
+    }
+}
