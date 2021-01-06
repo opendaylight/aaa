9bd949c06377ea969d77bc0b7e4e22aa0264e335
Robert Varga <robert.varga@pantheon.tech>
2021-01-06 12:26:12 +0100

    WIP: Split out H2 IIDStore
    // FIXME: update features
    // FIXME: resolve naming (aaa.idmstore.h2)
    // FIXME: clean up the initialization bit
              - just drop the MD-SAL configuration, use service admin
                instead
    
    Change-Id: I295ec75662ce9bf46e881aa1cdfb8f7ec06c2118
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- aaa-authn-h2/src/test/java/org/opendaylight/aaa/authn/h2/GrantStoreTest.java
+++ aaa-authn-h2/src/test/java/org/opendaylight/aaa/authn/h2/GrantStoreTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2014, 2017 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
+ * Copyright (c) 2014, 2016 Hewlett-Packard Development Company, L.P. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.aaa.datastore.h2;
+package org.opendaylight.aaa.authn.h2;
 
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -14,52 +14,58 @@ import static org.mockito.Mockito.verify;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import org.junit.Test;
 import org.mockito.Mockito;
-import org.opendaylight.aaa.api.model.Roles;
+import org.opendaylight.aaa.api.model.Grants;
 
-public class RoleStoreTest {
+public class GrantStoreTest {
 
     private final Connection connectionMock = mock(Connection.class);
 
     private final ConnectionProvider connectionFactoryMock = () -> connectionMock;
 
-    private final RoleStore roleStoreUnderTest = new RoleStore(connectionFactoryMock);
+    private final GrantStore grantStoreUnderTest = new GrantStore(connectionFactoryMock);
+
+    private final String did = "5";
+    private final String uid = "5";
 
     @Test
-    public void getRolesTest() throws SQLException, Exception {
+    public void getGrantsTest() throws Exception {
         // Setup Mock Behavior
         String[] tableTypes = { "TABLE" };
         Mockito.when(connectionMock.isClosed()).thenReturn(false);
         DatabaseMetaData dbmMock = mock(DatabaseMetaData.class);
         Mockito.when(connectionMock.getMetaData()).thenReturn(dbmMock);
         ResultSet rsUserMock = mock(ResultSet.class);
-        Mockito.when(dbmMock.getTables(null, null, "ROLES", tableTypes)).thenReturn(rsUserMock);
+        Mockito.when(dbmMock.getTables(null, null, "GRANTS", tableTypes)).thenReturn(rsUserMock);
         Mockito.when(rsUserMock.next()).thenReturn(true);
 
-        Statement stmtMock = mock(Statement.class);
-        Mockito.when(connectionMock.createStatement()).thenReturn(stmtMock);
+        PreparedStatement pstmtMock = mock(PreparedStatement.class);
+        Mockito.when(connectionMock.prepareStatement(anyString())).thenReturn(pstmtMock);
 
         ResultSet rsMock = getMockedResultSet();
-        Mockito.when(stmtMock.executeQuery(anyString())).thenReturn(rsMock);
+        Mockito.when(pstmtMock.executeQuery()).thenReturn(rsMock);
 
         // Run Test
-        Roles roles = roleStoreUnderTest.getRoles();
+        Grants grants = grantStoreUnderTest.getGrants(did, uid);
 
         // Verify
-        assertTrue(roles.getRoles().size() == 1);
-        verify(stmtMock).close();
+        assertTrue(grants.getGrants().size() == 1);
+        verify(pstmtMock).close();
     }
 
     public ResultSet getMockedResultSet() throws SQLException {
         ResultSet rsMock = mock(ResultSet.class);
         Mockito.when(rsMock.next()).thenReturn(true).thenReturn(false);
-        Mockito.when(rsMock.getInt(RoleStore.SQL_ID)).thenReturn(1);
-        Mockito.when(rsMock.getString(RoleStore.SQL_NAME)).thenReturn("RoleName_1");
-        Mockito.when(rsMock.getString(RoleStore.SQL_DESCR)).thenReturn("Desc_1");
+        Mockito.when(rsMock.getInt(GrantStore.SQL_ID)).thenReturn(1);
+        Mockito.when(rsMock.getString(GrantStore.SQL_TENANTID)).thenReturn(did);
+        Mockito.when(rsMock.getString(GrantStore.SQL_USERID)).thenReturn(uid);
+        Mockito.when(rsMock.getString(GrantStore.SQL_ROLEID)).thenReturn("Role_1");
+
         return rsMock;
     }
+
 }
