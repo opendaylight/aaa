1b54fccdcc7381a23f6684facb589a0f39079c4d
tadei.bilan <tadei.bilan@pantheon.tech>
2021-07-08 13:51:08 +0300

    Split out datastore implementation from aaa-shiro
    Split H2 database from aaa-shiro into separate module.
    
    JIRA: AAA-208
    Change-Id: Ie90e7d7379e598bbf1b62436a27dae8a8b8c7372
    Signed-off-by: tadei.bilan <tadei.bilan@pantheon.tech>

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/ThreadLocals.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/ThreadLocals.java
@@ -10,8 +10,8 @@ package org.opendaylight.aaa.shiro.web.env;
 import org.opendaylight.aaa.api.AuthenticationService;
 import org.opendaylight.aaa.api.TokenStore;
 import org.opendaylight.aaa.api.password.service.PasswordHashService;
+import org.opendaylight.aaa.api.tokenauthrealm.auth.TokenAuthenticators;
 import org.opendaylight.aaa.cert.api.ICertificateManager;
-import org.opendaylight.aaa.shiro.tokenauthrealm.auth.TokenAuthenticators;
 import org.opendaylight.mdsal.binding.api.DataBroker;
 
 /**
