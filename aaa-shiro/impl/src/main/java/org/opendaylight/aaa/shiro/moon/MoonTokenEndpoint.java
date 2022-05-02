ca18e9fc0e86f8c644dd54c65b8008c5253dd1dd
Robert Varga <robert.varga@pantheon.tech>
2022-05-02 20:55:08 +0200

    Use SCR/HTTP Whiteboard for MoonTokenEndpoint
    Rather than going through blueprint and HttpService directly, use SCR to
    publish MoonTokenEndpoint to HTTP Whiteboard so it is properly
    processed.
    
    Change-Id: I10a1bad89448d071785b6f76649be817a0721874
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/moon/MoonTokenEndpoint.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/moon/MoonTokenEndpoint.java
@@ -8,16 +8,28 @@
 package org.opendaylight.aaa.shiro.moon;
 
 import java.io.IOException;
+import javax.servlet.Servlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardServletName;
+import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardServletPattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MoonTokenEndpoint extends HttpServlet {
-
+// FIXME: @WebServlet?
+@HttpWhiteboardServletPattern("/moon")
+@HttpWhiteboardServletName("MoonTokenEndpoint")
+@Component(service = Servlet.class, scope = ServiceScope.PROTOTYPE)
+public final class MoonTokenEndpoint extends HttpServlet {
     private static final Logger LOG = LoggerFactory.getLogger(MoonTokenEndpoint.class);
     private static final long serialVersionUID = 4980356362831585417L;
+
+    public MoonTokenEndpoint() {
+        // required public constructor
+    }
 
     @Override
     public String getServletName() {
