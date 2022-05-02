a8e9e36a1c516a466ee90e6f58e4e902dd657ce9
Robert Varga <robert.varga@pantheon.tech>
2022-05-02 22:04:52 +0200

    Publish ShiroWebEnvironmentLoaderListener to HTTP whiteboard
    As part of ditching WebSecurer, convert let HTTP Whiteboard understand
    this is a listener.
    
    Change-Id: I8840055856a6730197b11471b2e315dc6f83764f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/ShiroWebEnvironmentLoaderListener.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/ShiroWebEnvironmentLoaderListener.java
@@ -25,7 +25,9 @@ import org.slf4j.LoggerFactory;
  *
  * @author Thomas Pantelis
  */
-public class ShiroWebEnvironmentLoaderListener extends EnvironmentLoaderListener {
+// FIXME: @WebListener and CDI (or whatever) injection?
+// FIXME: @Component with ServletContextListener
+public final class ShiroWebEnvironmentLoaderListener extends EnvironmentLoaderListener {
     private static final Logger LOG = LoggerFactory.getLogger(ShiroWebEnvironmentLoaderListener.class);
 
     private final ShiroConfiguration shiroConfiguration;
@@ -36,6 +38,8 @@ public class ShiroWebEnvironmentLoaderListener extends EnvironmentLoaderListener
     private final TokenStore tokenStore;
     private final PasswordHashService passwordHashService;
 
+    // FIXME: @Inject for CDI, except we have ShiroConfiguration injected
+    // FIXME: @Activate for OSGi @Component, except we have ShiroConfiguration which needs to come from somewhere
     public ShiroWebEnvironmentLoaderListener(final ShiroConfiguration shiroConfiguration, final DataBroker dataBroker,
                                              final ICertificateManager certificateManager,
                                              final AuthenticationService authenticationService,
