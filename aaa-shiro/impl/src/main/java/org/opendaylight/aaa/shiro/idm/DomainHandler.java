bea76ded5e5d4b6b12c8258d3ff59f361e7ee8b2
Robert Varga <robert.varga@pantheon.tech>
2022-07-01 15:38:10 +0200

    Use MediaType.APPLICATION_JSON
    We have a number of repetitions of application/json -- use the JAX-RS
    constant for that.
    
    Change-Id: I672440b10ec1ee181240b85ac4fd6d91ac9c6121
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/idm/DomainHandler.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/idm/DomainHandler.java
@@ -20,6 +20,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriInfo;
@@ -67,7 +68,7 @@ public class DomainHandler {
      * @return a response with all domains stored in the H2 database
      */
     @GET
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getDomains() {
         LOG.info("Get /domains");
         final Domains domains;
@@ -92,7 +93,7 @@ public class DomainHandler {
      */
     @GET
     @Path("/{id}")
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getDomain(@PathParam("id") final String domainId) {
         LOG.info("Get /domains/{}", domainId);
         final Domain domain;
@@ -127,8 +128,8 @@ public class DomainHandler {
      * @return A response stating success or failure of domain creation.
      */
     @POST
-    @Consumes("application/json")
-    @Produces("application/json")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response createDomain(@Context final UriInfo info, final Domain domain) {
         LOG.info("Post /domains");
 
@@ -176,8 +177,8 @@ public class DomainHandler {
      */
     @PUT
     @Path("/{id}")
-    @Consumes("application/json")
-    @Produces("application/json")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response putDomain(@Context final UriInfo info, final Domain domain,
             @PathParam("id") final String domainId) {
         LOG.info("Put /domains/{}", domainId);
@@ -255,8 +256,8 @@ public class DomainHandler {
      */
     @POST
     @Path("/{did}/users/{uid}/roles")
-    @Consumes("application/json")
-    @Produces("application/json")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response createGrant(@Context final UriInfo info, @PathParam("did") final String domainId,
             @PathParam("uid") final String userId, final Grant grant) {
         LOG.info("Post /domains/{}/users/{}/roles", domainId, userId);
@@ -378,8 +379,8 @@ public class DomainHandler {
      */
     @POST
     @Path("/{did}/users/roles")
-    @Consumes("application/json")
-    @Produces("application/json")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response validateUser(@Context final UriInfo info, @PathParam("did") final String domainId,
             final UserPwd userpwd) {
         LOG.info("GET /domains/{}/users", domainId);
@@ -477,7 +478,7 @@ public class DomainHandler {
      */
     @GET
     @Path("/{did}/users/{uid}/roles")
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getRoles(@Context final UriInfo info, @PathParam("did") final String domainId,
             @PathParam("uid") final String userId) {
         LOG.info("GET /domains/{}/users/{}/roles", domainId, userId);
