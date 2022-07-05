5924d89b329af0ab5e4b825b54ab612d33a5b564
Robert Varga <robert.varga@pantheon.tech>
2022-07-05 22:27:09 +0200

    Clean up H2 SQL interactions
    We have a rather ugly mess around consistency of various queries. Clean
    them up, so we can comfortably change things around in future.
    
    JIRA: AAA-221
    Change-Id: Ide9ad6fb7028ba996ed8c1ed60df9cae17318999
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ aaa-idm-store-h2/src/main/java/org/opendaylight/aaa/datastore/h2/SQLTable.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.datastore.h2;
+
+import static com.google.common.base.Verify.verify;
+
+import com.google.common.base.MoreObjects;
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Enumeration of tables in our schema.
+ */
+@NonNullByDefault
+enum SQLTable {
+    /**
+     * Domains.
+     */
+    // FIXME: Yeah, say more in documentation:
+    //        - what is a domain?
+    //        - how does it relate to others?
+    DOMAIN(DomainStore.TABLE),
+    /**
+     * Users.
+     */
+    // FIXME: Yeah, say more in documentation
+    USER(UserStore.TABLE),
+    /**
+     * Roles.
+     */
+    // FIXME: Yeah, say more in documentation
+    ROLE(RoleStore.TABLE),
+    /**
+     * Grants. Probably just a (domain, user, role) tuple, but do not take my word for it.
+     */
+    // FIXME: Yeah, say more in documentation
+    GRANT(GrantStore.TABLE);
+
+    private final String tableName;
+
+    SQLTable(final String name) {
+        tableName = name;
+    }
+
+    String tableName() {
+        return tableName;
+    }
+
+    void verifyTable(final String storeTableName) {
+        verify(tableName.equals(storeTableName), "Mismatched table name '%s' with allocation %s", storeTableName, this);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(name()).add("tableName", tableName).toString();
+    }
+}
