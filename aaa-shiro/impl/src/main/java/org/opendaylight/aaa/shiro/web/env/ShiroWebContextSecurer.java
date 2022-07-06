58bfb23476aa02f7f44e6c8d92816ca83f572282
Robert Varga <robert.varga@pantheon.tech>
2022-07-06 19:08:03 +0200

    Refactor Shiro/HTTP integration
    ShiroFilter is designed to work with EnvironmentLoaderListener, both of
    which are designed to work with web.xml.
    
    We are in programmatic-land, hence the indirection through
    ServletContext lifecycle is completely unnecessary and actually is
    hurtful, as we cannot smuggle WebEnvironment through OSGi HTTP
    Whiteboard -- it requires String properties when coupled with
    ServletContextHelper.
    
    Here we create a much more direct connection, where AAAWebEnvironment
    is eagerly-initialized and it is passed directly to AAAShiroFilter. This
    allows us to use AbstractShiroFilter only without having to dance around
    init() methods -- and eliminate binding to to static SecurityManager
    wiring as well.
    
    JIRA: AAA-225
    Change-Id: Id3990dd533b5e60f3781f9e4cdc3114163ecb931
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/ShiroWebContextSecurer.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/ShiroWebContextSecurer.java
@@ -9,7 +9,7 @@ package org.opendaylight.aaa.shiro.web.env;
 
 import static java.util.Objects.requireNonNull;
 
-import org.apache.shiro.web.env.EnvironmentLoaderListener;
+import org.apache.shiro.web.env.WebEnvironment;
 import org.opendaylight.aaa.shiro.filters.AAAShiroFilter;
 import org.opendaylight.aaa.web.FilterDetails;
 import org.opendaylight.aaa.web.WebContext;
@@ -22,20 +22,19 @@ import org.opendaylight.aaa.web.WebContextSecurer;
  * @author Michael Vorburger.ch
  */
 public class ShiroWebContextSecurer implements WebContextSecurer {
-    private final EnvironmentLoaderListener environmentLoaderListener;
+    private final WebEnvironment webEnvironment;
 
-    public ShiroWebContextSecurer(final EnvironmentLoaderListener environmentLoaderListener) {
-        this.environmentLoaderListener = requireNonNull(environmentLoaderListener);
+    public ShiroWebContextSecurer(final WebEnvironment webEnvironment) {
+        this.webEnvironment = requireNonNull(webEnvironment);
     }
 
     @Override
     public void requireAuthentication(final WebContextBuilder webContextBuilder, final boolean asyncSupported,
             final String... urlPatterns) {
         webContextBuilder
-            .addListener(environmentLoaderListener)
             // AAA filter in front of these REST web services as well as for moon endpoints
             .addFilter(FilterDetails.builder()
-                .filter(new AAAShiroFilter())
+                .filter(new AAAShiroFilter(webEnvironment))
                 .addUrlPatterns(urlPatterns)
                 .asyncSupported(asyncSupported)
                 .build());
