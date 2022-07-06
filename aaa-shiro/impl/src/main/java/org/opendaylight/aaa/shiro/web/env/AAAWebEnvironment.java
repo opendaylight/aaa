2657a8d74c09181c0a0b70df331d68141f9d0058
Robert Varga <robert.varga@pantheon.tech>
2022-07-06 19:08:03 +0200

    Refactor Shiro/HTTP integration
    ShiroFilter is designed to work with EnvironmentLoaderListener, both of
    which are designed to work with web.xml.
    
    We are in programmatic-land, hence the indirection through
    ServletContext lifecycle is completely unnecessary and actually is
    hurtful, as we cannot smuggle WebEnvironment through OSGi HTTP
    Whiteboard -- it requires String properties when coupled with
    ServletContextHelper.
    
    Here we create a much more direct connection, where AAAWebEnvironment
    is eagerly-initialized and it is passed directly to AAAShiroFilter. This
    allows us to use AbstractShiroFilter only without having to dance around
    init() methods.
    
    JIRA: AAA-215
    Change-Id: Id3990dd533b5e60f3781f9e4cdc3114163ecb931
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/AAAWebEnvironment.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2018 Inocybe Technologies and others.  All rights reserved.
+ * Copyright (c) 2022 PANTHEON.tech, s.r.o.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.shiro.web.env;
+
+import org.apache.shiro.config.Ini;
+import org.apache.shiro.web.env.IniWebEnvironment;
+import org.opendaylight.aaa.api.AuthenticationService;
+import org.opendaylight.aaa.api.TokenStore;
+import org.opendaylight.aaa.api.password.service.PasswordHashService;
+import org.opendaylight.aaa.cert.api.ICertificateManager;
+import org.opendaylight.aaa.shiro.realm.KeystoneAuthRealm;
+import org.opendaylight.aaa.shiro.realm.MDSALDynamicAuthorizationFilter;
+import org.opendaylight.aaa.shiro.realm.MdsalRealm;
+import org.opendaylight.aaa.shiro.realm.MoonRealm;
+import org.opendaylight.aaa.shiro.realm.TokenAuthRealm;
+import org.opendaylight.aaa.tokenauthrealm.auth.TokenAuthenticators;
+import org.opendaylight.aaa.web.servlet.ServletSupport;
+import org.opendaylight.mdsal.binding.api.DataBroker;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.aaa.app.config.rev170619.ShiroConfiguration;
+import org.opendaylight.yangtools.util.ClassLoaderUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Extends {@code BasicInitEnvironment} to provide the {@link Ini} configuration via a clustered app config,
+ * Initialization happens in the context of this class's ClassLoader, with dependencies being injected into their
+ * thread-local variables.
+ */
+public final class AAAWebEnvironment extends IniWebEnvironment {
+    private static final Logger LOG = LoggerFactory.getLogger(AAAWebEnvironment.class);
+
+    private AAAWebEnvironment(final Ini ini) {
+        setIni(ini);
+        configure();
+    }
+
+    public static AAAWebEnvironment create(final ShiroConfiguration shiroConfiguration, final DataBroker dataBroker,
+            final ICertificateManager certificateManager, final AuthenticationService authenticationService,
+            final TokenAuthenticators tokenAuthenticators, final TokenStore tokenStore,
+            final PasswordHashService passwordHashService, final ServletSupport servletSupport) {
+        final var ini = new Ini();
+
+        final var mainSection = ini.addSection("main");
+        for (var main : shiroConfiguration.nonnullMain()) {
+            mainSection.put(main.getPairKey(), main.getPairValue());
+        }
+
+        final var urlsSection = ini.addSection("urls");
+        for (var url : shiroConfiguration.nonnullUrls()) {
+            urlsSection.put(url.getPairKey(), url.getPairValue());
+        }
+
+        // Initialize the Shiro environment from clustered-app-config
+        return ClassLoaderUtils.getWithClassLoader(AAAWebEnvironment.class.getClassLoader(), () -> {
+            try (
+                var filterLoad = MDSALDynamicAuthorizationFilter.prepareForLoad(dataBroker);
+                var keyStoneLoad = KeystoneAuthRealm.prepareForLoad(certificateManager, servletSupport);
+                var mdsalLoad = MdsalRealm.prepareForLoad(passwordHashService, dataBroker);
+                var moonLoad = MoonRealm.prepareForLoad(servletSupport);
+                var tokenAuthLoad = TokenAuthRealm.prepareForLoad(authenticationService, tokenAuthenticators,
+                    tokenStore)) {
+                LOG.debug("AAAIniWebEnvironment created");
+                return new AAAWebEnvironment(ini);
+            }
+        });
+    }
+}
