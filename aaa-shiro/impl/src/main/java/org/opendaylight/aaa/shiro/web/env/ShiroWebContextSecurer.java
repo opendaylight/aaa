9f6cc412e8df9e752624e0271ecedb036b15ef57
Robert Varga <robert.varga@pantheon.tech>
2022-07-06 19:08:03 +0200

    Remove ShiroWebEnvironmentLoaderListener
    Rather than relying on ServletContextLifecycle, inject WebEnvironment
    as an initial parameter.
    
    Change-Id: Id3990dd533b5e60f3781f9e4cdc3114163ecb931
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/ShiroWebContextSecurer.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/web/env/ShiroWebContextSecurer.java
@@ -9,7 +9,8 @@ package org.opendaylight.aaa.shiro.web.env;
 
 import static java.util.Objects.requireNonNull;
 
-import org.apache.shiro.web.env.EnvironmentLoaderListener;
+import org.apache.shiro.web.env.EnvironmentLoader;
+import org.apache.shiro.web.env.WebEnvironment;
 import org.opendaylight.aaa.shiro.filters.AAAShiroFilter;
 import org.opendaylight.aaa.web.FilterDetails;
 import org.opendaylight.aaa.web.WebContext;
@@ -22,17 +23,20 @@ import org.opendaylight.aaa.web.WebContextSecurer;
  * @author Michael Vorburger.ch
  */
 public class ShiroWebContextSecurer implements WebContextSecurer {
-    private final EnvironmentLoaderListener environmentLoaderListener;
+    private final WebEnvironment webEnvironment;
 
-    public ShiroWebContextSecurer(final EnvironmentLoaderListener environmentLoaderListener) {
-        this.environmentLoaderListener = requireNonNull(environmentLoaderListener);
+    public ShiroWebContextSecurer(final WebEnvironment webEnvironment) {
+        this.webEnvironment = requireNonNull(webEnvironment);
     }
 
     @Override
     public void requireAuthentication(final WebContextBuilder webContextBuilder, final boolean asyncSupported,
             final String... urlPatterns) {
         webContextBuilder
-            .addListener(environmentLoaderListener)
+            // Replacement for Shiro's EnvironmentLoaderListener mechanics. Rather than reacting to the ServletContext
+            // being initialized, inject the environment directly.
+            .putContextParam(EnvironmentLoader.ENVIRONMENT_ATTRIBUTE_KEY, webEnvironment)
+
             // AAA filter in front of these REST web services as well as for moon endpoints
             .addFilter(FilterDetails.builder()
                 .filter(new AAAShiroFilter())
