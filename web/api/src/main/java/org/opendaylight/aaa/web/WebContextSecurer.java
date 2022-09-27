4ada45ea9a3f250b4f343beb8adbe2e1d2a39b9f
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2022-09-27 12:42:49 +0200

    Document and validate web-api constructs
    We ditch immutables.org and use interface/builder/record to provide
    minimal API footprint change. Any violations are flagged by builder
    setter methods.
    
    This forces a rather more thorough change in APIs, but the result is
    more worth the churn.
    
    The urlPatterns are check for compliance with Java Servlet
    Specification, version 3.1.
    
    JIRA: AAA-233
    Change-Id: If65aa0fac7ee7040e89d926bf115b4f124c5b4e2
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- web/api/src/main/java/org/opendaylight/aaa/web/WebContextSecurer.java
+++ web/api/src/main/java/org/opendaylight/aaa/web/WebContextSecurer.java
@@ -8,13 +8,16 @@
 package org.opendaylight.aaa.web;
 
 /**
- * Secures a {@link WebContextBuilder}.
+ * Secures a {@link WebContext.Builder}.
  *
  * @author Michael Vorburger.ch
  */
 public interface WebContextSecurer {
     /**
-     * Configures the WebContext in an implementation specific manner so that it requires authentication to access the
+     * Configure the WebContext to require auth for specified URLs.
+     *
+     * <p>
+     * Configure the WebContext so that it requires authentication to access the
      * given URL Patterns. Typically, this will be done by adding a {@code javax.servlet.Filter} (or several, and
      * whatever else they need).
      *
@@ -22,25 +25,36 @@ public interface WebContextSecurer {
      * @param asyncSupported true if asynchronous communication should also be supported
      * @param urlPatterns URL patterns that require authentication
      */
-    void requireAuthentication(WebContextBuilder webContextBuilder, boolean asyncSupported, String... urlPatterns);
+    void requireAuthentication(WebContext.Builder webContextBuilder, boolean asyncSupported, String... urlPatterns);
 
     /**
-     * Configures the WebContext in an implementation specific manner so that it requires authentication to access the
+     * Configure the WebContext to require auth for specified URLs.
+     *
+     * <p>
+     * Configures the WebContext so that it requires authentication to access the
      * given URL Patterns. Typically, this will be done by adding a {@code javax.servlet.Filter} (or several, and
      * whatever else they need).
      *
      * <p>
      * This method is equivalent to {@code requireAuthentication(webContextBuilder, false, urlPatterns}.
+     *
+     * @param webContextBuilder builder to secure
+     * @param urlPatterns URL patterns that require authentication
      */
-    default void requireAuthentication(final WebContextBuilder webContextBuilder, final String... urlPatterns) {
+    default void requireAuthentication(final WebContext.Builder webContextBuilder, final String... urlPatterns) {
         requireAuthentication(webContextBuilder, false, urlPatterns);
     }
 
     /**
+     * Configure the WebContext to require auth all URLs.
+     *
+     * <p>
      * Configures the WebContext so that all its URL patterns ({@code/**}) require authentication.
-     * @see #requireAuthentication(WebContextBuilder, String...)
+     *
+     * @param webContextBuilder builder to secure
+     * @see #requireAuthentication(WebContext.Builder, String...)
      */
-    default void requireAuthentication(final WebContextBuilder webContextBuilder) {
+    default void requireAuthentication(final WebContext.Builder webContextBuilder) {
         requireAuthentication(webContextBuilder, "/*");
     }
 }
