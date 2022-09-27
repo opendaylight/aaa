1f83cc2e56239c6ad3df6e8adf6f3abe15d645f7
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2022-09-27 12:42:49 +0200

    Add validation for web-api constructs
    We ditch immutables.org and use interface/builder/record to provide
    minimal API footprint change. Any violations are flagged by builder
    setter methods.
    
    This forces a rather more thorough change in APIs, but the result is
    more worth the churn.
    
    The urlPatterns are check for compliance with Java Servlet
    Specification, version 3.1.
    
    JIRA: AAA-233
    Change-Id: If65aa0fac7ee7040e89d926bf115b4f124c5b4e2
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- web/api/src/main/java/org/opendaylight/aaa/web/ResourceDetails.java
+++ web/api/src/main/java/org/opendaylight/aaa/web/ResourceDetails.java
@@ -7,32 +7,89 @@
  */
 package org.opendaylight.aaa.web;
 
-import org.immutables.value.Value;
-import org.immutables.value.Value.Default;
+import static java.util.Objects.requireNonNull;
+
+import org.eclipse.jdt.annotation.NonNull;
 
 /**
  * Details about a resource registration.
  *
  * @author Thomas Pantelis
  */
-@Value.Immutable
-@Value.Style(visibility = Value.Style.ImplementationVisibility.PRIVATE, depluralize = true)
 public interface ResourceDetails {
-
-    static ResourceDetailsBuilder builder() {
-        return new ResourceDetailsBuilder();
-    }
-
     /**
      * The base name of the resources that will be registered, typically a directory in the bundle/jar where "/"
      * is used to denote the root.
      */
-    String name();
+    @NonNull String name();
 
     /**
      * The name in the URI namespace to which the resources are mapped. This defaults to the {@link #name()}.
      */
-    @Default default String alias() {
-        return name();
+    @NonNull String alias();
+
+    static @NonNull Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Builds instances of type {@link ResourceDetails ResourceDetails}. Initialize attributes and then invoke the
+     * {@link #build()} method to create an immutable instance.
+     *
+     * <p><em>{@code ResourceDetails.Builder} is not thread-safe and generally should not be stored in a field or
+     * collection, but instead used immediately to create instances.</em>
+     */
+    final class Builder {
+        private record ImmutableResourceDetails(String name, String alias) implements ResourceDetails {
+            // Not much else here
+        }
+
+        private String name;
+        private String alias;
+
+        private Builder() {
+            // Hidden on purpose
+        }
+
+        /**
+         * Initializes the value for the {@link ResourceDetails#name() name} attribute.
+         *
+         * @param name The value for name
+         * @return {@code this} builder for use in a chained invocation
+         * @throws NullPointerException if {code name} is {@code null}
+         */
+        @SuppressWarnings("checkstyle:hiddenField")
+        public @NonNull Builder name(final String name) {
+            this.name = requireNonNull(name);
+            return this;
+        }
+
+        /**
+         * Initializes the value for the {@link ResourceDetails#alias() alias} attribute.
+         *
+         * <p><em>If not set, this attribute will have the same as {@link ResourceDetails#name() name}.</em>
+         *
+         * @param alias The value for alias
+         * @return {@code this} builder for use in a chained invocation
+         * @throws NullPointerException if {code alias} is {@code null}
+         */
+        @SuppressWarnings("checkstyle:hiddenField")
+        public @NonNull Builder alias(final String alias) {
+            this.alias = requireNonNull(alias);
+            return this;
+        }
+
+        /**
+         * Builds a new {@link ResourceDetails ResourceDetails}.
+         *
+         * @return An immutable instance of ResourceDetails
+         * @throws IllegalStateException if any required attributes are missing
+         */
+        public @NonNull ResourceDetails build() {
+            if (name == null) {
+                throw new IllegalStateException("name not specified");
+            }
+            return new ImmutableResourceDetails(name, alias == null ? name : alias);
+        }
     }
 }
