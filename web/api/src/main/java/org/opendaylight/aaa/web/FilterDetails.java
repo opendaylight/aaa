ad71e83b8f3aa2bafd1c9825fddc00fe38f28645
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2022-10-03 12:28:45 +0200

    Document web-api constructs code
    Documented FilterDetails, ServletDetails and other related classes
    according to javadoc standarts.
    
    JIRA: AAA-233
    Change-Id: Ibc9897c32686766f97447506c97dfc9c6789c2f1
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>

--- web/api/src/main/java/org/opendaylight/aaa/web/FilterDetails.java
+++ web/api/src/main/java/org/opendaylight/aaa/web/FilterDetails.java
@@ -22,26 +22,54 @@ import org.immutables.value.Value.Default;
 @Value.Style(visibility = Value.Style.ImplementationVisibility.PRIVATE, depluralize = true)
 public abstract class FilterDetails {
 
+    /**
+     * Builder for FilterDetails.
+     * @return {@link FilterDetailsBuilder}
+     */
     public static FilterDetailsBuilder builder() {
         return new FilterDetailsBuilder();
     }
 
+    /**
+     * Details about a {@link Filter}.
+     * @return {@link Filter} instance
+     */
     public abstract Filter filter();
 
+    /**
+     * Filter's class name.
+     * @return {@link String} name of class
+     */
     @Default
     public String name() {
         return filter().getClass().getName();
     }
 
+    /**
+     * Filter url patterns. Used for mapping filters. This is controls where filter is applied.
+     * Restrictions to urls and how it should look like are same as for {@link ServletDetails#urlPatterns()}.
+     * @return {@link List} of web resource mapping
+     */
     public abstract List<String> urlPatterns();
 
+    /**
+     * Filter initial parameters.
+     * @return {@link Map} that contains initial parameters
+     */
     public abstract Map<String, String> initParams();
 
+    /**
+     * Flag that used to allow async requests by AAA because SSE (Server Sent Events) use async communication.
+     * @return {@link Boolean} support async requests flag
+     */
     @Default
     public Boolean getAsyncSupported() {
         return false;
     }
 
+    /**
+     * Checking url patterns according to specification.
+     */
     @Value.Check
     protected void check() {
         urlPatterns().forEach(ServletPathSpecValidator::checkUrlPattern);
--- web/api/src/main/java/org/opendaylight/aaa/web/ServletDetails.java
+++ web/api/src/main/java/org/opendaylight/aaa/web/ServletDetails.java
@@ -22,26 +22,64 @@ import org.immutables.value.Value.Default;
 @Value.Style(visibility = Value.Style.ImplementationVisibility.PRIVATE, depluralize = true)
 public abstract class ServletDetails {
 
+    /**
+     * Builder for ServletDetails.
+     * @return {@link ServletDetailsBuilder}
+     */
     public static ServletDetailsBuilder builder() {
         return new ServletDetailsBuilder();
     }
 
+    /**
+     * Details about a {@link Servlet}.
+     * @return {@link Servlet} instance
+     */
     public abstract Servlet servlet();
 
+    /**
+     * Servlet's class name.
+     * @return {@link String} name of class
+     */
     @Default
     public String name() {
         return servlet().getClass().getName();
     }
 
+    /**
+     * Servlet url patterns. Used for mapping servlets. This is controls how you access a servlet.
+     * Restrictions to urls and how it should look like are next:
+     * <ul>
+     * <li>A string beginning with a ‘ / ’ character and ending with a ‘ /*’ suffix is used for path mapping.
+     * <li>A string beginning with a ‘ *. ’ prefix is used as an extension mapping.
+     * <li>The empty string ("") is a special URL pattern that exactly maps to the application's context root, i.e.,
+     * requests of the form http://host:port/"context-root"/. In this case the path info is ’ / ’ and the servlet
+     * path and context path is empty string (““).
+     * <li>A string containing only the ’ / ’ character indicates the "default" servlet of the application. In this case
+     * the servlet path is the request URI minus the context path and the path info is null.
+     * <li>All other strings are used for exact matches only.
+     * </ul>
+     * @return {@link List} of web resource mapping
+     */
     public abstract List<String> urlPatterns();
 
+    /**
+     * Servlet initial parameters.
+     * @return {@link Map} that contains initial parameters
+     */
     public abstract Map<String, String> initParams();
 
+    /**
+     * Flag that used to allow async requests by AAA because SSE (Server Sent Events) use async communication.
+     * @return {@link Boolean} support async requests flag
+     */
     @Default
     public Boolean getAsyncSupported() {
         return false;
     }
 
+    /**
+     * Checking url patterns according to specification.
+     */
     @Value.Check
     protected void check() {
         urlPatterns().forEach(ServletPathSpecValidator::checkUrlPattern);
