2d7197b938a24911769e14f2ac0ec8ae7ce29151
OleksandrZharov <Oleksandr.Zharov@pantheon.tech>
2023-03-20 10:42:53 +0100

    Remove OAuth2 remnants
    Removed code related to tokens and their validation from
    TokenAuthRealm class - OAuth2 is gone so we don't need it
    
    JIRA: AAA-255
    Change-Id: I67e1a155c70f6ea8a328676bd462acd97332d58b
    Signed-off-by: OleksandrZharov <Oleksandr.Zharov@pantheon.tech>

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/realm/TokenAuthRealm.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/realm/TokenAuthRealm.java
@@ -125,7 +125,6 @@ public class TokenAuthRealm extends AuthorizingRealm {
                 "{\"error\":\"Only basic authentication is supported by TokenAuthRealm\"}", e);
         }
 
-        // if the password is empty, this is an OAuth2 request, not a Basic HTTP Auth request
         if (!Strings.isNullOrEmpty(password)) {
             Map<String, List<String>> headers = HeaderUtils.formHeaders(username, password, domain);
             // iterate over <code>TokenAuth</code> implementations and
@@ -149,30 +148,7 @@ public class TokenAuthRealm extends AuthorizingRealm {
             }
         }
 
-        // extract the authentication token and attempt validation of the token
-        final String token = TokenUtils.extractUsername(authenticationToken);
-        try {
-            final Authentication auth = validate(token);
-            final ODLPrincipal odlPrincipal = ODLPrincipalImpl.createODLPrincipal(auth);
-            return new SimpleAuthenticationInfo(odlPrincipal, "", getName());
-        } catch (AuthenticationException e) {
-            LOG.debug("Unknown OAuth2 Token Access Request", e);
-        }
-
         LOG.debug("Authentication failed: exhausted TokenAuth resources");
         return null;
-    }
-
-    private Authentication validate(final String token) {
-        if (tokenStore == null) {
-            throw new AuthenticationException("Token store not available, could not validate the token " + token);
-        }
-
-        final Authentication auth = tokenStore.get(token);
-        if (auth == null) {
-            throw new AuthenticationException("Could not validate the token " + token);
-        }
-        authService.set(auth);
-        return auth;
     }
 }
