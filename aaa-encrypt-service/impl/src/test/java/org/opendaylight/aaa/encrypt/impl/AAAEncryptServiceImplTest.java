e34ab62c6576dad4bb6884ac8362455be5b7d8b9
Peter Suna <peter.suna@pantheon.tech>
2023-10-25 13:52:15 +0200

    Throw exception if decryption/encryption fails
    Previously, in the case of a failure during encryption or decryption
    in AAA, the system would only log an error and return insered string.
    This could lead user to believe that the operation was successful,
    resulting in them receiving unencrypted/undecrypted data.
    Also is possible that IllegalArgumentException is thrown which is also
    wrong.
    
    This patch simplifies things by throwing GeneralSecurityException, as
    that is quite a natural thing to do.
    
    In terms of IAEs -- this relates to String encoding and not encryption,
    so we solve this by simply not providing String-based services, forcing
    users to deal with translation themselves.
    
    Since we are in the area, also convert unit tests to JUnit5, as they are
    extremely simplistic.
    
    Also make the service null-hostile, as that it almost is -- encrypt path
    would throw IAE on null bytes, this turns it into a NPE and guards the
    decrypt path the same way.
    
    Finally we take care of the asymmetry in encrypt/descrypt when we do not
    have a key -- simply by refusing to start it we fail to initialize.
    
    JIRA: AAA-266
    Change-Id: I4c9078e293fe5b98f0e6b69568ca10a75a4fbe07
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- aaa-encrypt-service/impl/src/test/java/org/opendaylight/aaa/encrypt/impl/AAAEncryptServiceImplTest.java
+++ aaa-encrypt-service/impl/src/test/java/org/opendaylight/aaa/encrypt/impl/AAAEncryptServiceImplTest.java
@@ -7,24 +7,28 @@
  */
 package org.opendaylight.aaa.encrypt.impl;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Arrays;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.opendaylight.yang.gen.v1.config.aaa.authn.encrypt.service.config.rev160915.AaaEncryptServiceConfigBuilder;
 
 /*
  *  @author - Sharon Aicler (saichler@gmail.com)
  */
 @Deprecated
-public class AAAEncryptServiceImplTest {
+class AAAEncryptServiceImplTest {
     private AAAEncryptionServiceImpl impl;
 
-    @Before
-    public void setup() {
+    @BeforeEach
+    void setup() {
         impl = new AAAEncryptionServiceImpl(new EncryptServiceConfigImpl(
             OSGiEncryptionServiceConfigurator.generateConfig(new AaaEncryptServiceConfigBuilder()
                 .setCipherTransforms("AES/CBC/PKCS5Padding")
@@ -36,88 +40,6 @@ public class AAAEncryptServiceImplTest {
                 .setEncryptType("AES")
                 .setPasswordLength(12)
                 .build())));
-    }
-
-    @Test
-    public void testShortString() {
-        String before = "shortone";
-        String encrypt = impl.encrypt(before);
-        assertNotEquals(before, encrypt);
-        String after = impl.decrypt(encrypt);
-        assertEquals(before, after);
-    }
-
-    @Test
-    public void testLongString() {
-        String before = "This is a very long string to encrypt for testing 1...2...3";
-        String encrypt = impl.encrypt(before);
-        assertNotEquals(before, encrypt);
-        String after = impl.decrypt(encrypt);
-        assertEquals(before, after);
-    }
-
-    @Test
-    public void testNetconfEncodedPasswordWithoutPadding() {
-        changePadding();
-        String password = "bmV0Y29uZgo=";
-        String unencrypted = impl.decrypt(password);
-        assertEquals(password, unencrypted);
-    }
-
-    @Test
-    public void testNetconfEncodedPasswordWithPadding() {
-        String password = "bmV0Y29uZgo=";
-        String unencrypted = impl.decrypt(password);
-        assertEquals(password, unencrypted);
-    }
-
-    @Test
-    public void testNetconfPasswordWithoutPadding() {
-        changePadding();
-        String password = "netconf";
-        String encodedPassword = Base64.getEncoder().encodeToString(password.getBytes(StandardCharsets.UTF_8));
-        String unencrypted = impl.decrypt(encodedPassword);
-        assertEquals(encodedPassword, unencrypted);
-    }
-
-    @Test
-    public void testNetconfPasswordWithPadding() {
-        String password = "netconf";
-        String encodedPassword = Base64.getEncoder().encodeToString(password.getBytes(StandardCharsets.UTF_8));
-        String unencrypted = impl.decrypt(encodedPassword);
-        assertEquals(encodedPassword, unencrypted);
-    }
-
-    @Test
-    public void testAdminEncodedPasswordWithoutPadding() {
-        changePadding();
-        String password = "YWRtaW4K";
-        String unencrypted = impl.decrypt(password);
-        assertEquals(password, unencrypted);
-    }
-
-    @Test
-    public void testAdminEncodedPasswordWithPadding() {
-        String password = "YWRtaW4K";
-        String unencrypted = impl.decrypt(password);
-        assertEquals(password, unencrypted);
-    }
-
-    @Test
-    public void testAdminPasswordWithoutPadding() {
-        changePadding();
-        String password = "admin";
-        String encodedPassword = Base64.getEncoder().encodeToString(password.getBytes(StandardCharsets.UTF_8));
-        String unencrypted = impl.decrypt(encodedPassword);
-        assertEquals(encodedPassword, unencrypted);
-    }
-
-    @Test
-    public void testAdminPasswordWithPadding() {
-        String password = "admin";
-        String encodedPassword = Base64.getEncoder().encodeToString(password.getBytes(StandardCharsets.UTF_8));
-        String unencrypted = impl.decrypt(encodedPassword);
-        assertEquals(encodedPassword, unencrypted);
     }
 
     private void changePadding() {
@@ -132,5 +54,88 @@ public class AAAEncryptServiceImplTest {
                 .setEncryptType("AES")
                 .setPasswordLength(12)
                 .build())));
+    }
+
+    @Test
+    void testShortString() throws Exception {
+        final var before = "shortone".getBytes();
+        final var encrypt = impl.encrypt(before);
+        assertFalse(Arrays.equals(before, encrypt));
+        assertArrayEquals(before, impl.decrypt(encrypt));
+    }
+
+    @Test
+    void testLongString() throws Exception {
+        final var before = "This is a very long string to encrypt for testing 1...2...3".getBytes();
+        final var encrypt = impl.encrypt(before);
+        assertFalse(Arrays.equals(before, encrypt));
+        assertArrayEquals(before, impl.decrypt(encrypt));
+    }
+
+    @Test
+    void testNetconfEncodedPasswordWithoutPadding() {
+        changePadding();
+        final var ex = assertThrows(IllegalBlockSizeException.class, () -> impl.decrypt("netconf\n".getBytes()));
+        assertEquals("Input length not multiple of 16 bytes", ex.getMessage());
+    }
+
+    @Test
+    void testNetconfEncodedPasswordWithPadding() {
+        final var ex = assertThrows(IllegalBlockSizeException.class, () -> impl.decrypt("netconf\n".getBytes()));
+        assertEquals("Input length must be multiple of 16 when decrypting with padded cipher", ex.getMessage());
+    }
+
+    @Test
+    void testNetconfPasswordWithoutPadding() {
+        changePadding();
+        final var ex = assertThrows(IllegalBlockSizeException.class, () -> impl.decrypt("netconf".getBytes()));
+        assertEquals("Input length not multiple of 16 bytes", ex.getMessage());
+    }
+
+    @Test
+    void testNetconfPasswordWithPadding() {
+        final var ex = assertThrows(IllegalBlockSizeException.class, () -> impl.decrypt("netconf".getBytes()));
+        assertEquals("Input length must be multiple of 16 when decrypting with padded cipher", ex.getMessage());
+    }
+
+    @Test
+    void testAdminEncodedPasswordWithoutPadding() {
+        changePadding();
+        final var ex = assertThrows(IllegalBlockSizeException.class, () -> impl.decrypt("admin\n".getBytes()));
+        assertEquals("Input length not multiple of 16 bytes", ex.getMessage());
+    }
+
+    @Test
+    void testAdminEncodedPasswordWithPadding() {
+        final var ex = assertThrows(IllegalBlockSizeException.class, () -> impl.decrypt("admin\n".getBytes()));
+        assertEquals("Input length must be multiple of 16 when decrypting with padded cipher", ex.getMessage());
+    }
+
+    @Test
+    void testAdminPasswordWithoutPadding() {
+        changePadding();
+        final var ex = assertThrows(IllegalBlockSizeException.class, () -> impl.decrypt("admin".getBytes()));
+        assertEquals("Input length not multiple of 16 bytes", ex.getMessage());
+    }
+
+    @Test
+    void testAdminPasswordWithPadding() {
+        final var ex = assertThrows(IllegalBlockSizeException.class, () -> impl.decrypt("admin".getBytes()));
+        assertEquals("Input length must be multiple of 16 when decrypting with padded cipher", ex.getMessage());
+    }
+
+    @Test
+    void testDecryptWithIllegalBlockSizeException() {
+        final var ex = assertThrows(IllegalBlockSizeException.class, () -> impl.decrypt("adminadmin".getBytes()));
+        assertEquals("Input length must be multiple of 16 when decrypting with padded cipher", ex.getMessage());
+    }
+
+    @Test
+    void testDecryptWithBadPaddingException() {
+        final var bytes = new byte[] { 85, -87, 98, 116, -23, -84, 123, -82, 4, -99, -54, 29, 121, -48, -38, -75 };
+        final var ex = assertThrows(BadPaddingException.class, () -> impl.decrypt(bytes));
+        assertEquals(
+            "Given final block not properly padded. Such issues can arise if a bad key is used during decryption.",
+            ex.getMessage());
     }
 }
