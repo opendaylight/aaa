11ef0a54c30f78bc0c2140e01218f281cd3539c6
Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>
2024-02-01 13:28:31 +0200

    Use GCM mode as default
    The GCM mode operation has been set as default in
    aaa-encrypt-service-config
    
    JIRA: AAA-267
    Change-Id: Ic386bf8b466458ce4e8536f63c1edbd3f9ca695b
    Signed-off-by: Yaroslav Lastivka <yaroslav.lastivka@pantheon.tech>

--- aaa-encrypt-service/impl/src/main/java/org/opendaylight/aaa/encrypt/impl/AAAEncryptionServiceImpl.java
+++ aaa-encrypt-service/impl/src/main/java/org/opendaylight/aaa/encrypt/impl/AAAEncryptionServiceImpl.java
@@ -10,18 +10,24 @@ package org.opendaylight.aaa.encrypt.impl;
 import static com.google.common.base.Verify.verifyNotNull;
 import static java.util.Objects.requireNonNull;
 
+import java.nio.ByteBuffer;
 import java.security.GeneralSecurityException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Map;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import org.opendaylight.aaa.encrypt.AAAEncryptionService;
-import org.opendaylight.yang.gen.v1.config.aaa.authn.encrypt.service.config.rev160915.EncryptServiceConfig;
+import org.opendaylight.yang.gen.v1.config.aaa.authn.encrypt.service.config.rev240202.EncryptServiceConfig;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -40,36 +46,21 @@ public final class AAAEncryptionServiceImpl implements AAAEncryptionService {
 
     private static final Logger LOG = LoggerFactory.getLogger(AAAEncryptionServiceImpl.class);
     private static final String CONFIG_PROP = ".config";
+    private static final SecureRandom SECURE_RANDOM = new SecureRandom();
 
     private final SecretKey key;
-    private final Cipher encryptCipher;
-    private final Cipher decryptCipher;
+    private final EncryptServiceConfig configuration;
 
     public AAAEncryptionServiceImpl(final EncryptServiceConfig configuration) {
+        this.configuration = configuration;
         final byte[] encryptionKeySalt = configuration.requireEncryptSalt();
-        final IvParameterSpec ivSpec;
         try {
             final var keyFactory = SecretKeyFactory.getInstance(configuration.getEncryptMethod());
             final var spec = new PBEKeySpec(configuration.requireEncryptKey().toCharArray(), encryptionKeySalt,
                     configuration.getEncryptIterationCount(), configuration.getEncryptKeyLength());
             key = new SecretKeySpec(keyFactory.generateSecret(spec).getEncoded(), configuration.getEncryptType());
-            ivSpec = new IvParameterSpec(encryptionKeySalt);
         } catch (GeneralSecurityException e) {
             throw new IllegalStateException("Failed to initialize secret key", e);
-        }
-        try {
-            final var cipher = Cipher.getInstance(configuration.getCipherTransforms());
-            cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
-            encryptCipher = cipher;
-        } catch (GeneralSecurityException e) {
-            throw new IllegalStateException("Failed to create encrypt cipher.", e);
-        }
-        try {
-            final var cipher = Cipher.getInstance(configuration.getCipherTransforms());
-            cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
-            decryptCipher = cipher;
-        } catch (GeneralSecurityException e) {
-            throw new IllegalStateException("Failed to create decrypt cipher.", e);
         }
         LOG.info("AAAEncryptionService activated");
     }
@@ -91,13 +82,52 @@ public final class AAAEncryptionServiceImpl implements AAAEncryptionService {
 
     @Override
     public byte[] encrypt(final byte[] data) throws BadPaddingException, IllegalBlockSizeException {
-        synchronized (encryptCipher) {
-            return encryptCipher.doFinal(requireNonNull(data));
+        final var iv = getRandomNonce(configuration.getIvLength());
+        final Cipher encryptCipher;
+        try {
+            encryptCipher = initCipher(Cipher.ENCRYPT_MODE, iv);
+        } catch (GeneralSecurityException e) {
+            throw new IllegalStateException("Failed to create encrypt cipher.", e);
         }
+        final var encryptedData = encryptCipher.doFinal(requireNonNull(data));
+        return ByteBuffer.allocate(iv.length + encryptedData.length)
+            .put(iv)
+            .put(encryptedData)
+            .array();
     }
 
     @Override
-    public byte[] decrypt(final byte[] encryptedData) throws BadPaddingException, IllegalBlockSizeException {
+    public byte[] decrypt(final byte[] encryptedDataWithIv) throws BadPaddingException, IllegalBlockSizeException {
+        if (encryptedDataWithIv.length < configuration.getIvLength()) {
+            throw  new IllegalArgumentException("Invalid encrypted data length.");
+        }
+        final var byteBuffer = ByteBuffer.wrap(encryptedDataWithIv);
+
+        final var iv = new byte[configuration.getIvLength()];
+        byteBuffer.get(iv);
+
+        final var encryptedData = new byte[byteBuffer.remaining()];
+        byteBuffer.get(encryptedData);
+
+        final Cipher decryptCipher;
+        try {
+            decryptCipher = initCipher(Cipher.DECRYPT_MODE, iv);
+        } catch (GeneralSecurityException e) {
+            throw new IllegalStateException("Failed to create decrypt cipher.", e);
+        }
         return decryptCipher.doFinal(requireNonNull(encryptedData));
+    }
+
+    private static byte[] getRandomNonce(int length) {
+        byte[] nonce = new byte[length];
+        SECURE_RANDOM.nextBytes(nonce);
+        return nonce;
+    }
+
+    private Cipher initCipher(int mode, byte[] iv) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException {
+        final var cipher = Cipher.getInstance(configuration.getCipherTransforms());
+        cipher.init(mode, key, new GCMParameterSpec(configuration.getAuthTagLength(), iv));
+        return cipher;
     }
 }
