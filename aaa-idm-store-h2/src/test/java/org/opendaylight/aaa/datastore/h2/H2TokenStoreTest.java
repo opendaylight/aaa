5665f882b0fc714856cac70a81ccd642d5dd2398
Peter Suna <peter.suna@pantheon.tech>
2024-03-25 18:17:24 +0100

    Migrate from Ehcache 2.x to Ehcache 3.x
    The Ehcache 2.x version is now archived and unsupported.
    https://github.com/ehcache/ehcache2
    
    Migrate to the supported Ehcache 3.x version 3.10.8
    https://github.com/ehcache/ehcache3/releases/tag/v3.10.8
    
    JIRA: AAA-274
    Change-Id: If335fb2b95cd68841f47b56bb06db75c917d331e
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- aaa-idm-store-h2/src/test/java/org/opendaylight/aaa/datastore/h2/H2TokenStoreTest.java
+++ aaa-idm-store-h2/src/test/java/org/opendaylight/aaa/datastore/h2/H2TokenStoreTest.java
@@ -22,15 +22,19 @@ import org.opendaylight.aaa.tokenauthrealm.auth.ClaimBuilder;
  * @author mserngawy
  */
 public class H2TokenStoreTest {
-    private final H2TokenStore h2TokenStore = new H2TokenStore(36000, 3600);
+    private static H2TokenStore h2TokenStore;
 
     @After
     public void teardown() throws Exception {
-        h2TokenStore.close();
+        if (h2TokenStore != null) {
+            h2TokenStore.close();
+            h2TokenStore.destroyPersistentFiles();
+        }
     }
 
     @Test
-    public void testTokenStore() throws InterruptedException {
+    public void testTokenStore() {
+        initTokenStore(36000, 3600);
         final String fooToken = "foo_token";
         Authentication auth = new AuthenticationBuilder(
                 new ClaimBuilder().setUser("foo").setUserId("1234").addRole("admin").build()).build();
@@ -38,5 +42,83 @@ public class H2TokenStoreTest {
         assertEquals(auth, h2TokenStore.get(fooToken));
         h2TokenStore.delete(fooToken);
         assertNull(h2TokenStore.get(fooToken));
+    }
+
+    @Test
+    public void testTokenStorePersistence() {
+        initTokenStore(36000, 3600);
+        final var fooToken = "foo_token";
+        final var auth1 = new AuthenticationBuilder(
+            new ClaimBuilder().setUser("foo").setUserId("1234").addRole("admin").build()).build();
+        h2TokenStore.put(fooToken, auth1);
+        final var auth2Token = "user2_token";
+        final var auth2 = new AuthenticationBuilder(
+            new ClaimBuilder().setUser("user2").setUserId("4321").addRole("admin").setDomain("domain")
+                .setClientId("clientID").build())
+            .setExpiration(100).build();
+        h2TokenStore.put(auth2Token, auth2);
+        // Restart TokenStore.
+        h2TokenStore.close();
+        initTokenStore(36000, 3600);
+        // Verify the loaded persistent data.
+        assertEquals(auth1, h2TokenStore.get(fooToken));
+        assertEquals(auth2, h2TokenStore.get(auth2Token));
+    }
+
+    @Test
+    public void testTokenStoreTimeToLive() throws Exception {
+        initTokenStore(1, 0);
+        final var fooToken = "foo_token";
+        final var auth1 = new AuthenticationBuilder(
+            new ClaimBuilder().setUser("foo").setUserId("1234").addRole("admin").build()).build();
+        h2TokenStore.put(fooToken, auth1);
+        Thread.sleep(550);
+        assertEquals(auth1, h2TokenStore.get(fooToken));
+        // Verify that after expired token time is removed.
+        Thread.sleep(550);
+        assertNull(h2TokenStore.get(fooToken));
+    }
+
+    @Test
+    public void testTokenStoreTimeToIdle() throws Exception {
+        initTokenStore(0, 1);
+        final var fooToken = "foo_token";
+        final var auth1 = new AuthenticationBuilder(
+            new ClaimBuilder().setUser("foo").setUserId("1234").addRole("admin").build()).build();
+        h2TokenStore.put(fooToken, auth1);
+        // Verify that the token is present.
+        Thread.sleep(550);
+        assertEquals(auth1, h2TokenStore.get(fooToken));
+        // Verify that the countdown is reset after the first access.
+        Thread.sleep(550);
+        assertEquals(auth1, h2TokenStore.get(fooToken));
+        // Verify that the token is removed after the TTI is reached.
+        Thread.sleep(1100);
+        assertNull(h2TokenStore.get(fooToken));
+    }
+
+    @Test
+    public void testTokenStoreCombinedTimeToIdleAndTimeToLive() throws Exception {
+        initTokenStore(2, 1);
+        final var fooToken = "foo_token";
+        final var auth1 = new AuthenticationBuilder(
+            new ClaimBuilder().setUser("foo").setUserId("1234").addRole("admin").build()).build();
+        h2TokenStore.put(fooToken, auth1);
+        // Verify token is present after expiring TTI, but before TTL expiration.
+        Thread.sleep(1100);
+        assertEquals(auth1, h2TokenStore.get(fooToken));
+        // Reset the TTI expiration.
+        Thread.sleep(550);
+        assertEquals(auth1, h2TokenStore.get(fooToken));
+        // Verify that the token is present after TTL has expired, but before the TTI expiration after a reset.
+        Thread.sleep(550);
+        assertEquals(auth1, h2TokenStore.get(fooToken));
+        // Verify that the data is removed after the TTI expiration.
+        Thread.sleep(1100);
+        assertNull(h2TokenStore.get(fooToken));
+    }
+
+    private static void initTokenStore(final long timeToLive, final long timeToIdle) {
+        h2TokenStore = new H2TokenStore(timeToLive, timeToIdle);
     }
 }
