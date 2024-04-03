6ec38222a333b16f25d8e13b9289d042cfad42d6
Peter Suna <peter.suna@pantheon.tech>
2024-04-03 09:54:20 +0200

    Remove TokenStore
    The usage of TokenStore was removed with OAuth2 implementations.
    https://git.opendaylight.org/gerrit/c/aaa/+/104968
    
    JIRA: AAA-255
    Change-Id: Ice6bb231bc4bd0f2c5df719c4f7c1673b41ff7fd
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/realm/TokenAuthRealm.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/realm/TokenAuthRealm.java
@@ -21,11 +21,9 @@ import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
-import org.eclipse.jdt.annotation.Nullable;
 import org.opendaylight.aaa.api.Authentication;
 import org.opendaylight.aaa.api.AuthenticationService;
 import org.opendaylight.aaa.api.TokenAuth;
-import org.opendaylight.aaa.api.TokenStore;
 import org.opendaylight.aaa.api.shiro.principal.ODLPrincipal;
 import org.opendaylight.aaa.shiro.principal.ODLPrincipalImpl;
 import org.opendaylight.aaa.shiro.realm.util.TokenUtils;
@@ -43,37 +41,27 @@ public class TokenAuthRealm extends AuthorizingRealm {
     private static final Logger LOG = LoggerFactory.getLogger(TokenAuthRealm.class);
     private static final ThreadLocal<TokenAuthenticators> AUTHENICATORS_TL = new ThreadLocal<>();
     private static final ThreadLocal<AuthenticationService> AUTH_SERVICE_TL = new ThreadLocal<>();
-    private static final ThreadLocal<TokenStore> TOKEN_STORE_TL = new ThreadLocal<>();
 
     private final TokenAuthenticators authenticators;
     private final AuthenticationService authService;
-    private final TokenStore tokenStore;
 
     public TokenAuthRealm() {
-        this(verifyLoad(AUTH_SERVICE_TL), verifyLoad(AUTHENICATORS_TL), TOKEN_STORE_TL.get());
+        this(verifyLoad(AUTH_SERVICE_TL), verifyLoad(AUTHENICATORS_TL));
     }
 
     public TokenAuthRealm(final AuthenticationService authService, final TokenAuthenticators authenticators) {
-        this(authService, authenticators, null);
-    }
-
-    public TokenAuthRealm(final AuthenticationService authService, final TokenAuthenticators authenticators,
-            final @Nullable TokenStore tokenStore) {
         this.authService = requireNonNull(authService);
         this.authenticators = requireNonNull(authenticators);
-        this.tokenStore = tokenStore;
         super.setName("TokenAuthRealm");
     }
 
     public static Registration prepareForLoad(final AuthenticationService authService,
-            final TokenAuthenticators authenticators, final @Nullable TokenStore tokenStore) {
+            final TokenAuthenticators authenticators) {
         AUTH_SERVICE_TL.set(requireNonNull(authService));
         AUTHENICATORS_TL.set(requireNonNull(authenticators));
-        TOKEN_STORE_TL.set(tokenStore);
         return () -> {
             AUTH_SERVICE_TL.remove();
             AUTHENICATORS_TL.remove();
-            TOKEN_STORE_TL.remove();
         };
     }
 
