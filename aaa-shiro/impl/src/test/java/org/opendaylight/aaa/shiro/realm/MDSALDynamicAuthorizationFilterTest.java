32ae3606151ffb2f69adc83ac7d7266243e6d2e7
Matej Sramcik <matej.sramcik@pantheon.tech>
2024-07-22 14:21:05 +0200

    Add test for null value in permissions
    Added a test for MDSALDynamicAuthorizationFilter where permissions are
    set to null, previously causing a NullPointerException.
    
    Issue resolved by: https://git.opendaylight.org/gerrit/c/aaa/+/109475
    
    JIRA: AAA-224
    Change-Id: Ia9ffcd752760d0bcae35fe8491f505f624668530
    Signed-off-by: Matej Sramcik <matej.sramcik@pantheon.tech>

--- aaa-shiro/impl/src/test/java/org/opendaylight/aaa/shiro/realm/MDSALDynamicAuthorizationFilterTest.java
+++ aaa-shiro/impl/src/test/java/org/opendaylight/aaa/shiro/realm/MDSALDynamicAuthorizationFilterTest.java
@@ -316,4 +316,32 @@ public class MDSALDynamicAuthorizationFilterTest {
         when(request.getMethod()).thenReturn("Get");
         assertTrue(filter.isAccessAllowed(request, null, null));
     }
+
+    @Test
+    public void testNullPermissions() throws Exception {
+        // admin can do anything
+        final String resource = "/**";
+        final String description = "Test description";
+        final var innerPolicies = mock(org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.aaa.rev161214
+            .http.authorization.policies.Policies.class);
+        when(innerPolicies.getResource()).thenReturn(resource);
+        when(innerPolicies.getDescription()).thenReturn(description);
+
+        // Return null permissions
+        when(innerPolicies.getPermissions()).thenReturn(null);
+        final Policies policies = mock(Policies.class);
+        when(policies.getPolicies()).thenReturn(List.of(innerPolicies));
+        final HttpAuthorization httpAuthorization = mock(HttpAuthorization.class);
+        when(httpAuthorization.getPolicies()).thenReturn(policies);
+
+        final Subject subject = mock(Subject.class);
+        final MDSALDynamicAuthorizationFilter filter = newFilter(subject, mockDataBroker(httpAuthorization));
+
+        final HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getRequestURI()).thenReturn("/abc");
+        when(request.getMethod()).thenReturn("Put");
+
+        when(request.getMethod()).thenReturn("Get");
+        assertFalse(filter.isAccessAllowed(request, null, null));
+    }
 }
