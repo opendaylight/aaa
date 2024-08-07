81473fa3273c5a979f6c29318a7590a619b53d12
Peter Suna <peter.suna@pantheon.tech>
2024-08-07 13:53:21 +0200

    Create Filter for jetty Request
    The Authentication field is set to UNAUTHENTICATED because
    the PaxWebServletHandler could not set this field due to
    the missing remote.user attribute.
    
    To resolve this create custom aaa filter
    JettyAuthenticationLogFilter.
    This filter sets the Authentication field with the user from
    Basic authorization to ensure the correct name is printed
    in the NCSA logs.
    
    To enable this filter is required to activate it in Karaf console:
    scr:enable org.opendaylight.aaa.filter.JettyAuthenticationLogFilter
    
    Or activate it through configuration file:
    `etc/org.opendaylight.aaa.filterchain.cfg`:
    
    customFilterList=org.opendaylight.aaa.filter
    .JettyAuthenticationLogFilter
    
    JIRA: AAA-282
    Change-Id: I26c1975ab30f0e57f0c57519d4cc1cc0becdc826
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ aaa-jetty-auth-log-filter/src/test/java/org/opendaylight/aaa/filter/JettyAuthenticationLogFilterTest.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.filter;
+
+import static org.apache.shiro.subject.support.DefaultSubjectContext.PRINCIPALS_SESSION_KEY;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+import java.security.Principal;
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpSession;
+import org.apache.shiro.subject.SimplePrincipalCollection;
+import org.eclipse.jetty.security.UserAuthentication;
+import org.eclipse.jetty.server.Authentication;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class JettyAuthenticationLogFilterTest {
+    private static final String SESSION_USER = "sessionUser";
+    private static final String BASIC_USER = "admin";
+
+    @Mock
+    private HttpServletRequestWrapper mockServletRequest;
+    @Mock
+    private Response mockResponse;
+    @Mock
+    private FilterChain mockFilterChain;
+    @Mock
+    private HttpSession mockHttpSession;
+    @Mock
+    private Request mockRequest;
+    @Mock
+    private Principal mockPrincipal;
+    @Mock
+    private SimplePrincipalCollection mockSimplePrincipal;
+    @Captor
+    private ArgumentCaptor<UserAuthentication> userCaptor;
+
+    private final JettyAuthenticationLogFilter logFilter = new JettyAuthenticationLogFilter();
+
+    @BeforeEach
+    void beforeEach() {
+        // Prepare environment.
+        doReturn(mockRequest).when(mockServletRequest).getRequest();
+    }
+
+    @AfterEach
+    public void teardown() {
+        logFilter.destroy();
+    }
+
+    @Test
+    void testFilterWithBasicAuth() throws Exception {
+        // Setup environment only for Basic Auth without session.
+        doReturn(200).when(mockResponse).getStatus();
+        doReturn("Basic YWRtaW46YWRtaW4=").when(mockRequest).getHeader(eq("Authorization"));
+
+        // Execute filter.
+        logFilter.doFilter(mockServletRequest, mockResponse, mockFilterChain);
+
+        // Verify correct Authentication user.
+        verify(mockRequest).setAuthentication(userCaptor.capture());
+        final var noSessionAuthentication = userCaptor.getValue();
+        assertNotNull(noSessionAuthentication);
+        assertEquals(BASIC_USER, noSessionAuthentication.getUserIdentity().getUserPrincipal().getName());
+    }
+
+    @Test
+    void testFilterWithUnauthenticatedRequestWithBasicAuth() throws Exception {
+        // Setup environment only for Basic Auth without session and Request with Unauthenticated value.
+        doReturn(200).when(mockResponse).getStatus();
+        doReturn("Basic YWRtaW46YWRtaW4=").when(mockRequest).getHeader(eq("Authorization"));
+        doReturn(Authentication.UNAUTHENTICATED).when(mockRequest).getAuthentication();
+
+        // Execute filter.
+        logFilter.doFilter(mockServletRequest, mockResponse, mockFilterChain);
+
+        // Verify correct Authentication user.
+        verify(mockRequest).setAuthentication(userCaptor.capture());
+        final var sessionAuthentication = userCaptor.getValue();
+        assertNotNull(sessionAuthentication);
+        assertEquals(BASIC_USER, sessionAuthentication.getUserIdentity().getUserPrincipal().getName());
+    }
+
+    @Test
+    void testFilterWithSession() throws Exception {
+        // Setup environment only for Session without Basic Authentication.
+        doReturn(200).when(mockResponse).getStatus();
+        doReturn(mockHttpSession).when(mockRequest).getSession(eq(false));
+        doReturn(mockSimplePrincipal).when(mockHttpSession).getAttribute(eq(PRINCIPALS_SESSION_KEY));
+        doReturn(mockPrincipal).when(mockSimplePrincipal).getPrimaryPrincipal();
+        doReturn(SESSION_USER).when(mockPrincipal).getName();
+
+        // Execute filter.
+        logFilter.doFilter(mockServletRequest, mockResponse, mockFilterChain);
+
+        // Verify correct Authentication user.
+        verify(mockRequest).setAuthentication(userCaptor.capture());
+        final var sessionAuthentication = userCaptor.getValue();
+        assertNotNull(sessionAuthentication);
+        assertEquals(SESSION_USER, sessionAuthentication.getUserIdentity().getUserPrincipal().getName());
+    }
+
+    @Test
+    void testFilterWithSessionAndUnauthorizedStatus() throws Exception {
+        // Set up the environment with Session, Basic Authentication, and a 401 HTTP status.
+        doReturn("Basic YWRtaW46YWRtaW4=").when(mockRequest).getHeader(eq("Authorization"));
+        doReturn(mockHttpSession).when(mockRequest).getSession(eq(false));
+        doReturn(401).when(mockResponse).getStatus();
+
+        // Execute filter.
+        logFilter.doFilter(mockServletRequest, mockResponse, mockFilterChain);
+
+        // Verify correct Authentication user.
+        verify(mockRequest).setAuthentication(userCaptor.capture());
+        final var sessionAuthentication = userCaptor.getValue();
+        assertNotNull(sessionAuthentication);
+        assertEquals(BASIC_USER, sessionAuthentication.getUserIdentity().getUserPrincipal().getName());
+    }
+}
