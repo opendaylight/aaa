75e27dfa0b82278e82ea42b630813987b46f4f85
Peter Suna <peter.suna@pantheon.tech>
2024-08-07 13:53:21 +0200

    Set Authentication field in jetty Request
    The Authentication field is set to UNAUTHENTICATED because
    the PaxWebServletHandler could not set this field due to
    the missing remote.user attribute.
    
    This commit sets the Authentication field with the user from
    Basic authorization to ensure the correct name is printed
    in the NCSA logs.
    
    JIRA: AAA-282
    Change-Id: I26c1975ab30f0e57f0c57519d4cc1cc0becdc826
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- aaa-filterchain/src/main/java/org/opendaylight/aaa/filterchain/filters/CustomFilterAdapter.java
+++ aaa-filterchain/src/main/java/org/opendaylight/aaa/filterchain/filters/CustomFilterAdapter.java
@@ -9,7 +9,11 @@ package org.opendaylight.aaa.filterchain.filters;
 
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -17,6 +21,14 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpSession;
+import org.eclipse.jetty.security.AbstractLoginService;
+import org.eclipse.jetty.security.DefaultUserIdentity;
+import org.eclipse.jetty.security.UserAuthentication;
+import org.eclipse.jetty.server.Authentication;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
 import org.opendaylight.aaa.filterchain.configuration.CustomFilterAdapterConfiguration;
 import org.opendaylight.aaa.filterchain.configuration.CustomFilterAdapterListener;
 import org.slf4j.Logger;
@@ -42,8 +54,11 @@ import org.slf4j.LoggerFactory;
  */
 public class CustomFilterAdapter implements Filter, CustomFilterAdapterListener {
     private static final Logger LOG = LoggerFactory.getLogger(CustomFilterAdapter.class);
+    private static final String BASIC = "Basic";
+    private static final String BASIC_SEP = BASIC + " ";
 
     private final CustomFilterAdapterConfiguration customFilterAdapterConfig;
+    private final Map<HttpSession, Authentication> sessionMap;
 
     private FilterConfig filterConfig;
 
@@ -54,6 +69,7 @@ public class CustomFilterAdapter implements Filter, CustomFilterAdapterListener 
 
     public CustomFilterAdapter(final CustomFilterAdapterConfiguration customFilterAdapterConfig) {
         this.customFilterAdapterConfig = customFilterAdapterConfig;
+        sessionMap = new HashMap<>();
     }
 
     @Override
@@ -73,6 +89,9 @@ public class CustomFilterAdapter implements Filter, CustomFilterAdapterListener 
             chain.doFilter(request, response);
         } else {
             AAAFilterChain.createAAAFilterChain().doFilter(request, response, chain, localFilterChain);
+        }
+        if (request instanceof HttpServletRequestWrapper requestWrapper) {
+            setRequestAuthenticationFromUserInHeader(requestWrapper, useSession(response));
         }
     }
 
@@ -98,5 +117,53 @@ public class CustomFilterAdapter implements Filter, CustomFilterAdapterListener 
     @Override
     public FilterConfig getFilterConfig() {
         return filterConfig;
+    }
+
+    private void setRequestAuthenticationFromUserInHeader(final HttpServletRequestWrapper request,
+            final boolean useSession) {
+        if (request.getRequest() instanceof Request baseRequest) {
+            final var requestSession = baseRequest.getSession();
+            if (useSession && sessionMap.containsKey(requestSession)) {
+                baseRequest.setAuthentication(sessionMap.get(requestSession));
+            } else {
+                final var authorization = baseRequest.getHeader("Authorization");
+                if (authorization != null && authorization.startsWith(BASIC_SEP)
+                    && requestIsNullOrUnauthenticated(baseRequest)) {
+                    final var userAndPassword = new String(
+                        Base64.getDecoder().decode(authorization.substring(BASIC_SEP.length())), StandardCharsets.UTF_8)
+                        .split(":");
+
+                    final var userPrincipal = new AbstractLoginService.UserPrincipal(userAndPassword[0], null);
+                    final var defaultUserIdentity = new DefaultUserIdentity(null, userPrincipal, new String[0]);
+                    final var userAuthentication = new UserAuthentication(BASIC, defaultUserIdentity);
+                    baseRequest.setAuthentication(userAuthentication);
+                    if (useSession && requestSession != null) {
+                        removeOldSessionForUser(userAuthentication);
+                        sessionMap.put(requestSession, userAuthentication);
+                    }
+                }
+            }
+        }
+    }
+
+    private void removeOldSessionForUser(final UserAuthentication userAuthentication) {
+        sessionMap.entrySet().stream()
+            .filter(entry -> userAuthentication.toString().equals(entry.getValue().toString()))
+            .map(Map.Entry::getKey)
+            .toList()
+            .forEach(sessionMap::remove);
+    }
+
+    private static boolean requestIsNullOrUnauthenticated(final Request baseRequest) {
+        return baseRequest.getAuthentication() == null
+            || Authentication.UNAUTHENTICATED == baseRequest.getAuthentication();
+    }
+
+    private static boolean useSession(final ServletResponse servletResponse) {
+        if (servletResponse instanceof Response response) {
+            final var status = response.getStatus();
+            return status < 400 || status >= 500;
+        }
+        return true;
     }
 }
