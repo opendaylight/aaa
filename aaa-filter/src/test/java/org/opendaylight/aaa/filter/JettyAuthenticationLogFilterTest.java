31847d2de6924f8e416d2cf9fae9bcde481207e1
Peter Suna <peter.suna@pantheon.tech>
2024-08-07 13:53:21 +0200

    Create Filter for jetty Request
    The Authentication field is set to UNAUTHENTICATED because
    the PaxWebServletHandler could not set this field due to
    the missing remote.user attribute.
    
    To resolve this create custom aaa filter
    JettyAuthenticationLogFilter.
    This filter sets the Authentication field with the user from
    Basic authorization to ensure the correct name is printed
    in the NCSA logs.
    
    To enable this filter is required to edit filterchain configuration
    etc/org.opendaylight.aaa.filterchain.cfg :
    
    `customFilterList
    =org.opendaylight.aaa.filter.JettyAuthenticationLogFilter`
    
    JIRA: AAA-282
    Change-Id: I26c1975ab30f0e57f0c57519d4cc1cc0becdc826
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

---
+++ aaa-filter/src/test/java/org/opendaylight/aaa/filter/JettyAuthenticationLogFilterTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2024 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.filter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpSession;
+import org.eclipse.jetty.security.UserAuthentication;
+import org.eclipse.jetty.server.Authentication;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+public class JettyAuthenticationLogFilterTest {
+    private static JettyAuthenticationLogFilter customFilterAdapter;
+
+    private final HttpServletRequestWrapper mockServletRequest = mock(HttpServletRequestWrapper.class);
+    private final Response mockResponse = mock(Response.class);
+    private final FilterChain mockFilterChain = mock(FilterChain.class);
+
+    @BeforeAll
+    public static void setUp() {
+        customFilterAdapter = new JettyAuthenticationLogFilter();
+    }
+
+    @AfterAll
+    public static void teardown() {
+        customFilterAdapter.destroy();
+    }
+
+    @Test
+    public void testSettingAuthenticationIntoJettyRequest() throws Exception {
+        // Prepare environment.
+        doReturn(200).when(mockResponse).getStatus();
+        final var mockRequest = mock(Request.class);
+        doReturn("Basic YWRtaW46YWRtaW4=").when(mockRequest).getHeader(eq("Authorization"));
+        final var mockHttpSession = mock(HttpSession.class);
+        doReturn(mockHttpSession).when(mockRequest).getSession();
+        doReturn(mockRequest).when(mockServletRequest).getRequest();
+
+        // Test JettyAuthenticationLogFilter with Basic authentication only.
+        customFilterAdapter.doFilter(mockServletRequest, mockResponse, mockFilterChain);
+
+        final var UserCaptor = ArgumentCaptor.forClass(UserAuthentication.class);
+        verify(mockRequest).setAuthentication(UserCaptor.capture());
+        final var noSessionAuthentication = UserCaptor.getValue();
+        assertNotNull(noSessionAuthentication);
+        assertEquals("admin", noSessionAuthentication.getUserIdentity().getUserPrincipal().getName());
+
+        // Test JettyAuthenticationLogFilter with a session and an unauthenticated request.
+        doReturn(Authentication.UNAUTHENTICATED).when(mockRequest).getAuthentication();
+        customFilterAdapter.doFilter(mockServletRequest, mockResponse, mockFilterChain);
+
+        verify(mockRequest, times(2)).setAuthentication(UserCaptor.capture());
+        final var sessionAuthentication = UserCaptor.getValue();
+        assertNotNull(sessionAuthentication);
+        assertEquals(noSessionAuthentication.hashCode(), sessionAuthentication.hashCode());
+        assertEquals(noSessionAuthentication, sessionAuthentication);
+
+        // Test JettyAuthenticationLogFilter with a session restart due to unauthorized access.
+        doReturn(401).when(mockResponse).getStatus();
+        customFilterAdapter.doFilter(mockServletRequest, mockResponse, mockFilterChain);
+
+        verify(mockRequest, times(3)).setAuthentication(UserCaptor.capture());
+        final var unauthorizedUserAuthentication = UserCaptor.getValue();
+        assertNotNull(unauthorizedUserAuthentication);
+        assertNotSame(unauthorizedUserAuthentication, sessionAuthentication);
+        assertEquals(unauthorizedUserAuthentication.getUserIdentity().getUserPrincipal().getName(),
+            sessionAuthentication.getUserIdentity().getUserPrincipal().getName());
+        assertEquals(1, customFilterAdapter.sessionMap().size());
+    }
+}
