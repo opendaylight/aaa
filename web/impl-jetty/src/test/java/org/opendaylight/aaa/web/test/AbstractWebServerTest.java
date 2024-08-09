cbd6be52e985478e296f2cfa8440941101f1f9ec
Robert Varga <robert.varga@pantheon.tech>
2024-08-09 21:28:44 +0200

    Clean up AbstractWebServerTest
    Do not use URI constructor, as that has been deprecated since Java 20.
    While we are here, perform a bit of modernization.
    
    Change-Id: I1179f53f4f165958711dfb299fc56b06f44520a2
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- web/impl-jetty/src/test/java/org/opendaylight/aaa/web/test/AbstractWebServerTest.java
+++ web/impl-jetty/src/test/java/org/opendaylight/aaa/web/test/AbstractWebServerTest.java
@@ -7,17 +7,13 @@
  */
 package org.opendaylight.aaa.web.test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
-import com.google.common.io.CharStreams;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
-import javax.servlet.ServletException;
 import org.junit.Test;
 import org.opendaylight.aaa.web.FilterDetails;
 import org.opendaylight.aaa.web.ServletDetails;
@@ -34,7 +30,7 @@ public abstract class AbstractWebServerTest {
     protected abstract WebServer getWebServer();
 
     @Test
-    public void testAddAfterStart() throws ServletException, IOException {
+    public void testAddAfterStart() throws Exception {
         var webContext = WebContext.builder()
             .contextPath("/test1")
             .addServlet(ServletDetails.builder().addUrlPattern("/*").name("Test").servlet(new TestServlet()).build())
@@ -45,7 +41,7 @@ public abstract class AbstractWebServerTest {
     }
 
     @Test
-    public void testAddAfterStartWithoutSlashOnServlet() throws ServletException, IOException {
+    public void testAddAfterStartWithoutSlashOnServlet() throws Exception {
         // NB subtle difference to testAddAfterStart() test: addUrlPattern("*") instead of /* with slash!
         var builder = WebContext.builder()
             .contextPath("/test1");
@@ -91,13 +87,9 @@ public abstract class AbstractWebServerTest {
         assertFalse(testListener.isInitialized);
     }
 
-    static void checkTestServlet(final String urlPrefix) throws IOException {
-        try (var inputStream = new URL(urlPrefix + "/something").openConnection().getInputStream()) {
-            // The hard-coded ASCII here is strictly speaking wrong of course
-            // (should interpret header from reply), but good enough for a test.
-            try (var reader = new InputStreamReader(inputStream, StandardCharsets.US_ASCII)) {
-                assertEquals("hello, world\r\n", CharStreams.toString(reader));
-            }
+    static void checkTestServlet(final String urlPrefix) throws Exception {
+        try (var inputStream = new URI(urlPrefix + "/something").toURL().openConnection().getInputStream()) {
+            assertArrayEquals("hello, world\r\n".getBytes(StandardCharsets.US_ASCII), inputStream.readAllBytes());
         }
     }
 }
