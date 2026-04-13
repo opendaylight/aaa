0f84c546553e713f1b125491e451dcc8eedcb1e6
Samuel Schneider <samuel.schneider@pantheon.tech>
2026-04-13 18:38:34 +0200

    Implement oauth2 token realm
    Add filter and realm which authorize and authenticate user based
    on oauth2-proxy forwarded headers after successful authorization
    on identity provider server.
    The realm relies on forwarded information being trustworthy.
    
    JIRA: AAA-298
    Change-Id: Iea4251d9a1b8faf945f2109c5e2c6bc8335b4f9c
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/filters/Oauth2ProxyTokenFilter.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.shiro.filters;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
+import org.apache.shiro.web.util.WebUtils;
+
+/**
+ * Requires the requesting user to be {@link org.apache.shiro.subject.Subject#isAuthenticated() authenticated} for the
+ * request to continue, and if they're not, requires oauth2 forwarded headers after successful user authorization on
+ * identity provider server.
+ * <p/>
+ * The {@link #onAccessDenied(ServletRequest, ServletResponse)} method will
+ * only be called if the subject making the request is not
+ * {@link org.apache.shiro.subject.Subject#isAuthenticated() authenticated}
+ */
+public class Oauth2ProxyTokenFilter extends AuthenticatingFilter {
+    // ODL is set as upstream of OAuth2-Proxy thus X-Forwarded instead of X-Auth-Request headers
+    private static final String PROXY_HEADER_USER = "X-Forwarded-User";
+    private static final String PROXY_HEADER_GROUPS = "X-Forwarded-Groups";
+
+    @Override
+    protected AuthenticationToken createToken(final ServletRequest request, final ServletResponse response) {
+        final var httpRequest = WebUtils.toHttp(request);
+        final var user = httpRequest.getHeader(PROXY_HEADER_USER);
+        final var groups = httpRequest.getHeader(PROXY_HEADER_GROUPS);
+        return new Oauth2ProxyToken(groups, user);
+    }
+
+    @Override
+    protected boolean onAccessDenied(final ServletRequest request, final ServletResponse response) throws Exception {
+        final var token = ((HttpServletRequest) request).getHeader(PROXY_HEADER_USER);
+
+        if (token != null) {
+            return executeLogin(request, response);
+        }
+
+        var httpResponse = WebUtils.toHttp(response);
+        httpResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        return false;
+    }
+}
---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/realm/Oauth2ProxyTokenRealm.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.shiro.realm;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.opendaylight.aaa.api.shiro.principal.ODLPrincipal;
+import org.opendaylight.aaa.shiro.filters.Oauth2ProxyToken;
+import org.opendaylight.aaa.shiro.principal.ODLPrincipalImpl;
+
+/**
+ * Realm that relies on user being successfully authorized by identity provider server. Process information
+ * forwarded by oauth2-proxy, authorize and authenticate user based on that.
+ */
+public final class Oauth2ProxyTokenRealm extends AuthorizingRealm {
+    private static final String SDN_DOMAIN = "sdn";
+
+    @Override
+    public boolean supports(final AuthenticationToken token) {
+        return token instanceof Oauth2ProxyToken;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(final AuthenticationToken token)
+            throws AuthenticationException {
+        if (!(token instanceof Oauth2ProxyToken proxyToken)) {
+            throw new AuthenticationException("Only Oauth2ProxyToken is supported by Oauth2ProxyRealm");
+        }
+        final var userId = proxyToken.user() + "@" + SDN_DOMAIN;
+        final var odlPrincipal = ODLPrincipalImpl.createODLPrincipal(proxyToken.user(), SDN_DOMAIN, userId,
+            parseRoles(proxyToken.groups()));
+        return new SimpleAuthenticationInfo(odlPrincipal, token.getCredentials(), getName());
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(final PrincipalCollection principalCollection) {
+        final var primary = principalCollection.getPrimaryPrincipal();
+        if (primary instanceof ODLPrincipal odlPrincipal) {
+            return new SimpleAuthorizationInfo(odlPrincipal.getRoles());
+        }
+        return new SimpleAuthorizationInfo();
+    }
+
+    // Parse roles from X-Auth-Request-Groups header. example: role:global-admin,role:odl-application:admin
+    // roles are separated by ","
+    // each role can have namespace with ":" as separator, we are only interested in the actual value
+    private static Set<String> parseRoles(final String groups) {
+        return Arrays.stream(groups.split(","))
+            .map(s -> s.split(":"))
+            .map(s -> s[s.length - 1])
+            .collect(Collectors.toSet());
+    }
+}
