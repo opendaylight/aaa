e4885bd03708288d773f878270dd8fec8ac0ea10
Samuel Schneider <samuel.schneider@pantheon.tech>
2026-04-13 18:38:34 +0200

    Implement oauth2 proxy header realm
    Add filter and realm which authorize and authenticate user based
    on oauth2-proxy forwarded headers after successful authorization
    on identity provider server.
    
    The realm relies on forwarded information being trustworthy.
    
    JIRA: AAA-298
    Change-Id: Iea4251d9a1b8faf945f2109c5e2c6bc8335b4f9c
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/filters/Oauth2ProxyHeaderFilter.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.shiro.filters;
+
+import java.util.Collections;
+import java.util.List;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
+import org.apache.shiro.web.util.WebUtils;
+
+/**
+ * Shiro filter that authenticates requests forwarded by an upstream OAuth2-Proxy instance.
+ *
+ * <p>ODL is deployed as an upstream service behind OAuth2-Proxy. After the proxy authenticates
+ * the user against an external identity provider, it injects {@code X-Forwarded-User} and
+ * {@code X-Forwarded-Groups} headers into the proxied request. This filter reads those headers
+ * and creates an {@link Oauth2ProxyHeaderToken} for the configured realm to process.
+ *
+ * <p><strong>Security prerequisite:</strong> direct HTTP access to ODL that bypasses the proxy
+ * must be blocked at the network level. Failure to do so allows any caller to forge these headers
+ * and authenticate as an arbitrary user.
+ */
+public final class Oauth2ProxyHeaderFilter extends AuthenticatingFilter {
+    // ODL is set as upstream of OAuth2-Proxy thus X-Forwarded instead of X-Auth-Request headers
+    private static final String PROXY_HEADER_USER = "X-Forwarded-User";
+    private static final String PROXY_HEADER_GROUPS = "X-Forwarded-Groups";
+
+    @Override
+    protected AuthenticationToken createToken(final ServletRequest request, final ServletResponse response) {
+        final var httpRequest = WebUtils.toHttp(request);
+        final var groupHeader = httpRequest.getHeaders(PROXY_HEADER_GROUPS);
+        final var groups = groupHeader != null ? Collections.list(groupHeader) : List.<String>of();
+        return new Oauth2ProxyHeaderToken(groups, extractUser(request));
+    }
+
+    @Override
+    protected boolean onAccessDenied(final ServletRequest request, final ServletResponse response) throws Exception {
+        final var user = extractUser(request);
+        if (user != null && !user.isEmpty()) {
+            return executeLogin(request, response);
+        }
+        WebUtils.toHttp(response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        return false;
+    }
+
+    private static String extractUser(final ServletRequest request) {
+        return WebUtils.toHttp(request).getHeader(PROXY_HEADER_USER);
+    }
+}
