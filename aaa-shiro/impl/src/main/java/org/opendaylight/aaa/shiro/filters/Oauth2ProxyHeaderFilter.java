f09d0a3e7933a1501f257133e24c511a58b3f9ca
Samuel Schneider <samuel.schneider@pantheon.tech>
2026-04-13 18:38:34 +0200

    Implement oauth2 proxy header realm
    Add filter and realm which authorize and authenticate user based
    on oauth2-proxy forwarded headers after successful authorization
    on identity provider server.
    
    The realm relies on forwarded information being trustworthy.
    
    JIRA: AAA-298
    Change-Id: Iea4251d9a1b8faf945f2109c5e2c6bc8335b4f9c
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

---
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/filters/Oauth2ProxyHeaderFilter.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2026 PANTHEON.tech, s.r.o. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.shiro.filters;
+
+import java.util.Collections;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
+import org.apache.shiro.web.util.WebUtils;
+
+/**
+ * Requires the requesting user to be {@link org.apache.shiro.subject.Subject#isAuthenticated() authenticated} for the
+ * request to continue, and if they're not, requires oauth2 forwarded headers after successful user authorization on
+ * identity provider server.
+ * The {@link #onAccessDenied(ServletRequest, ServletResponse)} method will
+ * only be called if the subject making the request is not
+ * {@link org.apache.shiro.subject.Subject#isAuthenticated() authenticated}
+ */
+public class Oauth2ProxyHeaderFilter extends AuthenticatingFilter {
+    // ODL is set as upstream of OAuth2-Proxy thus X-Forwarded instead of X-Auth-Request headers
+    private static final String PROXY_HEADER_USER = "X-Forwarded-User";
+    private static final String PROXY_HEADER_GROUPS = "X-Forwarded-Groups";
+
+    @Override
+    protected AuthenticationToken createToken(final ServletRequest request, final ServletResponse response) {
+        final var httpRequest = WebUtils.toHttp(request);
+        final var user = httpRequest.getHeader(PROXY_HEADER_USER);
+        final var groups = Collections.list(httpRequest.getHeaders(PROXY_HEADER_GROUPS));
+        return new Oauth2ProxyHeaderToken(groups, user);
+    }
+
+    @Override
+    protected boolean onAccessDenied(final ServletRequest request, final ServletResponse response) throws Exception {
+        final var token = WebUtils.toHttp(request).getHeader(PROXY_HEADER_USER);
+
+        if (token != null && !token.isEmpty()) {
+            return executeLogin(request, response);
+        }
+
+        final var httpResponse = WebUtils.toHttp(response);
+        httpResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        return false;
+    }
+}
