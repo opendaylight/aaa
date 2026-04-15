9d4364a530d8146c9b4591d5d999158388ee5f56
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2026-04-15 15:58:49 +0200

    Eliminate TokenUtils class
    KeystoneAuthRealm, MdsalRealm, MoonRealm and TokenAuthRealm are using
    the default UsernamePasswordToken.
    
    At the same time they are the only users of TokenUtils class which
    accepts AuthenticationToken interface but in fact it works with
    UsernamePasswordToken only.
    
    Remove the utility class and (since we can) assume in concedes realm
    implementations presence of UsernamePasswordToken which allows us
    to cast to expected token type and get principal and credentials in
    desired types directly.
    
    Since all AuthenticatingRealm descendants are by default ensured to use
    UsernamePasswordToken we can remove all test cases testing null or
    other types of token being pass to particular realms.
    
    JIRA: AAA-299
    Change-Id: I6b299a5d6ce6dfd06c3a0e1d7008a759aca7675a
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/realm/KeystoneAuthRealm.java
+++ aaa-shiro/impl/src/main/java/org/opendaylight/aaa/shiro/realm/KeystoneAuthRealm.java
@@ -63,7 +63,6 @@ public class KeystoneAuthRealm extends AuthorizingRealm {
     private static final String NO_CATALOG_OPTION = "nocatalog";
     private static final String DEFAULT_KEYSTONE_DOMAIN = "Default";
     private static final String USERNAME_DOMAIN_SEPARATOR = "@";
-    private static final String FATAL_ERROR_BASIC_AUTH_ONLY = "{\"error\":\"Only basic authentication is supported\"}";
     private static final String FATAL_ERROR_INVALID_URL = "{\"error\":\"Invalid URL to Keystone server\"}";
     private static final String UNABLE_TO_AUTHENTICATE = "{\"error\":\"Could not authenticate\"}";
     private static final String AUTH_PATH = "v3/auth/tokens";
@@ -151,10 +150,7 @@ public class KeystoneAuthRealm extends AuthorizingRealm {
     protected AuthenticationInfo doGetAuthenticationInfo(
             final AuthenticationToken authenticationToken,
             final SimpleHttpClient client) {
-        if (!(authenticationToken instanceof UsernamePasswordToken usernamePasswordToken)) {
-            LOG.error("Only basic authentication is supported");
-            throw new AuthenticationException(FATAL_ERROR_BASIC_AUTH_ONLY);
-        }
+        final var usernamePasswordToken = (UsernamePasswordToken) authenticationToken;
 
         final URI theServerUri = getServerUri();
         if (theServerUri == null) {
